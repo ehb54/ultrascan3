e368b810f85b463b0968548e8944bcdcf209fca0
demeler <7659499+demeler@users.noreply.github.com>
2008-06-15 19:01:59 +0000

    Set font to system font

--- develop/src/us_win.cpp
+++ develop/src/us_win.cpp
@@ -60,11 +60,11 @@ UsWin::UsWin(QWidget *parent, const char *name)
 	//painter.setPen( QPen( Qt::blue, 4 ) );
 	//painter.drawRect( QRect( 0, 0, pw, ph ) );
 
-	painter.setFont( QFont( "Arial", 11, QFont::Bold ) );
+	painter.setFont( QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize, QFont::Bold ) );
 	painter.setPen( Qt::white );
 
 	QString version = "UltraScan " + US_Version +  " for " OS_TITLE;
-	QFontMetrics metrics( QFont( "Arial", 12, QFont::Bold ) );
+	QFontMetrics metrics( QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize, QFont::Bold ) );
 	int sWidth = metrics.boundingRect( version ).width();
 	int x      = ( pw - sWidth ) / 2;
 
