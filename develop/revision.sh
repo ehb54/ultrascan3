f0710c21b75f9aff9150f84bd1693d76e7858d49
demeler <7659499+demeler@users.noreply.github.com>
2008-06-29 15:46:56 +0000

    Avoids recompiling us_win.cpp

--- develop/revision.sh
+++ develop/revision.sh
@@ -1,3 +1,9 @@
-REV=`svn info|grep Revision`
-echo "#define REVISION \"$REV\"" > include/us_revision.h
+REV1=`svn info|grep Revision`
+REV2=`cut -b 19-31 include/us_revision.h`
+if [ "$REV1" != "$REV2" ]
+then
+		  echo $REV1
+		  echo $REV2
+echo "#define REVISION \"$REV1\"" > include/us_revision.h
+fi
 
