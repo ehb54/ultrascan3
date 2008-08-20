334c7febaca4375bd72b52d4e5aa197c47ccc468
dubbs <dubbs@4bb6f89d-cd66-4eab-8136-febab30699dd>
2008-08-20 23:12:31 +0000

    Cleaned up th ehtml generated in us_montecarloreport. It still needs work though, including getHeader() and getFooter() to be valid ( in us_util ).

--- develop/include/us_montecarloreport.h
+++ develop/include/us_montecarloreport.h
@@ -2,41 +2,29 @@
 #define US_MONTECARLOREPORT_H
 
 #include "us.h"
-
 #include "us_util.h"
-#include "us_dtactrl.h"
+
 #include <qobject.h>
 #include <qfile.h>
 #include <qmessagebox.h>
 
-class US_EXTERN US_Report_MonteCarlo : public QWidget
+class US_EXTERN US_Report_MonteCarlo: public QWidget
 {
 	Q_OBJECT
-	
+
 	public:
 	
-		US_Report_MonteCarlo(bool, QWidget *p=0, const char *name=0);
-		US_Report_MonteCarlo(QString, QWidget *p=0, const char *name=0);
-		~US_Report_MonteCarlo();
+		US_Report_MonteCarlo( bool,    QWidget* p = 0, const char* name = 0 );
+		US_Report_MonteCarlo( QString, QWidget* p = 0, const char* name = 0 );
+		~US_Report_MonteCarlo() {};
+
+		void generate( const QString& );
 
 	private:
-		US_Config *USglobal;
+		US_Config* USglobal;
 
-#ifdef WIN32
-  #pragma warning ( disable: 4251 )
-#endif
-
-		vector <QString> parameter_name;
-	
-#ifdef WIN32
-  #pragma warning ( default: 4251 )
-#endif
-
-	public slots:
-		void generate(QString);	
-	private slots:
-		void load();
-		void view(QString);
+		void load( void );
+		void view( const QString& );
 };
 #endif
 
