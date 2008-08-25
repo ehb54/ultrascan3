645a0ba0bf3545ce26cc400de19c40e7ff836ab2
dubbs <dubbs@4bb6f89d-cd66-4eab-8136-febab30699dd>
2008-08-25 17:47:53 +0000

    Remove unnecessary statemetn from  us_db_tbl_expdata main

--- develop/us_db_tbl_expdata/main.cpp
+++ develop/us_db_tbl_expdata/main.cpp
@@ -2,27 +2,29 @@
 #include "../include/us_register.h"
 #include <qapplication.h>
 
-int main (int argc, char **argv)
+int main ( int argc, char** argv )
 {
-	QApplication a(argc, argv);
-	US_Register *us_register;
-	us_register = new US_Register();
+	QApplication a( argc, argv );
+	
+	US_Register* us_register = new US_Register();
 
-	if(us_register->read())      //this is a correct file
+	if ( us_register->read() )   // Is this is a correct file?
 	{
 		delete us_register;
-		US_ExpData_DB *exp_db;
-		exp_db = new US_ExpData_DB();
-		exp_db->all_done = false;
-		exp_db->setCaption("Experimental Data Table");
+
+		US_ExpData_DB* exp_db = new US_ExpData_DB();
+
+		exp_db->setCaption( "Experimental Data Table" );
 		exp_db->show();
-		a.setMainWidget(exp_db);
-		a.setDesktopSettingsAware(false);
+		
+		a.setMainWidget( exp_db );
+		a.setDesktopSettingsAware( false );
 	}
-	else     // problem with license (either missing, incorrect or expired)
+	else     // Problem with license (either missing, incorrect or expired)
 	{
-		a.setMainWidget(us_register);
-		a.setDesktopSettingsAware(false);
+		a.setMainWidget( us_register );
+		a.setDesktopSettingsAware( false );
 	}
+
 	return a.exec();
 }
