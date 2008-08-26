bdb988860561fb9788fec40184c1a47f137e336d
dubbs <dubbs@4bb6f89d-cd66-4eab-8136-febab30699dd>
2008-08-26 04:54:21 +0000

    Incorporate us_tar and us_gzip in us_db_equil

--- develop/include/us_db_equil.h
+++ develop/include/us_db_equil.h
@@ -1,81 +1,93 @@
 #ifndef US_DB_EQUIL_H
 #define US_DB_EQUIL_H
 
-#include "us_db.h"
+#include "us_db_widgets.h"
 #include "us_equilreport.h"
 #include "us_db_runrequest.h"
+
 #include <qwidget.h>
 #include <qprogressdialog.h>
 
-class US_EXTERN US_DB_Equil : public US_DB
+class US_EXTERN US_DB_Equil : public US_DB_Widgets
 {
 	Q_OBJECT
-	
-	public:
-	
-		US_DB_Equil(QWidget *p=0, const char *name=0);
-		~US_DB_Equil();
-		
-		struct US_ExpResult exp_rst;		/*!< A struct US_ExpRsesult for storing experimental data result. */
-		int run_type, source_type;						/*! < 0 means from Hard Drive, 1 means from Database. */
-		bool from_HD, from_DB, query_flag, retrieve_flag, del_flag;
-		QPushButton	*pb_runrequest;			/*!< A PushButton connect to sel_runrequest(). */
-		QPushButton *pb_load;					/*!< A PushButton connect to load(). */
-		QPushButton *pb_load_db;				/*!< A PushButton connect to read_db(). */
-		QPushButton *pb_retrieve_db;			/*!< A PushButton connect to retrieve_db(). */
-		QPushButton *pb_save_db;				/*!< A PushButton connect to save_db(). */
-		QPushButton *pb_del_db;					/*!< A PushButton connect to del_db(). */
-		QPushButton *pb_investigator;			/*!< A PushButton connect to sel_investigator(). */
-		QPushButton *pb_help;					/*!< A PushButton connect to help(). */
-		QPushButton *pb_close;					/*!< A PushButton connect to quit(). */
-		QPushButton *pb_reset;					/*!< A PushButton connect to clear(). */
-		QPushButton *pb_display;				/*!< A PushButton connect to display(). */
-		QListBox *lb_result;						/*!< A ListBox shows a result filename list. */
-		QLabel *lbl_instr;						/*!< A label shows instruction. */
-		QLabel *lbl_expdata;						/*!< A label shows 'Experimental Data ID'. */
-		QLabel *lbl_expdt;						/*!< A label shows loaded data's expdata ID. */
-		QLabel *lbl_runrequest;					/*!< A Label shows selected runrequest Information. */
-		QLabel *lbl_invst;						/*!< A label shows loaded data's investigator ID. */
-		QLabel *lbl_date;							/*!< A label shows 'Date'. */
-		QLabel *lbl_dt;							/*!< A label shows loaded data's date. */
-		QLabel *lbl_description;				/*!< A label shows loaded 'Description:'. */
-		QLabel *le_dscrpt;						/*!< A Label shows loaded data's description. */
-			
-		QProgressDialog *pd;						/*!< A Progress Bar for submitting data to DB. */
-		QProcess *compress_proc;
-		QProcess *uncompress_proc;
-		int c_step, uc_step;
-		int border, spacing, xpos, ypos, X, Y, Xpos, Ypos, buttonw, buttonh;
-		Data_Control_W *data_control;
-		QString run_id, fileName, baseName, htmlDir, printerFile;
-		int DataID, *item_ExpdataID;
-		QString *item_Description, *display_Str;
-		int runrequestID;
 
-		public slots:
-		
-		void setup_GUI();
-		bool insertCompressData();
-		void endCompressProcess();
+	public:
+
+		US_DB_Equil( QWidget* p = 0, const char* name = 0 );
+		~US_DB_Equil() {};
+
+	private: 
+
+		// Widgets
+
+		QPushButton* pb_runrequest;
+		QPushButton* pb_load;
+		QPushButton* pb_load_db;
+		QPushButton* pb_retrieve_db;
+		QPushButton* pb_save_db;
+		QPushButton* pb_del_db;
+		QPushButton* pb_investigator;
+		QPushButton* pb_help;
+		QPushButton* pb_close;
+		QPushButton* pb_reset;
+		QPushButton* pb_display;
+
+		QListBox*    lb_result;
+
+		QLabel*      lbl_instr;
+		QLabel*      lbl_expdata;
+		QLabel*      lbl_expdt;
+		QLabel*      lbl_runrequest;
+		QLabel*      lbl_invst;		
+		QLabel*      lbl_date;
+		QLabel*      lbl_dt;
+		QLabel*      lbl_description;
+		QLabel*      le_dscrpt;
+
+		// Class variables 
+
+		struct US_ExpResult exp_rst;
+
+		int                 DataID;
+		int*                item_ExpdataID;
+		int                 runrequestID;
+                        
+		bool                from_HD;
+		bool                from_DB;
+		bool                query_flag;
+		bool                retrieve_flag;
+		bool                del_flag;
+
+		QString             run_id;
+		QString*            item_Description;
+
+		// Internal methods
+		void setup_GUI              ( void );
 		void endUncompressProcess();
-		void cleanCompressFile();
-		void sel_runrequest();
-		void update_runrequest_lbl(int);
-		void sel_investigator();
-		void update_investigator_lbl(QString, int );
-		void load_HD();
-		void save_db();
-		void query_db();
-		void retrieve_db();
-		void select_result(int);
-		void check_permission();
-		void delete_db(bool);
-		void reset();
-		void display();
-		void help();
-		void quit();
-		void closeEvent( QCloseEvent *e);
-		
+
+		void cleanCompressFile      ( void );
+		void closeEvent             ( QCloseEvent* );
+		bool create_tar             ( const QString&, const QString&, QStringList& );
+
+	private slots:
+
+		void load_HD                ( void );
+		void help                   ( void );
+		void quit                   ( void );
+		void query_db               ( void );
+		void save_db                ( void );
+		void retrieve_db            ( void );
+		void check_permission       ( void );
+		void select_result          ( int );
+		void reset                  ( void );
+		void display                ( void );
+		void sel_runrequest         ( void );
+		void sel_investigator       ( void );
+
+		void update_runrequest_lbl  ( int );
+		void update_investigator_lbl( QString, int );
+		void delete_db              ( bool );
 };
 
 #endif
--- develop/us_db_equil/main.cpp
+++ develop/us_db_equil/main.cpp
@@ -2,26 +2,28 @@
 #include "../include/us_register.h"
 #include <qapplication.h>
 
-int main (int argc, char **argv)
+int main ( int argc, char** argv )
 {
-	QApplication a(argc, argv);
-	US_Register *us_register;
-	us_register = new US_Register();
+	QApplication a( argc, argv );
+	
+	US_Register* us_register = new US_Register();
 
-	if(us_register->read())      //this is a correct file
+	if ( us_register->read() )      // Is this is a correct file?
 	{
 		delete us_register;
-		US_DB_Equil *equil_result_db;
-		equil_result_db = new US_DB_Equil;
-		equil_result_db->setCaption("Equilibrium Experimental Data Result Table");
+
+		US_DB_Equil* equil_result_db = new US_DB_Equil;
+		
+		equil_result_db->setCaption( "Equilibrium Experimental Data Result Table" );
 		equil_result_db->show();
-		a.setMainWidget(equil_result_db);
-		a.setDesktopSettingsAware(false);
+		
+		a.setMainWidget          ( equil_result_db );
+		a.setDesktopSettingsAware( false );
 	}
-	else     // problem with license (either missing, incorrect or expired)
+	else     // Problem with license (either missing, incorrect or expired)
 	{
-		a.setMainWidget(us_register);
-		a.setDesktopSettingsAware(false);
+		a.setMainWidget          ( us_register );
+		a.setDesktopSettingsAware( false );
 	}
 	return a.exec();
 }
