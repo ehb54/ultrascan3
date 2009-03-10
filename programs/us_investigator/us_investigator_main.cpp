b866cae1e053ea827a2bf4b1aa4cfdf3e6007d5b
dubbs <dubbs@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2009-03-10 21:49:57 +0000

    Rename files to us_investigator

--- programs/us_investigator/us_investigator_main.cpp
+++ programs/us_investigator/us_investigator_main.cpp
@@ -2,7 +2,7 @@
 
 #include <QApplication>
 
-#include "us_db_tbl_investigator.h"
+#include "us_investigator.h"
 #include "us_license_t.h"
 #include "us_license.h"
 
@@ -16,7 +16,7 @@ int main( int argc, char* argv[] )
    #include "main1.inc"
 
    // License is OK.  Start up.
-   US_DB_TblInvestigator* w = new US_DB_TblInvestigator;
+   US_Investigator* w = new US_Investigator;
    w->show();                  //!< \memberof QWidget
    return application.exec();  //!< \memberof QApplication
 }
