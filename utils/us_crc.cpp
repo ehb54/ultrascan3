09de16d531e2f6f60a9ffb3b9b5066d583edec3c
dubbs <dubbs@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2009-07-21 21:01:08 +0000

    Add crc functions

---
+++ utils/us_crc.cpp
@@ -0,0 +1,16 @@
+#include "us_crc.h"
+
+unsigned long US_Crc::crc32( 
+      unsigned long& crc, const unsigned char* buf, unsigned int len )
+{
+   if ( buf == 0 ) return 0UL;
+
+   crc = crc ^ 0xffffffffUL;
+   
+   if ( len ) do 
+   {
+     crc = crc_table[ ( (int)crc ^ ( *buf++ ) ) & 0xff ] ^ ( crc >> 8 );
+   } while ( --len );
+               
+   return crc ^ 0xffffffffUL;
+}
