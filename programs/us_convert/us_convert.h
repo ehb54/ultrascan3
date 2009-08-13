5b795458634db5b3073e4bfb66c21388bca9201e
zollarsd <zollarsd@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2009-08-13 16:12:46 +0000

    Added plot window, including excluding of individual scans

--- programs/us_convert/us_convert.h
+++ programs/us_convert/us_convert.h
@@ -3,19 +3,25 @@
 
 #include <QtGui>
 
+#include "qwt_plot_marker.h"
+
+#include "us_extern.h"
 #include "us_widgets.h"
 #include "us_help.h"
+#include "us_plot.h"
 #include "us_dataIO.h"
 
 class US_Convert : public US_Widgets
 {
-  Q_OBJECT
-  public:
-      US_Convert();
+	Q_OBJECT
+
+	public:
+		US_Convert();
 
 	private:
 
-      US_Help       showHelp;
+      US_Help        showHelp;
+      US_PlotPicker* pick;
 
       QString       runType;
 
@@ -25,28 +31,45 @@ class US_Convert : public US_Widgets
       QComboBox*    cb_channel;
       QComboBox*    cb_wavelength;
 
+      QwtCounter*   ct_from;
+      QwtCounter*   ct_to;
+
       QPushButton*  pb_write;
       QPushButton*  pb_writeAll;
+      QPushButton*  pb_exclude;
+      QPushButton*  pb_excludeRange;
+      QPushButton*  pb_include;
 
-      QTextEdit*    te_data;
+      QList< beckmanRaw > legacyData;          // legacy data from file
+      QList< beckmanRaw* > ccwLegacyData;      // legacy data with this cell/channel/wl
+      rawData     newRawData;                  // filtered legacy data in new raw format
 
-      QList< beckmanRaw > legacyData;
-
-      QProgressBar* progress;
+      QList< int >  includes;                  // list of points to include in plot
+      QwtPlot*      data_plot;
+      QwtPlotGrid*  grid;
 
       int  write           ( const QString& );
       void setInterpolated ( unsigned char*, int );
-
-  private slots:
-      void load     ( void );
-      void reset    ( void );
-
-      void write    ( void );
-
-
-
-      void writeAll ( void );
-      void help     ( void )
-      { showHelp.show_help( "manual/us_convert.html" ); };
+      void plot_current    ( void );
+      void init_includes   ( void );
+      void plot_all        ( void );
+      void replot          ( void );
+      void set_colors      ( const QList< int >& );
+   
+	private slots:
+      void load            ( void );
+      void reset           ( void );
+      void read            ( void );
+      void convert         ( void );
+      void focus_from      ( double );
+      void focus_to        ( double );
+      void focus           ( int, int );
+      void exclude_one     ( void );
+      void exclude_range   ( void );
+      void include         ( void );
+      void write           ( void );
+      void writeAll        ( void );
+		  void help            ( void )
+        { showHelp.show_help( "manual/us_convert.html" ); };
 };
 #endif
