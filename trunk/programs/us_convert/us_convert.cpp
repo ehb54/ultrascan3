1a2c16d9cce0fda57f35119a3eec6e8458537db4
dubbs <dubbs@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2009-08-28 18:16:28 +0000

    Convert US_Plot and associated classes to use US_WidgetsDialog

--- trunk/programs/us_convert/us_convert.cpp
+++ trunk/programs/us_convert/us_convert.cpp
@@ -677,9 +677,9 @@ void US_Convert::details( void )
 
    US_RunDetails* dialog
       = new US_RunDetails( allData, currentTriple, runID, dirname, triples );
-   dialog->show();
-   //qApp->processEvents();
-   //delete dialog;
+   dialog->exec();
+   qApp->processEvents();
+   delete dialog;
 }
 
 void US_Convert::changeCcw( int index )
