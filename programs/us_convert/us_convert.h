11319ae41557918eca9f988f2a66ed481372f51f
zollarsd <zollarsd@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2009-09-28 20:58:01 +0000

    Added pseudo absorbance conversion to RI  data

--- programs/us_convert/us_convert.h
+++ programs/us_convert/us_convert.h
@@ -11,7 +11,7 @@
 #include "us_plot.h"
 #include "us_dataIO.h"
 
-class US_Convert : public US_Widgets
+class US_EXTERN US_Convert : public US_Widgets
 {
   Q_OBJECT
 
@@ -19,6 +19,8 @@ class US_Convert : public US_Widgets
       US_Convert();
 
   private:
+
+      enum { SPLIT, REFERENCE, NONE } step;
 
       US_Help        showHelp;
       US_PlotPicker* picker;
@@ -47,11 +49,14 @@ class US_Convert : public US_Widgets
       QPushButton*  pb_details;
       QPushButton*  pb_define;
       QPushButton*  pb_process;
+      QPushButton*  pb_reference;
+      QPushButton*  pb_cancelref;
 
       QList< US_DataIO::beckmanRaw >  legacyData;     // legacy data from file
       QList< US_DataIO::beckmanRaw* > ccwLegacyData;  // legacy data with this cell/channel/wl
       US_DataIO::rawData              newRawData;     // filtered legacy data in new raw format
       QList< US_DataIO::rawData >     allData;        // all the data, separated by c/c/w
+      QList< US_DataIO::rawData >     RIData;         // to save RI data, after converting to RP
 
       QList< int >  includes;                         // list of points to include in plot
       QwtPlot*      data_plot;
@@ -61,7 +66,11 @@ class US_Convert : public US_Widgets
       QProgressBar* progress;
 
       QList< double > ss_limits;                      // list of subset boundaries
-      bool clicking;
+      double        reference_start;                  // boundary of reference scans
+      double        reference_end;
+      bool          RP_averaged;                      // true if RI averages have been done
+      int           RP_reference_triple;              // number of the triple that is the reference
+      QList< double > RP_averages;
 
       int  write           ( const QString& );
       void setInterpolated ( unsigned char*, int );
@@ -70,6 +79,7 @@ class US_Convert : public US_Widgets
       void plot_all        ( void );
       void replot          ( void );
       void set_colors      ( const QList< int >& );
+      void RP_calc_avg     ( void );
    
   private slots:
       void load            ( QString dir = "" );
@@ -90,9 +100,15 @@ class US_Convert : public US_Widgets
       void reset_scan_ctrls( void );
       void write           ( void );
       int  writeAll        ( void );
-      void start_clicking  ( void );
-      void click           ( const QPoint& );
-      void end_clicking    ( void );
+      void cClick          ( const QwtDoublePoint& );
+      void define_subsets  ( void );
+      void process_subsets ( void );
+      void cDrag           ( const QwtDoublePoint& );
+      void define_reference  ( void );
+      void start_reference   ( const QwtDoublePoint& );
+      void process_reference ( const QwtDoublePoint& );
+      void cancel_reference( void );
+      void draw_vline      ( double );
       void help            ( void )
         { showHelp.show_help( "manual/us_convert.html" ); };
 };
