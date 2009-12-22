98ad18698544b5372c3411d5824727f03ed44513
emre <ehb54@users.noreply.github.com>
2009-12-22 22:34:42 +0000

    SOMO / minor update / correctly process command line files...

--- develop/us_hydrodyn/main.cpp
+++ develop/us_hydrodyn/main.cpp
@@ -3,6 +3,7 @@
 
 int main (int argc, char **argv)
 {
+   QDir *dir = new QDir(QDir::currentDirPath());
    QApplication a(argc, argv);
    US_Register *us_register;
    us_register = new US_Register();
@@ -14,7 +15,7 @@ int main (int argc, char **argv)
       vector < QString > batch_file;
       for ( int i = 1; i < a.argc(); i++ ) 
       {
-         batch_file.push_back( a.argv()[i] );      
+         batch_file.push_back( dir->filePath(a.argv()[i]) );      
       }
       hydrodyn = new US_Hydrodyn(batch_file);
       hydrodyn->setCaption("SOMO Solution Modeler");
