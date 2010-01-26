dde2175081e0315b4fb2e75831b5d436f469ab5f
zollarsd <zollarsd@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2010-01-26 18:45:18 +0000

    Added dOxygen comments, added enum for db errors, new constructor

--- db/us_db2.h
+++ db/us_db2.h
@@ -8,19 +8,57 @@
 #include "us_extern.h"
 
 /*! \brief This class provides connectivity and convenience functions
-           for database access.
+           for database access in the US3 system. 
 */
 
 class US_EXTERN US_DB2
 {
   public:
 
+    /*! \brief Generic constructor for the US_DB2 class. Initialization is
+        performed but database connection is not attempted.
+    */
     US_DB2();
+
+    /*! \brief Constructor for the US_DB2 class. Initialization is performed
+        and connection is attempted. Connects to a database using the currently 
+        defined database stored by \ref US_Config and \ref US_Settings. This 
+        constructor uses an SSL connection.
+
+        \param masterPW Master password to decrypt DB password
+    */
+    US_DB2( const QString& );
+
+    /*! \brief Generic destructor for the US_DB2 class. Database connection 
+        is terminated and any resources in use are closed and deleted.
+    */
     ~US_DB2();
+
+    //! Some status codes returned by the US3 database
+    enum dbStatus
+    {
+      OK             = 0,       //! Query ok; no error
+      ERROR          = -1,      //! Unspecified error
+
+      NOT_CONNECTED  = 1,      //! Database system is not connected
+     
+      DUP_EMAIL      = 101,     //! Duplicate email addresses exist
+      NO_ACCT        = 102,     //! No account with that email address was found
+      INACTIVE       = 103,     //! The account has not been activated
+      BADPASS        = 104,     //! Incorrect password
+     
+      NOTPERMITTED   = 201,     //! User does not have permission 
+     
+      NOROWS         = 301,     //! No rows returned
+     
+      INSERTNULL     = 401,     //! Attempt to insert NULL value in a NOT NULL field
+      INSERTDUP      = 402      //! Attempt to insert a duplicate value in a primary
+                                //!    or unique key field
+    };
 
     // Methods
 
-    /*! \brief This function provides a quick test of database connectivity
+    /*! \brief Provides a quick test of database connectivity
         to ensure that the parameters in the database setup are correct.
         The database is merely opened and then immediately closed.
 
@@ -28,15 +66,15 @@ class US_EXTERN US_DB2
         \param dbname   The name of the database to access.
         \param user     The user name that can access the database.
         \param password The unencrypted password for the database/user.
-        \param error    A reference to a string for error responses as defined
-                        in the class QSqlError.
+        \param error    A reference to a string for error responses.
     */
     bool test_db_connection( const QString&, const QString&, 
-        const QString&, const QString&, QString& );
+                             const QString&, const QString&, 
+                             QString& );
 
-    /*! \brief This function provides a quick test of database connectivity
-        over an SSL connection.
-        The database is merely opened and then immediately closed.
+    /*! \brief Provides a quick test of database connectivity
+        over an SSL connection. The database is merely opened and 
+        then immediately closed.
 
         \param host     The name of the host of the database server.
         \param dbname   The name of the database to access.
@@ -44,36 +82,112 @@ class US_EXTERN US_DB2
         \param password The unencrypted password for the database/user.
         \param email    The email address of the investigator
         \param inv_pw   The investigator's password
-        \param error    A reference to a string for error responses as defined
-                        in the class QSqlError.
+        \param error    A reference to a string for error responses.
     */
     bool test_secure_connection( const QString&, const QString&, 
                                  const QString&, const QString&, 
                                  const QString&, const QString&, 
                                  QString& );
 
-    /*! \brief A function to open a database using the currently defined database
-               stored by \ref US_Config and \ref US_Settings.
+    /*! \brief Connects to a database using the currently defined database
+        stored by \ref US_Config and \ref US_Settings. This constructor 
+        uses an SSL connection.
 
         \param masterPW Master password to decrypt DB password
-        \param error    A reference to a string for error responses as defined
-                        in the class QSqlError.
+        \param error    A reference to a string for error responses.
     */
     bool          connect     ( const QString&, QString& );
+
+    /*! \brief Connects to a database using authentication parameters
+        supplied in the argument list.
+
+        \param host     The name of the host of the database server.
+        \param dbname   The name of the database to access.
+        \param user     The user name that can access the database.
+        \param password The unencrypted password for the database/user.
+        \param error    A reference to a string for error responses.
+    */
     bool          connect     ( const QString&, const QString&, 
                                 const QString&, const QString&, 
                                 QString& );
+
+    /*! \brief Implements a simple query on the active database 
+        connection, cleaning up any unused results from a previous query.
+
+        \param query    A string containing the US3 query to execute.
+    */
     void          rawQuery    ( const QString& );
+
+    /*! \brief Implements a query on the active database connection, 
+        cleaning up any unused results from a previous query. StatusQuery()
+        returns the integer status of the query from the US3 database,
+        but no other result data. Useful for INSERT, UPDATE and DELETE 
+        style queries.
+
+        \param query    A string containing the US3 query to execute.
+    */
     int           statusQuery ( const QString& );
+
+    /*! \brief Implements a general-purpose query on the active database 
+        connection, cleaning up any unused results from a previous query. 
+        Query() can be used for INSERT, UPDATE, or DELETE style queries, 
+        but statusQuery() is easier to use in those cases. With SELECT
+        queries, Query() retrieves the result set and prepares to return
+        individual values using the next() and value() methods. Use the 
+        numRows() method to find out how many rows there are.
+
+        \param query    A string containing the US3 query to execute.
+    */
     void          query       ( const QString& );
+
+    /*! \brief Fetches the next row in the result set, if one exists. 
+        Returns TRUE if the operation has been successful, or FALSE 
+        if there are no more rows. Use the value() function to 
+        retrieve the individual values in the row.
+    */
     bool          next        ( void );
+
+    /*! \brief Returns the value of one column in the current row of
+        the result set. This function can be called repeatedly to retrieve
+        values for all columns in the row. If there are n columns in
+        the row, the columns are numbered 0...n-1. Value() returns 
+        the QVariant::invalid type in cases where the data type is unknown,
+        or if the index is out of bounds. Use next() to advance the row pointer.
+
+        \param index   The column number in the current row. 
+    */
     QVariant      value       ( unsigned );
                   
+    /*! \brief Returns TRUE if the US3 system is connected to a database,
+        or returns FALSE otherwise.
+    */
     bool          isConnected ( void );
+
+    /*! \brief Returns the number of rows returned in the current result
+        set. 
+    */
     int           numRows     ( void );
 
+    /*! \brief Returns a text string containing the most recent error encountered
+        by the US3 database system. If a query did not result in an error,
+        lastError() might return a text string describing the previous error,
+        or it might return an empty string indicating that there was no error. If
+        you wish to determine if there has been an error, call lastErrno() first
+        and then call lastError() to find out what the error was.
+    */
     QString       lastError   ( void ) { return error;  };
+
+    /*! \brief Returns the integer status code of the most recent query performed
+        by the US3 database system. If a query did not result in an error, then 
+        lastErrno() will return 0. If the status code is not 0, then you can use the
+        status code itself to determine what went wrong, or you can call lastError()
+        for a text string describing the error. 
+    */
     int           lastErrno   ( void ) { return errno;  };
+
+    /*! \brief Returns the ID of the row resulting from the most recent INSERT or
+        UPDATE statement. 
+    */
     int           lastInsertID( void );
     
   private:
