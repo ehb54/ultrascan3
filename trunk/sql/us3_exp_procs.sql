020f71a2d6394800560b8aa84824c94757814d6d
zollarsd <zollarsd@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2010-03-05 22:23:44 +0000

    Adding stored routines

--- trunk/sql/us3_exp_procs.sql
+++ trunk/sql/us3_exp_procs.sql
@@ -8,15 +8,315 @@
 
 DELIMITER $$
 
--- INSERTs information for a new experiment
+-- Verifies that the user has permission to view or modify
+--  the specified experiment
+DROP FUNCTION IF EXISTS verify_experiment_permission$$
+CREATE FUNCTION verify_experiment_permission( p_guid         CHAR(36),
+                                              p_password     VARCHAR(80),
+                                              p_experimentID INT )
+  RETURNS TINYINT
+  READS SQL DATA
+
+BEGIN
+  DECLARE count_experiments INT;
+  DECLARE status            TINYINT;
+
+  CALL config();
+  SET status   = @ERROR;
+
+  SELECT COUNT(*)
+  INTO   count_experiments
+  FROM   experimentPerson
+  WHERE  experimentID = p_experimentID
+  AND    personID = @US3_ID;
+ 
+  IF ( verify_user( p_guid, p_password ) = @OK &&
+       count_experiments > 0 ) THEN
+    SET status = @OK;
+
+  ELSEIF ( verify_userlevel( p_guid, p_password, @US3_ADMIN ) = @OK ) THEN
+    SET status = @OK;
+
+  ELSE
+    SET @US3_LAST_ERRNO = @NOTPERMITTED;
+    SET @US3_LAST_ERROR = 'MySQL: you do not have permission to view or modify this experiment';
+
+    SET status = @NOTPERMITTED;
+
+  END IF;
+
+  RETURN( status );
+
+END$$
+
+-- Returns the count of experiments associated with p_ID
+--  If p_ID = 0, retrieves count of all experiments in db
+--  Regular user can only get count of his own experiments
+DROP FUNCTION IF EXISTS count_experiments$$
+CREATE FUNCTION count_experiments( p_guid         CHAR(36),
+                                   p_password     VARCHAR(80),
+                                   p_ID           INT )
+  RETURNS INT
+  READS SQL DATA
+
+BEGIN
+  
+  DECLARE count_experiments INT;
+
+  CALL config();
+  SET count_experiments = 0;
+
+  IF ( verify_userlevel( p_guid, p_password, @US3_ADMIN ) = @OK ) THEN
+    -- This is an admin; he can get more info
+    IF ( p_ID > 0 ) THEN
+      SELECT COUNT(*)
+      INTO   count_experiments
+      FROM   experimentPerson
+      WHERE  personID = p_ID;
+
+    ELSE
+      SELECT COUNT(*)
+      INTO   count_experiments
+      FROM   experimentPerson;
+
+    END IF;
+
+  ELSEIF ( verify_user( p_guid, p_password ) = @OK ) THEN
+    IF ( (p_ID != 0) && (p_ID != @US3_ID) ) THEN
+      -- Uh oh, can't do that
+      SET @US3_LAST_ERRNO = @NOTPERMITTED;
+      SET @US3_LAST_ERROR = 'MySQL: you do not have permission to view those experiments';
+     
+    ELSE
+      -- This person is asking about his own experiments
+      -- Ignore p_experimentID and return user's own
+      SELECT COUNT(*)
+      INTO   count_experiments
+      FROM   experimentPerson
+      WHERE  personID = @US3_ID;
+
+    END IF;
+    
+  END IF;
+
+  RETURN( count_experiments );
+
+END$$
+
+-- INSERTs a new experiment with the specified information
 DROP PROCEDURE IF EXISTS new_experiment$$
-CREATE PROCEDURE new_experiment( p_guid     CHAR(36),
-                                 p_password VARCHAR(80),
-                                 p_exp_type ENUM('velocity', 'equilibrium', 'other'),
-                                 p_rotorID INT,
-                                 p_date DATE,
-                                 p_label VARCHAR(80),
-                                 p_comment TEXT)
+CREATE PROCEDURE new_experiment ( p_guid         CHAR(36),
+                                  p_password     VARCHAR(80),
+                                  p_projectID    INT,
+                                  p_labID        INT,
+                                  p_instrumentID INT,
+                                  p_operatorID   INT,
+                                  p_rotorID      INT,
+                                  p_type         ENUM('velocity', 'equilibrium', 'other'),
+                                  p_runTemp      FLOAT,
+                                  p_label        VARCHAR(80),
+                                  p_comment      TEXT,
+                                  p_centrifugeProtocol TEXT )
+  MODIFIES SQL DATA
+
+BEGIN
+  DECLARE l_experimentID INT;
+
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+  SET @LAST_INSERT_ID = 0;
+ 
+  IF ( verify_user( p_guid, p_password ) = @OK ) THEN
+    INSERT INTO experiment SET
+      projectID          = p_projectID,
+      labID              = p_labID,
+      instrumentID       = p_instrumentID,
+      operatorID         = p_operatorID,
+      rotorID            = p_rotorID,
+      type               = p_type,
+      runTemp            = p_runTemp,
+      label              = p_label,
+      comment            = p_comment,
+      centrifugeProtocol = p_centrifugeProtocol ;
+
+    SET @LAST_INSERT_ID  = LAST_INSERT_ID();
+
+    INSERT INTO experimentPerson SET
+      experimentID = @LAST_INSERT_ID,
+      personID     = @US3_ID;
+
+  END IF;
+
+  SELECT @US3_LAST_ERRNO AS status;
+
+END$$
+
+-- UPDATEs an existing experiment with the specified information
+DROP PROCEDURE IF EXISTS update_experiment$$
+CREATE PROCEDURE update_experiment ( p_guid         CHAR(36),
+                                     p_password     VARCHAR(80),
+                                     p_experimentID INT,
+                                     p_projectID    INT,
+                                     p_labID        INT,
+                                     p_instrumentID INT,
+                                     p_operatorID   INT,
+                                     p_rotorID      INT,
+                                     p_type         ENUM('velocity', 'equilibrium', 'other'),
+                                     p_runTemp      FLOAT,
+                                     p_label        VARCHAR(80),
+                                     p_comment      TEXT,
+                                     p_centrifugeProtocol TEXT )
+  MODIFIES SQL DATA
+
+BEGIN
+
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+
+  IF ( verify_experiment_permission( p_guid, p_password, p_experimentID ) = @OK ) THEN
+    UPDATE experiment SET
+      projectID          = p_projectID,
+      labID              = p_labID,
+      instrumentID       = p_instrumentID,
+      operatorID         = p_operatorID,
+      rotorID            = p_rotorID,
+      type               = p_type,
+      runTemp            = p_runTemp,
+      label              = p_label,
+      comment            = p_comment,
+      centrifugeProtocol = p_centrifugeProtocol
+    WHERE experimentID   = p_experimentID;
+
+  END IF;
+      
+  SELECT @US3_LAST_ERRNO AS status;
+
+END$$
+
+-- Returns the experimentID, label, and type of all experiments associated with p_ID
+--  If p_ID = 0, retrieves information about all experiments in db
+--  Regular user can only get info about his own experiments
+DROP PROCEDURE IF EXISTS get_experiment_desc$$
+CREATE PROCEDURE get_experiment_desc ( p_guid     CHAR(36),
+                                       p_password VARCHAR(80),
+                                       p_ID       INT )
+  READS SQL DATA
+
+BEGIN
+
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+
+  IF ( verify_userlevel( p_guid, p_password, @US3_ADMIN ) = @OK ) THEN
+    -- This is an admin; he can get more info
+    IF ( count_experiments( p_guid, p_password, p_ID ) < 1 ) THEN
+      SET @US3_LAST_ERRNO = @NOROWS;
+      SET @US3_LAST_ERROR = 'MySQL: no rows returned';
+   
+      SELECT @US3_LAST_ERRNO AS status;
+
+    ELSE
+      SELECT @OK AS status;
+  
+      IF ( p_ID > 0 ) THEN
+        SELECT   e.experimentID, label, type
+        FROM     experiment e, experimentPerson p
+        WHERE    e.experimentID = p.experimentID
+        AND      p.personID = p_ID
+        ORDER BY label;
+   
+      ELSE
+        SELECT   e.experimentID, label, type
+        FROM     experiment e, experimentPerson p
+        WHERE    e.experimentID = p.experimentID
+        ORDER BY label;
+
+      END IF;
+
+    END IF;
+
+  ELSEIF ( verify_user( p_guid, p_password ) = @OK ) THEN
+    IF ( (p_ID != 0) && (p_ID != @US3_ID) ) THEN
+      -- Uh oh, can't do that
+      SET @US3_LAST_ERRNO = @NOTPERMITTED;
+      SET @US3_LAST_ERROR = 'MySQL: you do not have permission to view this experiment';
+     
+      SELECT @US3_LAST_ERRNO AS status;
+
+    ELSEIF ( count_experiments( p_guid, p_password, @US3_ID ) < 1 ) THEN
+      SET @US3_LAST_ERRNO = @NOROWS;
+      SET @US3_LAST_ERROR = 'MySQL: no rows returned';
+   
+      SELECT @US3_LAST_ERRNO AS status;
+
+    ELSE
+      -- Ok, user wants his own info
+      SELECT @OK AS status;
+
+      SELECT   e.experimentID, label, type
+      FROM     experiment e, experimentPerson p
+      WHERE    e.experimentID = p.experimentID
+      AND      p.personID = @US3_ID
+      ORDER BY label;
+
+    END IF;
+
+  END IF;
+
+END$$
+
+-- Returns a more complete list of information about one experiment
+DROP PROCEDURE IF EXISTS get_experiment_info$$
+CREATE PROCEDURE get_experiment_info ( p_guid         CHAR(36),
+                                       p_password     VARCHAR(80),
+                                       p_experimentID INT )
+  READS SQL DATA
+
+BEGIN
+  DECLARE count_experiments INT;
+
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+
+  SELECT     COUNT(*)
+  INTO       count_experiments
+  FROM       experiment
+  WHERE      experimentID = p_experimentID;
+
+  IF ( verify_experiment_permission( p_guid, p_password, p_experimentID ) = @OK ) THEN
+    IF ( count_experiments = 0 ) THEN
+      SET @US3_LAST_ERRNO = @NOROWS;
+      SET @US3_LAST_ERROR = 'MySQL: no rows returned';
+
+      SELECT @US3_LAST_ERRNO AS status;
+
+    ELSE
+      SELECT @OK AS status;
+
+      SELECT   projectID, labID, instrumentID, operatorID, rotorID, type,
+               runTemp, label, comment, centrifugeProtocol, personID
+      FROM     experiment e, experimentPerson ep
+      WHERE    e.experimentID = ep.experimentID
+      AND      e.experimentID = p_experimentID;
+
+    END IF;
+
+  ELSE
+    SELECT @US3_LAST_ERRNO AS status;
+
+  END IF;
+
+END$$
+
+-- DELETEs an experiment, plus information in related tables
+DROP PROCEDURE IF EXISTS delete_experiment$$
+CREATE PROCEDURE delete_experiment ( p_guid      CHAR(36),
+                                     p_password  VARCHAR(80),
+                                     p_experimentID INT )
   MODIFIES SQL DATA
 
 BEGIN
@@ -24,17 +324,17 @@ BEGIN
   SET @US3_LAST_ERRNO = @OK;
   SET @US3_LAST_ERROR = '';
 
-  IF ( verify_user( p_guid, p_password ) ) THEN
-    INSERT INTO experiment SET
-      type        = p_exp_type,
-      rotorID     = p_rotorID,
-      dateBegin   = p_date,
-      label       = p_label,
-      comment     = p_comment;
+  IF ( verify_experiment_permission( p_guid, p_password, p_experimentID ) = @OK ) THEN
 
-      SET @LAST_INSERT_ID = LAST_INSERT_ID();
+    DELETE FROM experimentPerson
+    WHERE experimentID = p_experimentID;
+
+    DELETE FROM experiment
+    WHERE experimentID = p_experimentID;
 
   END IF;
- 
+
+  SELECT @US3_LAST_ERRNO AS status;
+
 END$$
 
--- trunk/sql/us3_hardware_procs.sql
+++ trunk/sql/us3_hardware_procs.sql
@@ -8,6 +8,10 @@
 
 DELIMITER $$
 
+--
+-- Rotor procedures
+--
+
 -- Returns the count of all rotors in db
 DROP FUNCTION IF EXISTS count_rotors$$
 CREATE FUNCTION count_rotors ( p_guid     CHAR(36),
@@ -202,6 +206,280 @@ BEGIN
     END IF;
 
   END IF;
+
+END$$
+
+--
+-- Lab procedures
+--
+
+-- Returns the count of all labs in db
+DROP FUNCTION IF EXISTS count_labs$$
+CREATE FUNCTION count_labs ( p_guid     CHAR(36),
+                             p_password VARCHAR(80) )
+  RETURNS INT
+  READS SQL DATA
+
+BEGIN
+
+  DECLARE count_labs INT;
+
+  CALL config();
+  SET count_labs = 0;
+
+  IF ( verify_user( p_guid, p_password ) = @OK ) THEN
+    SELECT    COUNT(*)
+    INTO      count_labs
+    FROM      lab;
+
+  END IF;
+
+  RETURN( count_labs );
+
+END$$
+
+-- SELECTs names of all labs
+DROP PROCEDURE IF EXISTS get_lab_names$$
+CREATE PROCEDURE get_lab_names ( p_guid     CHAR(36),
+                                 p_password VARCHAR(80) )
+  READS SQL DATA
+
+BEGIN
+  DECLARE count_labs INT;
+
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+
+  IF ( verify_user( p_guid, p_password ) = @OK ) THEN
+    SELECT    COUNT(*)
+    INTO      count_labs
+    FROM      lab;
+
+    IF ( count_labs = 0 ) THEN
+      SET @US3_LAST_ERRNO = @NOROWS;
+      SET @US3_LAST_ERROR = 'MySQL: no rows returned';
+ 
+      SELECT @US3_LAST_ERRNO AS status;
+
+    ELSE
+      SELECT @OK AS status;
+
+      SELECT labID, name
+      FROM lab
+      ORDER BY name;
+ 
+    END IF;
+
+  END IF;
+
+END$$
+
+-- Returns a more complete list of information about one lab
+DROP PROCEDURE IF EXISTS get_lab_info$$
+CREATE PROCEDURE get_lab_info ( p_guid     CHAR(36),
+                                p_password VARCHAR(80),
+                                p_labID  INT )
+  READS SQL DATA
+
+BEGIN
+  DECLARE count_labs INT;
+
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+
+  SELECT     COUNT(*)
+  INTO       count_labs
+  FROM       lab
+  WHERE      labID = p_labID;
+
+  IF ( verify_user( p_guid, p_password ) = @OK ) THEN
+    IF ( count_labs = 0 ) THEN
+      SET @US3_LAST_ERRNO = @NOROWS;
+      SET @US3_LAST_ERROR = 'MySQL: no rows returned';
+
+      SELECT @US3_LAST_ERRNO AS status;
+
+    ELSE
+      SELECT @OK AS status;
+
+      SELECT   name, building, room
+      FROM     lab
+      WHERE    labID = p_labID;
+
+    END IF;
+
+  ELSE
+    SELECT @US3_LAST_ERRNO AS status;
+
+  END IF;
+
+END$$
+
+-- adds a new lab
+DROP PROCEDURE IF EXISTS add_lab$$
+CREATE PROCEDURE add_lab ( p_guid            CHAR(36),
+                           p_password        VARCHAR(80),
+                           p_name            TEXT,
+                           p_building        TEXT,
+                           p_room            TEXT )
+  MODIFIES SQL DATA
+
+BEGIN
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+  SET @LAST_INSERT_ID = 0;
+
+  IF ( verify_userlevel( p_guid, p_password, @US3_ADMIN ) = @OK ) THEN
+    INSERT INTO lab SET
+      name              = p_name,
+      building          = p_building,
+      room              = p_room,
+      dateUpdated       = NOW();
+
+    SET @LAST_INSERT_ID = LAST_INSERT_ID();
+
+  END IF;
+
+  SELECT @US3_LAST_ERRNO AS status;
+
+END$$
+
+--
+-- Instrument procedures
+--
+
+-- Returns the count of all instruments in db
+DROP FUNCTION IF EXISTS count_instruments$$
+CREATE FUNCTION count_instruments ( p_guid     CHAR(36),
+                                    p_password VARCHAR(80) )
+  RETURNS INT
+  READS SQL DATA
+
+BEGIN
+
+  DECLARE count_instruments INT;
+
+  CALL config();
+  SET count_instruments = 0;
+
+  IF ( verify_user( p_guid, p_password ) = @OK ) THEN
+    SELECT    COUNT(*)
+    INTO      count_instruments
+    FROM      instrument;
+
+  END IF;
+
+  RETURN( count_instruments );
+
+END$$
+
+-- SELECTs names of all instruments
+DROP PROCEDURE IF EXISTS get_instrument_names$$
+CREATE PROCEDURE get_instrument_names ( p_guid     CHAR(36),
+                                        p_password VARCHAR(80) )
+  READS SQL DATA
+
+BEGIN
+  DECLARE count_instruments INT;
+
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+
+  IF ( verify_user( p_guid, p_password ) = @OK ) THEN
+    SELECT    COUNT(*)
+    INTO      count_instruments
+    FROM      instrument;
+
+    IF ( count_instruments = 0 ) THEN
+      SET @US3_LAST_ERRNO = @NOROWS;
+      SET @US3_LAST_ERROR = 'MySQL: no rows returned';
+ 
+      SELECT @US3_LAST_ERRNO AS status;
+
+    ELSE
+      SELECT @OK AS status;
+
+      SELECT instrumentID, name
+      FROM instrument
+      ORDER BY name;
+ 
+    END IF;
+
+  END IF;
+
+END$$
+
+-- Returns a more complete list of information about one instrument
+DROP PROCEDURE IF EXISTS get_instrument_info$$
+CREATE PROCEDURE get_instrument_info ( p_guid     CHAR(36),
+                                       p_password VARCHAR(80),
+                                       p_instrumentID  INT )
+  READS SQL DATA
+
+BEGIN
+  DECLARE count_instruments INT;
+
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+
+  SELECT     COUNT(*)
+  INTO       count_instruments
+  FROM       instrument
+  WHERE      instrumentID = p_instrumentID;
+
+  IF ( verify_user( p_guid, p_password ) = @OK ) THEN
+    IF ( count_instruments = 0 ) THEN
+      SET @US3_LAST_ERRNO = @NOROWS;
+      SET @US3_LAST_ERROR = 'MySQL: no rows returned';
+
+      SELECT @US3_LAST_ERRNO AS status;
+
+    ELSE
+      SELECT @OK AS status;
+
+      SELECT   name, serialNumber
+      FROM     instrument
+      WHERE    instrumentID = p_instrumentID;
+
+    END IF;
+
+  ELSE
+    SELECT @US3_LAST_ERRNO AS status;
+
+  END IF;
+
+END$$
+
+-- adds a new instrument
+DROP PROCEDURE IF EXISTS add_instrument$$
+CREATE PROCEDURE add_instrument ( p_guid            CHAR(36),
+                                  p_password        VARCHAR(80),
+                                  p_name            TEXT,
+                                  p_serialNumber    TEXT )
+  MODIFIES SQL DATA
+
+BEGIN
+  CALL config();
+  SET @US3_LAST_ERRNO = @OK;
+  SET @US3_LAST_ERROR = '';
+  SET @LAST_INSERT_ID = 0;
+
+  IF ( verify_userlevel( p_guid, p_password, @US3_ADMIN ) = @OK ) THEN
+    INSERT INTO instrument SET
+      name              = p_name,
+      serialNumber      = p_serialNumber,
+      dateUpdated       = NOW();
+
+    SET @LAST_INSERT_ID = LAST_INSERT_ID();
+
+  END IF;
+
+  SELECT @US3_LAST_ERRNO AS status;
 
 END$$
 
