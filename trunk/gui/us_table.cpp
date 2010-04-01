d57b25b1c79ef3ffdc9af37f6509e7f863dd7b0c
dubbs <dubbs@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2010-04-01 03:55:48 +0000

    Add ability to manage extinction coefficients to us_analyte

---
+++ trunk/gui/us_table.cpp
@@ -0,0 +1,134 @@
+//! \file us_table.cpp
+#include "us_table.h"
+#include "us_gui_settings.h"
+
+US_Table::US_Table( QMap< double, double >& map, const QString& type,
+      QWidget* parent, Qt::WindowFlags f )
+   : US_WidgetsDialog( parent, f ), values( map )
+{
+   setPalette  ( US_GuiSettings::frameColor() );
+   setAttribute( Qt::WA_DeleteOnClose );
+   
+   local = values;
+
+   QVBoxLayout* main = new QVBoxLayout( this );
+   main->setSpacing         ( 2 );
+   main->setContentsMargins ( 2, 2, 2, 2 );
+
+   QHBoxLayout* edits = new QHBoxLayout;
+
+   QLabel* lb_wl = us_label( tr( "Wavelength:" ) );
+   edits->addWidget( lb_wl );
+
+   le_wl = us_lineedit();
+   connect( le_wl, SIGNAL( editingFinished() ), SLOT( edited() ) ); 
+   edits->addWidget( le_wl );
+
+   QLabel* lb_value = us_label( type );
+   edits->addWidget( lb_value );
+
+   le_value = us_lineedit();
+   connect( le_value, SIGNAL( editingFinished() ), SLOT( edited() ) ); 
+   edits->addWidget( le_value );
+
+   main->addLayout( edits );
+
+   // Create and fill in list widget
+   lw_table = us_listwidget();
+
+   main->addWidget( lw_table );
+   connect( lw_table, SIGNAL( itemSelectionChanged( void ) ), 
+                      SLOT  ( new_row             ( void ) ) );
+
+   connect( lw_table, SIGNAL( itemDoubleClicked   ( QListWidgetItem* ) ), 
+                      SLOT  ( delete_row          ( QListWidgetItem* ) ) );
+
+   QBoxLayout* buttons = new QHBoxLayout;
+
+   QPushButton* pb_cancel = us_pushbutton( tr( "Cancel" ) );
+   connect( pb_cancel, SIGNAL( clicked() ), SLOT( reject() ) );
+   buttons->addWidget( pb_cancel );
+
+   QPushButton* pb_accept = us_pushbutton( tr( "Accept" ) );
+   connect( pb_accept, SIGNAL( clicked() ), SLOT( done() ) );
+   buttons->addWidget( pb_accept );
+
+   main->addLayout( buttons );
+
+   update();
+}
+
+void US_Table::done( void )
+{
+   values = local;
+   close();
+}
+
+void US_Table::edited( void )
+{
+   if ( le_wl->text().isEmpty() || le_value->text().isEmpty() ) return;
+
+   double wl    = le_wl   ->text().toDouble();
+   double value = le_value->text().toDouble();
+
+   if ( wl < 190.0  || wl > 750.0 )
+   {
+      QMessageBox::warning( this,
+            tr( "Error" ),
+            tr( "The wavelength must be in the range of 190-750." ) );
+      return;
+   }
+
+   if ( value < 0.0 )
+   {
+      QMessageBox::warning( this,
+            tr( "Error" ),
+            tr( "The value entered cannot be negative." ) );
+      return;
+   }
+
+   local.insert( wl, value );
+   update();
+
+   lw_table->setCurrentRow( -1 );
+}
+
+void US_Table::update( void )
+{
+   lw_table->clear();
+
+   QList< double > keys = local.keys();
+   qSort( keys );
+
+   for ( int i = 0; i < keys.size(); i++ )
+   {
+      QString wavelength = QString::number( keys[ i ], 'f', 1 );
+      QString value      = QString::number( local[ keys[ i ] ], 'f', 4 );
+
+      lw_table->addItem( wavelength + " / " + value );
+   }
+}
+
+void US_Table::new_row( void )
+{
+   QStringList v = lw_table->currentItem()->text().split( " / " );
+   le_wl   ->setText( v[ 0 ] );
+   le_value->setText( v[ 1 ] );
+}
+
+void US_Table::delete_row( QListWidgetItem* item )
+{
+   int response = QMessageBox::question( this,
+         tr( "Delete Entry?" ),
+         tr( "Delete the current entry?" ),
+         QMessageBox::Yes, QMessageBox::Cancel );
+
+   if ( response == QMessageBox::Yes ) 
+   {
+      QStringList v = item->text().split( " / " );
+      local.remove( v[ 0 ].toDouble() );
+      delete item;
+   }
+}
+
+
---
+++ trunk/gui/us_table.h
@@ -0,0 +1,39 @@
+#ifndef US_TABLE_H
+#define US_ANALYTE_H
+
+#include <QtGui>
+
+#include "us_widgets_dialog.h"
+#include "us_extern.h"
+
+class US_EXTERN US_Table : public US_WidgetsDialog
+{
+	Q_OBJECT
+
+	public:
+		US_Table( QMap< double, double >&, const QString&, 
+            QWidget* = 0, Qt::WindowFlags = 0 );
+
+   signals:
+      //void valueChanged( QMap< double, double > );
+      //void valueAnalyteID( const QString& );
+      //void component     ( US_FemGlobal_New::SimulationComponent );
+
+   private:
+      QMap< double, double >& values;
+      QMap< double, double >  local;
+      QListWidget*            lw_table;
+
+      QLineEdit*              le_wl;
+      QLineEdit*              le_value;
+
+      void update    ( void );
+
+   private slots:
+      void done      ( void );
+      void edited    ( void );
+      void new_row   ( void );
+      void delete_row( QListWidgetItem* );
+};
+#endif
+
