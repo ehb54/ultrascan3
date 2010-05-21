5677441f286f539fc92a225544ae0ef4aca110bb
zollarsd <zollarsd@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2010-05-21 22:38:53 +0000

    Created US_DataIO2 class, which moves radius vector outside the readings class

---
+++ trunk/utils/us_dataIO2.h
@@ -0,0 +1,348 @@
+//! \file us_dataIO2.h
+#ifndef US_DATAIO2_H
+#define US_DATAIO2_H
+
+#include <QtCore>
+
+/*! The US_DataIO2 class provides data structures and static methods to
+    read and write experimental data.
+
+    For memory efficiency, there is only a single vector of radius
+    values that applies to all the scans. So for instance, if you have
+    an instance of RawData or EditedData like this:
+
+    EditedData* data;
+
+    you could refer to an individual scan reading value like this:
+
+    data->scanData[ scanIndex ].readings[ readingIndex ].value
+
+    and the corresponding radius value could be specified like this:
+
+    data->radius( readingIndex )
+
+    In wavelength data, the wavelength would be specified like this:
+
+    data->scanWavelength( readingIndex )
+
+*/
+
+class US_DataIO2
+{
+   public:
+
+      //!  A constant value that identifies the format of the auc raw data.
+      //!  Internal programs use this value to determine if the format of the
+      //!  file is known.
+      static const uint format_version = 2;
+
+      //! A class to aid in representation of radius/wavelength data
+      class XValue
+      {
+         public:
+         union
+         {
+            double radius;     //!< Distance from the center of rotation (in centimeters)
+            double wavelength; //!< Overloaded entry for W style data
+         };                    //!< Data union for above values
+
+         XValue( void ) { }
+         XValue( double value) { radius = value; }
+      };
+
+      //!  A single Reading of data from the centrifuge
+      class RawReading
+      {
+         public:
+         union
+         {
+            double radius;     //!< Distance from the center of rotation (in centimeters)
+            double wavelength; //!< Overloaded entry for W style data
+         } d;                  //!< Data union for above values
+         double value;   //!< Value of the sensor's reading
+         double stdDev;  //!< Standard deviation of the reading.  Doesn't exist for P data.
+      };
+
+      //!  A single Reading of centrifuge data in the new format
+      class Reading
+      {
+         public:
+         double value;   //!< Value of the sensor's reading
+         double stdDev;  //!< Standard deviation of the reading.  Doesn't exist for P data.
+      };
+
+      /*!  This is the structure of a Beckman raw data file.  The file
+       *   types are:
+       *   - I intensity
+       *   - P interference
+       *   - R absorbance
+       *   - W multi-wavelength
+       *   - F fluorescence
+       *
+       *   For multi wavelength data, the wavelength and radius data are in
+       *   opposite positions.
+       */
+
+      class BeckmanRawScan
+      {
+         public:
+         QString description; //!<First line of data file
+         char    type;        //!< I P R W F  (1st Element of 2nd line)
+         char    channel;     //!< Channel ('A', 'B', etc) of scan data
+         int     cell;        //!< Cell (hole) of rotor for this data
+         double  temperature; //!< Temperature while acquiring data (C)
+         double  rpm;         //!< RPM of rotor for this file
+         double  seconds;     //!< Time elapsed since start of run
+         double  omega2t;     //!< Calculated integration of w2t since start of run
+         union
+         {
+            double radius;     //!< Distance from the center of rotation (in centimeters)
+            double wavelength; //!< Overloaded entry for W style data
+         } t;                  //!< Data union for above values
+         int     count;        //!< Actual number of Readings for this scan
+
+         QVector< RawReading > readings;  //!< The scan readings
+      };
+
+      //!  The scanned data in the UltraScan III format and the specified
+      //!  plateau after editing.  The Beckman data is interpolated so there
+      //!  are no missing entries.  
+      class Scan
+      {
+         public:
+         double temperature;     //!< Temperature while acquiring data (C)
+         double rpm;             //!< RPM of rotor for this scan
+         double seconds;         //!< Time elapsed since start of run
+         double omega2t;         //!< Calculated integration of w2t since start of run
+         double wavelength;      //!< Wavlength setting of optical system
+         double plateau;         //!< Reading value
+         double delta_r;         //!< Radial distance between Readings
+         QVector< Reading > readings; //!< The scan readings
+         //! A bit array.  One bit for each Reading.  0 is an actual Reading and
+         //! 1 is an interpolated Reading.
+         QByteArray interpolated; 
+      };
+
+      //!  All data associated with a cell / channel / wavelength (CCW)
+      class RawData
+      {
+         public:
+         char    type[ 2 ];         //!< Data type: "RA", "IP", "RI", "FI"
+                                    //!< "WA" << "WI";
+
+         char    guid[ 16 ];        //!< A generated globally unique identifier
+         int     cell;              //!< Cell (hole) of rotor for this data
+         char    channel;           //!< Channel ('A', 'B', etc) of scan data
+         QString description;       //!< Descriptive data taken from \ref BeckmanRawScan
+
+         QVector< XValue > x;       //!< Wavelength or radius information
+         QVector< Scan > scanData;  //!<  The collections of scans for the CCW
+         double radius ( int i )    //!< a couple of methods for ease of use
+            { return x[ i ].radius; }
+         double scanWavelength( int i ) 
+            { return x[ i ].wavelength; } 
+      };
+
+      //! Holds changes made to a scan value.  Created by the data editor and
+      //! applied when Reading data for analysis.
+      class EditedPoint
+      {
+         public:
+         int    scan;    //!< The index of the scan
+         double radius;  //!< The radius of the value to be changed
+         double value;   //!< The new value
+      };
+
+      //! A class that holds all entries assiciated with the edits of
+      //! a scan.
+      class EditValues
+      {
+         public:
+         //! A constructor that provides initial values for scan edits
+         EditValues()
+         {
+            excludes    .clear();
+            editedPoints.clear();
+            invert       = 1.0;
+            removeSpikes = false;
+            floatingData = false;
+            noiseOrder   = 0;
+
+            //! Air Gap is only for interference data
+            airGapLeft   = 0.0;
+            airGapRight  = 9.0;
+         };
+
+         QString              runID;       //!< Specified runID of raw data
+         QString              cell;        //!< Cell (hole) of rotor for this data
+         QString              channel;     //!< Channel ('A', 'B', etc) of scan data
+         QString              wavelength;  //!< Wavlength setting of optical system
+         QString              uuid;        //!< A globally uniquie ID
+         double               meniscus;    //!< Designated radius of meniscus
+         double               airGapLeft;  //!< Start of air gap for interference data
+         double               airGapRight; //!< End of air gap for interfence data
+         double               rangeLeft;   //!< Minimum radius for valid data
+         double               rangeRight;  //!< Maximum radius for valid data
+         double               plateau;     //!< A radius value
+         double               baseline;    //!< Designated baseline value of data
+         QList< int >         excludes;    //!< A list of scans excluded from the raw data
+         QList< EditedPoint > editedPoints; //!< A list of points specifically changed
+         int                  noiseOrder;   //!< The order of the polynomial for noise removal
+         double               invert;       //!< A setting to invert the sign of the data.
+                                            //!< Valid values are 1.0 and -1.0
+         bool                 removeSpikes; //!< A setting designating whether the spike
+                                            //!< removal algorithm should be run
+         bool                 floatingData; //!< A value indicating that the density of
+                                            //!< the analyte is less than the density of
+                                            //!< the buffer.
+      };
+
+      //! The CCW data after edits are applied
+      class EditedData
+      {
+         public:
+         QString       runID;       //!< Specified runID of raw data
+         QString       editID;      //!< The identifier of the file that contained
+                                    //!< the edits for the data
+         QString       dataType;    //!< Sensor type
+         QString       cell;        //!< Cell (hole) of rotor for this data
+         QString       channel;     //!< Channel ('A', 'B', etc) of scan data
+         QString       wavelength;  //!< Wavlength setting of optical system
+         QString       description; //!< ASCII description of the data
+         QString       uuid;        //!< A globally uniquie ID
+         double        meniscus;    //!< Designated radius of meniscus
+         double        plateau;     //!< Location of maximum value of data.  A radius value.
+         double        baseline;    //!< Designated baseline value of data
+         bool          floatingData; //!< A value indicating that the density of
+                                     //!< the analyte is less than the density of
+                                     //!< the buffer.
+         QVector< XValue > x;        //!< Wavelength or radius information
+         QVector< Scan > scanData;   //!< The actual data.  The interpolated data 
+                                     //!< array is omitted
+         double radius ( int i )     //!< a couple of methods for ease of use
+            { return x[ i ].radius; }
+         double scanWavelength( int i ) 
+            { return x[ i ].wavelength; } 
+      };
+
+      //! The CCW data after edits are applied
+      class NewEditedData
+      {
+         public:
+         QString       runID;       //!< Specified runID of raw data
+         QString       editID;      //!< The identifier of the file that contained
+                                    //!< the edits for the data
+         QString       dataType;    //!< Sensor type
+         QString       cell;        //!< Cell (hole) of rotor for this data
+         QString       channel;     //!< Channel ('A', 'B', etc) of scan data
+         QString       wavelength;  //!< Wavlength setting of optical system
+         QString       description; //!< ASCII description of the data
+         QString       uuid;        //!< A globally uniquie ID
+         double        meniscus;    //!< Designated radius of meniscus
+         double        plateau;     //!< Location of maximum value of data.  A radius value.
+         double        baseline;    //!< Designated baseline value of data
+         bool          floatingData; //!< A value indicating that the density of
+                                     //!< the analyte is less than the density of
+                                     //!< the buffer.
+         QVector< Scan > scanData;   //!< The actual data.  The interpolated data 
+                                     //!< array is omitted
+      };
+
+      //! Values that are returned for various errors when managing scan data
+      enum ioError { OK, CANTOPEN, BADCRC, NOT_USDATA, BADTYPE, BADXML, 
+                     NODATA, NO_UUID_MATCH, BAD_VERSION };
+
+      /*! Read a set of legacy data in raw Beckman data set format
+          \param file  The filename to be read
+          \param data  A reference to the data structure location for the read data
+      */
+      static bool    readLegacyFile( const QString&, BeckmanRawScan& );
+      
+      /*! Write a set of data to a file in the US3 binary format
+          \param file  The filename to be read
+          \param data  A reference to the data structure of
+                       th edata to be written
+      */
+      static int     writeRawData  ( const QString&, RawData& );
+      
+      /*! Read a set of data in the US3 binary format
+          \param file  The filename to be read
+          \param data  A reference to the data structure for the read data
+      */
+      static int     readRawData   ( const QString&, RawData& );
+
+      /*! Read a set of edit parameters in xml format
+          \param filename   The filename to be read
+          \param parameters A reference to the data structure for the read data
+      */
+      static int     readEdits     ( const QString&, EditValues& );
+
+      /*! A string describing the last read or write error
+          \param code  The error code to be described
+      */
+      static QString errorString   ( int );
+
+      /*! Determine the index of a radius in a scan
+          \param s A reference of the scan to be searched
+          \param radii A parallel vector of corresponding radius information
+          \param r The radius where the index is needed
+      */
+      static int     index         ( const Scan&, const QVector< XValue >&, double );
+
+
+      /*! Load edited data into a data structure.  This is the method most
+          analysis programs will call to read data into memory.  It uses
+          the functions readRawData and readEdits.
+          \param directory    The directory of the auc files
+          \param editFilename The the file with the edited parameters
+          \param data         The location where the edited data is placed
+          \param raw          The location where the raw data is placed
+      */
+
+      static int     loadData      ( const QString&, const QString&, 
+                                     QVector< EditedData >&, QVector< RawData >& );
+
+      /*! This is an overloaded method to read data into memory.  It uses
+          the functions readRawData and readEdits.
+          \param directory    The directory of the auc files
+          \param editFilename The the file with the edited parameters
+          \param data         The location where the edited data is placed
+      */
+
+      static int     loadData      ( const QString&, const QString&, 
+                                     QVector< EditedData >& ); 
+
+   private:
+
+      //!  \private A private convenience class
+      class Parameters
+      {
+         public:
+         double min_data1;
+         double max_data1;
+         double min_data2;
+         double max_data2;
+      };
+
+      static void writeScan( QDataStream&, const Scan&, quint32&, 
+                             const Parameters& );
+
+      static void write( QDataStream&, const char*, int, quint32& );
+      static void read ( QDataStream&,       char*, int, quint32& );
+      
+      static void ident     ( QXmlStreamReader&, EditValues& );
+      static void run       ( QXmlStreamReader&, EditValues& );
+      static void params    ( QXmlStreamReader&, EditValues& );
+      static void operations( QXmlStreamReader&, EditValues& );
+      static void do_edits  ( QXmlStreamReader&, EditValues& );
+      static void excludes  ( QXmlStreamReader&, EditValues& );
+
+      static void copyRange    ( double, double, const Scan&, Scan&, 
+                                 const QVector< XValue >& );
+      static void copyxRange   ( double, double, const Scan&, 
+                                 const QVector< XValue >&, QVector< XValue >& );
+      static bool spike_check  ( const Scan&, const QVector< XValue >&, 
+                                 int, int, int, double* );
+      static QList< double > calc_residuals( int, const QVector< Scan >& );
+};
+#endif
---
+++ trunk/utils/us_math2.h
@@ -0,0 +1,164 @@
+//! \brief us_math.h
+#ifndef US_MATH_H
+#define US_MATH_H
+
+#include <QtCore>
+#include "us_extern.h"
+#include "us_dataIO2.h"
+
+#define sq(x) ((x) * (x))
+#ifndef max
+   #define max(a,b) (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+   #define min(a,b) (((a) < (b)) ? (a) : (b))
+#endif
+
+#ifndef swap_double
+   // No need to protect a and b for swap
+   #define swap_double(a,b) {double t = a; a=b; b=t;}
+#endif
+
+//! \brief A collecion of methematical routines.  All functions are static.
+class US_EXTERN US_Math2
+{
+   public:
+      //! A structure used for calculating a peptide sequence
+      class Peptide
+      {
+         public:
+         uint   a;        //!< alanine
+         uint   b;        //!< M or D
+         uint   c;        //!< cysteine
+         uint   d;        //!< aspartic acid
+         uint   e;        //!< glutamic acid
+         uint   f;        //!< phenylalanine
+         uint   g;        //!< glycine
+         uint   h;        //!< histidine
+         uint   i;        //!< isoleucine
+         uint   j;        //!< Hao (James Nowick)
+         uint   k;        //!< lysine
+         uint   l;        //!< leucine
+         uint   m;        //!< methionine
+         uint   n;        //!< asparagine
+         uint   o;        //!< delta-linked ornithin (James Nowick)
+         uint   p;        //!< proline
+         uint   q;        //!< glutamine
+         uint   r;        //!< arginine
+         uint   s;        //!< serine
+         uint   t;        //!< threonine
+         uint   u;        //!< currently unused
+         uint   v;        //!< valine
+         uint   w;        //!< tryptophan
+         uint   x;        //!< unknown
+         uint   y;        //!< tyrosine
+         uint   z;        //!< E or  Q
+         uint   dab;      //!< diaminobutyric acid (John Kulp), symbol: "+"
+         uint   dpr;      //!< diaminopropanoic acid (John Kulp), symbol: "@"
+         double vbar;     //!< partial specific volume
+         double vbar20;   //!< partial specific volume at 20 deg.
+         double mw;       //!< molecular weight
+         double vbar_sum; //!< vbar sum
+         double weight;   //!< weight
+         double e280;     //!< extinction coefficient at 280 nm
+         uint   residues; //!< residues     
+      };
+
+      //! A structure used for holding solution data
+      class SolutionData
+      {
+         public:
+         double density;       //!< density
+         double density_wt;    //!< density wt
+         double density_tb;    //!< density tb
+         double viscosity;     //!< viscosity
+         double viscosity_wt;  //!< viscosity wt
+         double viscosity_tb;  //!< viscosity tb
+         double buoyancyb;     //!< buoyancy b
+         double buoyancyw;     //!< buoyancy w
+         double vbar;          //!< partial specific volume
+         double vbar20;        //!< partial specific volume at 20 deg.
+         double correction;    //!< correction
+      };
+
+
+      //! \brief Normal random variate generator
+      //! \param m - mean
+      //! \param s - standard deviation
+      static double  box_muller( double, double );
+
+      //! \brief Return a random floating point number
+      static double  ranf      ( void );      
+
+      //! \brief Given a line as a set of x and y coordinates, calulate
+      //!        the line's characteristics
+      //!       
+      //! \param x Pointer to an array of x values
+      //! \param y Pointer to an array of y values
+      //! \param slope       Pointer to location to retrun the line's slope
+      //! \param intercept   Pointer to location to retrun the line's y intercept
+      //! \param sigma       Pointer to location to retrun the standard deviation 
+      //! \param correlation Pointer to location to retrun the correlation
+      //! \param arraysize   Input array size
+      static double linefit   ( double**, double**, double*, double*, double*, 
+                                double* , int );
+
+      //! \brief A routine to calulate the vbar and MW of a peptide sequence
+      //! \param pep      The structure to be populated
+      //! \param sequence The secuence used for the calculations
+      //! \param temperature The temperature to use for calulations
+
+      static void calc_vbar( Peptide&, const QString&, double );
+
+      //! Adjust the vbar for temperature.  The values used in the 
+      //! unadjusted calculations are based on 25 degrees C.  We 
+      //! use an observed linear adjustment of 0.000425 per degreee.
+      
+      //! \param vbar The unadjusted vbar value
+      //! \param degC The temperature used for the adjustment
+      static double adjust_vbar20( double vbar, double degC )
+      { return vbar + 0.002125 + 4.25e-4 * ( degC - 25.0 ); }
+
+      //! The inverse of adjust_vbar20.  Returns vbar20.
+      
+      //! \param vbar The vbar value at the specified temperature
+      //! \param degC The temperature of the sample associated with vbar
+      static double adjust_vbar( double vbar, double degC )
+      { return vbar - 0.002125 - 4.25e-4 * ( degC - 25.0 ); }
+
+/*  
+      //! \brief An overloaded version of the above function with floats 
+      //!        instead of doubles.
+      //!       
+      //! \param x Pointer to an array of x values
+      //! \param y Pointer to an array of y values
+      //! \param slope       Pointer to location to retrun the line's slope
+      //! \param intercept   Pointer to location to retrun the line's y intercept
+      //! \param sigma       Pointer to location to retrun the standard deviation 
+      //! \param correlation Pointer to location to retrun the correlation
+      //! \param arraysize   Input array size
+      static float  linefit   ( float** , float** , float* , float* , float* , 
+                              float*  , int );
+*/
+      //! \brief Correct buffer data for temperature
+      //! \param t  Temperture of solution
+      //! \param d  Data to be corrected
+
+      static void data_correction( double, SolutionData& );
+
+      //! \brief A routine to calulate the value of a normally distrubted
+      //!        value.
+      //! \param sigma Standard deviation of the distribution
+      //! \param mean  Mean value of the distribution
+      //! \param x     Point where to compute desired value
+      static double normal_distribution( double, double, double );
+
+      //! \brief Calculate the time correction in a run due to acceleration
+      //!        of the rotor.
+      //! \param dataList The list of editedData for the run
+
+      static double time_correction( const QVector< US_DataIO2::EditedData >& );
+};
+#endif
+
