baedd4e66127141ca3080d41715925cc001848d9
dubbs <dubbs@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2010-05-23 00:07:29 +0000

    Add template for us_associations

--- trunk/programs/us_model_gui/modelgui.pro
+++ trunk/programs/us_model_gui/modelgui.pro
@@ -4,11 +4,13 @@ TARGET       = us_model
 DESTDIR      = ../../bin
 
 # Input
-SOURCES     += main.cpp          \
-               us_properties.cpp \
+SOURCES     += main.cpp            \
+               us_properties.cpp   \
+               us_associations.cpp \
                us_model_editor_new2.cpp
 
-HEADERS     += us_modelgui.h     \
-               us_properties.h   \
+HEADERS     += us_modelgui.h       \
+               us_properties.h     \
+               us_associations.h   \
                us_model_editor_new2.h
 
---
+++ trunk/programs/us_model_gui/us_associations.cpp
@@ -0,0 +1,56 @@
+//! \file us_assiciations_gui.cpp
+
+#include "us_associations.h"
+#include "us_gui_settings.h"
+#include "us_settings.h"
+#include "us_constants.h"
+
+US_AssociationsGui::US_AssociationsGui( 
+      US_FemGlobal_New::ModelSystem& current_model )
+   : US_WidgetsDialog( 0, 0 ), model( current_model )
+{
+   setWindowTitle   ( "UltraScan Model Assiciations" );
+   setPalette       ( US_GuiSettings::frameColor() );
+   setWindowModality( Qt::WindowModal );
+   
+   // Very light gray
+   QPalette gray = US_GuiSettings::editColor();
+   gray.setColor( QPalette::Base, QColor( 0xe0, 0xe0, 0xe0 ) );
+
+   QGridLayout* main = new QGridLayout( this );
+   main->setContentsMargins( 2, 2, 2, 2 );
+   main->setSpacing        ( 2 );
+
+   int row = 0;
+   
+   // Start widgets
+   // Models List Box
+   lw_analytes = new US_ListWidget;
+
+   //connect( lw_models, SIGNAL( currentRowChanged( int  ) ),
+   //                    SLOT  ( change_model     ( int  ) ) );
+
+   //connect( lw_models, SIGNAL( itemDoubleClicked( QListWidgetItem* ) ),
+   //                    SLOT  ( select_model     ( QListWidgetItem* ) ) );
+
+   main->addWidget( lw_analytes, row, 0, 5, 2 );
+   row += 5;
+
+
+   // Pushbuttons
+   QBoxLayout* buttonbox = new QHBoxLayout;
+
+   QPushButton* pb_help = us_pushbutton( tr( "Help") );
+   connect( pb_help, SIGNAL( clicked() ), SLOT( help()) );
+   buttonbox->addWidget( pb_help );
+
+   QPushButton* pb_close = us_pushbutton( tr( "Cancel") );
+   buttonbox->addWidget( pb_close );
+   connect( pb_close, SIGNAL( clicked() ), SLOT( close() ) );
+
+   QPushButton* pb_accept = us_pushbutton( tr( "Accept") );
+   buttonbox->addWidget( pb_accept );
+   //connect( pb_accept, SIGNAL( clicked() ), SLOT( accept_associations()) );
+
+   main->addLayout( buttonbox, row++, 0, 1, 4 );
+}
