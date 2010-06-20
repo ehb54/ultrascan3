63aa90c8722a51e5ca446571f790cd94fbc8aab2
dubbs <dubbs@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2010-06-20 23:27:33 +0000

    update sim defaults

--- trunk/utils/us_simparms.cpp
+++ trunk/utils/us_simparms.cpp
@@ -7,7 +7,7 @@ US_SimulationParameters::US_SimulationParameters()
    mesh_radius.clear(); 
    speed_step.clear();
 
-   simpoints         = 1400;  
+   simpoints         = 200;  
    meshType          = ADAPTIVE;
    gridType          = MOVING;
    radial_resolution = 0.001;
@@ -18,17 +18,17 @@ US_SimulationParameters::US_SimulationParameters()
    rinoise           = 0.0;     
    rotor             = 0;      
    band_forming      = false;   
-   band_volume       = 15.0;   
+   band_volume       = 0.0015;   
 }
 
 US_SimulationParameters::SpeedProfile::SpeedProfile()
 {
-   duration_hours    = 5;
+   duration_hours    = 0;
    duration_minutes  = 0;
    delay_hours       = 0;
    delay_minutes     = 0;
-   scans             = 30;
+   scans             = 0;
    acceleration      = 400;
-   rotorspeed        = 50000;
+   rotorspeed        = 0;
    acceleration_flag = true;
 }
