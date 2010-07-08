7ad660cb83fe19fdeed2115fe43ff6c78e1cbd80
Gary E. Gorbet <gegorbet@gmail.com>
2010-07-08 18:29:41 +0000

    Added static and nonstatic functions to apply noise to an EditedData set.

--- utils/us_noise.h
+++ utils/us_noise.h
@@ -4,6 +4,7 @@
 #include <QtCore>
 #include "us_extern.h"
 #include "us_db2.h"
+#include "us_dataIO2.h"
 
 class US_EXTERN US_Noise
 {
@@ -43,7 +44,7 @@ class US_EXTERN US_Noise
 
 
       //! An overloaded function to read a noise vector from the disk
-      //! \param filename The name, including full path, of the analyte file
+      //! \param filename The name, including full path, of the noise XML file
       //! \returns        The \ref US_DB2 return code for the operation
       int load( const QString& );  
       
@@ -54,8 +55,8 @@ class US_EXTERN US_Noise
       //! Write a noise vector to the disk or database
       //! \param db_access A flag to indicate if the DB (true) or disk (false)
       //!                  should be used to save the noise vector
-      //! \param filename  The filename (with path) where the xml file
-      //!                  be written if disk access is specified
+      //! \param filename  The filename (with path) where the xml file is
+      //!                  to be written if disk access is specified
       //! \param db        For DB access, pointer to an open database connection
       //! \returns         The \ref US_DB2 return code for the operation
       int write( bool, const QString&, US_DB2* = 0 );
@@ -70,6 +71,29 @@ class US_EXTERN US_Noise
       //! \returns         The \ref US_DB2 return code for the operation
       int write( const QString& );
 
+      //! Apply a noise vector to an EditedData set.
+      //! \param editdata  Reference to EditedData set to apply to.
+      //! \param remove    Flag to remove (true) or add back (false) noise.
+      //! \returns         Noise apply status (< 0 if error):
+      //!                    0  iff  noise subtracted/added.
+      //!                    1  iff  null noise or noise count==0.
+      //!                   -1  iff  noise count does not match readings count
+      //!                   -2  iff  noise count does not match scan count
+      int apply_to_data( US_DataIO2::EditedData&, bool = true );
+
+      //! Static function to apply a noise vector to an EditedData set.
+      //! \param editdata  Reference to EditedData set to apply to.
+      //! \param noise     Pointer to noise to apply to data (ti or ri).
+      //! \param remove    Flag to remove (true) or add back (false) noise.
+      //! \returns         Noise apply status (< 0 if error):
+      //!                    0  iff  noise subtracted/added.
+      //!                    1  iff  null noise or noise count==0.
+      //!                   -1  iff  noise count does not match readings count
+      //!                   -2  iff  noise count does not match scan count
+      static int apply_noise( US_DataIO2::EditedData&, US_Noise* = 0,
+            bool = true );
+
+      //! Static function to find and, if need be, create noise directory path.
       //! \param path A reference to the full disk path for the directory
       //!             into which to write the noise vector file.
       //! \returns    Success if the path is found or created and failure
