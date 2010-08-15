6af6899d991d7560cc3d3a7b4e230f3a1a30dc31
Gary E. Gorbet <gegorbet@gmail.com>
2010-08-15 03:26:27 +0000

    Three new utility functions. Expanded doxygen comments.

--- utils/us_util.cpp
+++ utils/us_util.cpp
@@ -1,6 +1,8 @@
+//! \file us_util.cpp
 #include "us_util.h"
 #include <uuid/uuid.h> 
 
+// get next token in a string, as defined by a separator; update for next pass
 QString US_Util::getToken( QString& s, const QString& separator )
 {
    QString token;
@@ -33,11 +35,116 @@ QString US_Util::getToken( QString& s, const QString& separator )
    return token;
 }
 
+// generate a new GUID value and return it in QString form
 QString US_Util::new_guid( void )
 {
-   uchar c[ 16 ];  // Binary
-   char  u[ 37 ];  // String
-   uuid_generate( c );
-   uuid_unparse ( c, u );
-   return QString( u );
+   uchar c[ 16 ];           // Binary
+   char  u[ 37 ];           // Char array
+
+   uuid_generate( c );      // generate a new UUID in binary form
+
+   uuid_unparse ( c, u );   // unparse it the standard character form
+
+   return QString( u );     // return a string representation
 }
+
+// calculate the md5hash and size of a named file (full path)
+QString US_Util::md5sum_file( QString filename )
+{
+   QFile f( filename );
+
+   // if unable to open the file, return "hash size" of "0 0"
+   if ( ! f.open( QIODevice::ReadOnly ) )
+      return "0 0";
+
+   // otherwise, get the full contents of the file into a QByteArray
+   QByteArray data = f.readAll();
+   f.close();
+
+   // get the md5 hash, convert to hex, get file size;  then format "hash size"
+   QString hashandsize =
+      QString( QCryptographicHash::hash( data, QCryptographicHash::Md5 )
+      .toHex() ) + " " + QString::number( QFileInfo( filename ).size() );
+
+   // return the "hash size" string
+   return hashandsize;
+}
+
+// convert DateTime string to unambiguous UTC form ("yyyy-mm-dd HH:MM:SS UTC")
+QString US_Util::toUTCDatetimeText( QString dttext, bool knownUTC )
+{
+   QString utctext = dttext;
+   int     ixLT    = dttext.length() - 2;          // expected last 'T'
+   int     ixT     = dttext.indexOf( "T" );        // index of first 'T'
+   bool    haveT   = ( ixT > 0  &&  ixT != ixLT ); // have lone 'T'
+   bool    haveUTC = dttext.endsWith( "UTC" );     // have "UTC" on end
+
+   if ( knownUTC )
+   {  // if known to be UTC, just insure no single 'T' and " UTC" suffix
+
+      if ( haveT )
+      {  // if single 'T' between date and time, replace with ' '
+         utctext.replace( ixT, 1, " " );
+      }
+
+      if ( !haveUTC )
+      {  // if no " UTC" suffix, append it
+         utctext.append( " UTC" );
+      }
+   }
+
+   else
+   {  // if we don't know that it's UTC, convert when " UTC" is missing
+
+      if ( !haveUTC )
+      {  // if no " UTC", convert to UTC and append " UTC"
+         QDateTime dt;
+
+         if ( dttext.indexOf( "-" ) == 4 )
+         {  // if already in ISO form, get UTC DateTime from string in ISO form
+            dt      = QDateTime::fromString( dttext, Qt::ISODate ).toUTC();
+         }
+
+         else
+         {  // if in unknown form, get UTC DateTime assuming default form
+            dt      = QDateTime::fromString( dttext ).toUTC();
+         }
+
+         // new text version in ISO form
+         utctext = dt.toString( Qt::ISODate );
+
+         // re-determine whether we have single 'T'
+         haveT   = ( utctext.indexOf( "T" ) > 0 );
+
+         // append the "UTC"
+         utctext.append( " UTC" );
+      }
+
+      if ( haveT )
+      {  // if single 'T' between date and time, replace with ' '
+         utctext.replace( ixT, 1, " " );
+      }
+   }
+
+   return utctext;
+}
+
+// insure DateTime text is in ISO form (ISO, UTC, or unknown on input )
+QString US_Util::toISODatetimeText( QString dttext )
+{
+   QString isotext = dttext;   // default assumes already in ISO form
+
+   if ( dttext.contains( " UTC" ) )
+   {  // if UTC form on input, convert by removing " UTC" and adding 'T'
+      isotext.replace( " UTC", "" ).replace( dttext.indexOf( " " ), 1, "T" );
+   }
+
+   else if ( dttext.indexOf( "-" ) != 4 )
+   {  // if not ISO (unknown), convert by back-and-forth from DateTime
+      isotext = QDateTime::fromString( dttext ).toString( Qt::ISODate );
+      // otherwise, we must already have ISO form
+   }
+
+   return isotext;
+}
+
--- utils/us_util.h
+++ utils/us_util.h
@@ -7,18 +7,76 @@
 #include "us_extern.h"
 
 //! \brief General utilities for UltraScan
+//!
+//! This class provides a number of general utility functions.
+//! All methods are static.
+//!
 class US_EXTERN US_Util
 {
    public:
-      //! \brief Returns the next string component where the componenets
-      //!        are separated by a common delimiter.  The function modifies
-      //!        the passed string.  
-      //! \param s String to search
+      //! \brief Returns the next string component where the components
+      //!        are separated by a common delimiter. The function modifies
+      //!        the passed string.
+      //!
+      //! This function is designed to be called iteratively. On each call,
+      //! the leading substring in the given string is returned and the
+      //! full string is modified by having that substring and the 
+      //! immediately following delimiter removed.
+      //!
+      //! \param s         String to search
       //! \param separator Delimiter to use for parsing
+      //! \returns         Next token in the string (substring before delimiter)
       static QString getToken( QString&, const QString& );
 
 
       //! \brief Generates and returns a new Global Identifier (guid)
+      //!
+      //! This function generates a new Global Identifier (guid) string.
+      //! The returned string is 36 characters in length and is in the
+      //! standard uuid form: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx".
+      //!
+      //! \returns         New GUID 36-character string
       static QString new_guid( void );
+
+      //! \brief Generates a string for a file with md5 hash and file size
+      //!
+      //! An MD5 hash is performed on the file whose full path name is given.
+      //! The size of the file in bytes is also determined. The returned
+      //! string combines the hash and size separated by a blank: "hash size".
+      //!
+      //! \param filename  Pull path name of file for md5 hash and size
+      //! \returns         A string in the form of "hash size" for the file
+      static QString md5sum_file( QString );
+
+      //! \brief Converts a DateTime string to unambiguous UTC form
+      //!
+      //! This function converts a DateTime string into an unambiguous UTC
+      //! form, as would be output by the Unix "date" command with arguments
+      //! of "-u +%F\ %T\ %Z"; a "yyyy-mm-dd HH:MM:SS UTC" form. The input
+      //! DateTime string may be in ISO form (e.g., "2010-08-10T09:42:16"),
+      //! already in UTC form, or in the default locale format, such as
+      //! "Mon Aug 9 23:24:45 2010". A boolean argument specifies whether
+      //! the implied time is already in UTC. If "false" and no " UTC" is in
+      //! the input string, the underlying DateTime is converted from local
+      //! time to Universal Time Coordinates. No time conversion occurs if
+      //! the boolean argument is "true".
+      //!
+      //! \param dttext    DateTime text string in ISO or other form
+      //! \param knownUTC  A boolean whether the input string is known to
+      //!                  represent date and time in Universal Time Coordinates.
+      //! \returns         A DateTime string in "yyyy-mm-dd HH:MM:SS UTC" form
+      static QString toUTCDatetimeText( QString, bool );
+
+      //! \brief Converts a DateTime string to Qt's ISODate form
+      //!
+      //! This function converts a DateTime string into the form created
+      //! or expected with a Qt::DateFormat of Qt::ISODate. That is, the
+      //! output is something like "2010-08-10T09:42:16". The input may
+      //! already be in ISO form; may be in UTC form; or may be an unknown
+      //! format assumed to be Qt:TextDate, such as "Mon Aug 9 23:24:45 2010".
+      //!
+      //! \param dttext    DateTime text string in UTC, ISO, or other form
+      //! \returns         A DateTime string in ISO form ("2010-08-14T21:41:22")
+      static QString toISODatetimeText( QString );
 };
 #endif
