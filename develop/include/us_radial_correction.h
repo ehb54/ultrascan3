2b1b3eca5d94556eba5e4685bf8f431c291dab11
demeler <7659499+demeler@users.noreply.github.com>
2010-10-10 12:22:57 +0000

    Improved radial calibration correction routine that allows also scaled radial corrections.

--- develop/include/us_radial_correction.h
+++ develop/include/us_radial_correction.h
@@ -13,6 +13,10 @@
 #include <qfiledialog.h>
 #include <qstringlist.h>
 #include <qprogressbar.h>
+#include <qlistbox.h>
+#include <qradiobutton.h>
+#include <qbuttongroup.h>
+#include <qcheckbox.h>
 
 // QWT includes:
 
@@ -20,6 +24,7 @@
 
 // UltraScan includes:
 #include "us_util.h"
+#include "us_math.h"
 #include "us_editor.h"
 
 extern int global_Xpos;
@@ -33,17 +38,41 @@ class US_EXTERN US_RadialCorrection : public QFrame
    
       US_RadialCorrection(QWidget *p=0 , const char *name=0);
       ~US_RadialCorrection();
-      double correction;
+      double correction, bottom_measured, top_measured, top_target, bottom_target, stretch;
+      double speed;
+      unsigned int rotor, centerpiece;
+      bool scaled, trim;
+
       US_Config *USglobal;
+      QRadioButton *rb_constant;
+      QRadioButton *rb_scaled;
+      QCheckBox *cb_trim;
+      QButtonGroup *bg_correction;
       QPushButton *pb_load;
       QPushButton *pb_process;
       QPushButton *pb_help;
       QPushButton *pb_close;
       QPushButton *pb_reset;
+      QListBox *lb_rotor;
+      QListBox *lb_centerpiece;
       QLabel *lbl_banner1;
       QLabel *lbl_correction;
       QLabel *lbl_scans;
+      QLabel *lbl_top_measured;
+      QLabel *lbl_bottom_measured;
+      QLabel *lbl_top_target;
+      QLabel *lbl_rotor;
+      QLabel *lbl_centerpiece;
+      QLabel *lbl_stretch1;
+      QLabel *lbl_stretch2;
+      QLabel *lbl_bottom1;
+      QLabel *lbl_bottom2;
+      QLabel *lbl_speed;
+      QwtCounter *cnt_speed;
       QwtCounter *cnt_correction;
+      QwtCounter *cnt_top_measured;
+      QwtCounter *cnt_top_target;
+      QwtCounter *cnt_bottom_measured;
       QProgressBar *pgb_progress;
 
 #ifdef WIN32
@@ -51,6 +80,8 @@ class US_EXTERN US_RadialCorrection : public QFrame
 #endif
 
       vector <QString> filenames;
+      vector < rotorInfo > rotor_list;
+      vector <struct centerpieceInfo> cp_list;
 
 #ifdef WIN32
 #pragma warning ( default: 4251 )
@@ -63,7 +94,16 @@ class US_EXTERN US_RadialCorrection : public QFrame
       void help();
       void reset();
       void set_correction(double);
-
+      void update();
+      void set_speed(double);
+      void set_top_measured(double);
+      void set_top_target(double);
+      void set_bottom_measured(double);
+      void update_scaled(int);
+      void update_rotor(int);
+      void update_centerpiece(int);
+      void update_trim();
+      
 };
 
 
--- develop/src/us_radial_correction.cpp
+++ develop/src/us_radial_correction.cpp
@@ -9,6 +9,38 @@ US_RadialCorrection::US_RadialCorrection(QWidget *p , const char *name) : QFrame
    setPalette(QPalette(USglobal->global_colors.cg_frame, USglobal->global_colors.cg_frame, USglobal->global_colors.cg_frame));
    setCaption(tr("UltraScan: Radial Correction Utility"));
 
+   unsigned int i;
+   QString str;
+   speed = 30000.0;
+   rotor = 0;
+   centerpiece = 0;
+   top_measured = 5.800;
+   top_target = 5.800;
+   bottom_measured = 7.200;
+   stretch = 0.00;
+   scaled = false;
+   trim = false;
+   cp_list.clear();
+   rotor_list.clear();
+   if (!readCenterpieceInfo(&cp_list))
+   {
+      QMessageBox::critical(0, "UltraScan Fatal Error:", "There was a problem opening the\n"
+                            "centerpiece database file:\n\n"
+                            + USglobal->config_list.system_dir + "/etc/centerpiece.dat\n\n"
+                            "Please install the centerpiece database file\n"
+                            "before proceeding.", QMessageBox::Ok, QMessageBox::NoButton, QMessageBox::NoButton);
+      exit(-1);
+   }
+   if (!readRotorInfo(&rotor_list))
+   {
+      QMessageBox::critical(0, "UltraScan Fatal Error:", "There was a problem opening the\n"
+                            "rotor database file:\n\n"
+                            + USglobal->config_list.system_dir + "/etc/rotor.dat\n\n"
+                            "Please install the rotor database file\n"
+                            "before proceeding.", QMessageBox::Ok, QMessageBox::NoButton, QMessageBox::NoButton);
+      exit(-1);
+   }
+   
    lbl_banner1 = new QLabel(tr(" Radial Calibration\nCorrection Routine "), this);
    Q_CHECK_PTR(lbl_banner1);
    lbl_banner1->setFrameStyle(QFrame::WinPanel|QFrame::Raised);
@@ -17,7 +49,6 @@ US_RadialCorrection::US_RadialCorrection(QWidget *p , const char *name) : QFrame
    lbl_banner1->setFont(QFont(USglobal->config_list.fontFamily, USglobal->config_list.fontSize, QFont::Bold));
    lbl_banner1->setMinimumHeight(70);
    lbl_banner1->setMaximumHeight(70);
-
 
    pb_load = new QPushButton(tr("Select Scans"), this);
    Q_CHECK_PTR(pb_load);
@@ -32,11 +63,15 @@ US_RadialCorrection::US_RadialCorrection(QWidget *p , const char *name) : QFrame
    lbl_scans->setPalette( QPalette(USglobal->global_colors.cg_edit, USglobal->global_colors.cg_edit, USglobal->global_colors.cg_edit));
    lbl_scans->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize));
 
-   lbl_correction = new QLabel(tr(" Radial Calibration Correction distance (cm): "), this);
-   Q_CHECK_PTR(lbl_correction);
-   lbl_correction->setAlignment(AlignLeft|AlignVCenter);
-   lbl_correction->setPalette( QPalette(USglobal->global_colors.cg_label, USglobal->global_colors.cg_label, USglobal->global_colors.cg_label));
-   lbl_correction->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1, QFont::Bold));
+   bg_correction = new QButtonGroup();
+   bg_correction->setRadioButtonExclusive(true);
+   connect(bg_correction, SIGNAL(clicked(int)), SLOT(update_scaled(int)) );
+
+   rb_constant = new QRadioButton(this);
+   rb_constant->setText(tr(" Constant Radial Correction: "));
+   rb_constant->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1)); //, QFont::Bold));
+   rb_constant->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
+   bg_correction->insert(rb_constant);
 
    cnt_correction= new QwtCounter(this);
    Q_CHECK_PTR(cnt_correction);
@@ -46,12 +81,175 @@ US_RadialCorrection::US_RadialCorrection(QWidget *p , const char *name) : QFrame
    cnt_correction->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
    connect(cnt_correction, SIGNAL(valueChanged(double)), SLOT(set_correction(double)));
 
+   rb_scaled = new QRadioButton(this);
+   rb_scaled->setText(tr(" Scaled Radial Correction"));
+   rb_scaled->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1)); //, QFont::Bold));
+   rb_scaled->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
+   bg_correction->insert(rb_scaled);
+
+   bg_correction->setButton(scaled);
+
+   cb_trim = new QCheckBox(tr(" Trim unneeded data"), this);
+   cb_trim->setChecked(trim);
+   cb_trim->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize));
+   cb_trim->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
+   connect(cb_trim, SIGNAL(clicked()), SLOT(update_trim()));
+
+   lbl_top_measured = new QLabel(tr(" Cell Top (measured, cm): "), this);
+   Q_CHECK_PTR(lbl_top_measured);
+   lbl_top_measured->setAlignment(AlignLeft|AlignVCenter);
+   lbl_top_measured->setPalette( QPalette(USglobal->global_colors.cg_label, USglobal->global_colors.cg_label, USglobal->global_colors.cg_label));
+   lbl_top_measured->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1, QFont::Bold));
+
+   cnt_top_measured = new QwtCounter(this);
+   Q_CHECK_PTR(cnt_top_measured);
+   cnt_top_measured->setRange(5.0, 7.0, 0.0001);
+   cnt_top_measured->setValue(top_measured);
+   cnt_top_measured->setNumButtons(3);
+   cnt_top_measured->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
+   connect(cnt_top_measured, SIGNAL(valueChanged(double)), SLOT(set_top_measured(double)));
+
+   lbl_top_target = new QLabel(tr(" Cell Top Target (cm): "), this);
+   Q_CHECK_PTR(lbl_top_target);
+   lbl_top_target->setAlignment(AlignLeft|AlignVCenter);
+   lbl_top_target->setPalette( QPalette(USglobal->global_colors.cg_label, USglobal->global_colors.cg_label, USglobal->global_colors.cg_label));
+   lbl_top_target->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1, QFont::Bold));
+
+   cnt_top_target = new QwtCounter(this);
+   Q_CHECK_PTR(cnt_top_target);
+   cnt_top_target->setRange(5.8, 6.0, 0.0001);
+   cnt_top_target->setValue(top_target);
+   cnt_top_target->setNumButtons(3);
+   cnt_top_target->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
+   connect(cnt_top_target, SIGNAL(valueChanged(double)), SLOT(set_top_target(double)));
+
+   lbl_bottom_measured = new QLabel(tr(" Cell Bottom (measured, cm): "), this);
+   Q_CHECK_PTR(lbl_bottom_measured);
+   lbl_bottom_measured->setAlignment(AlignLeft|AlignVCenter);
+   lbl_bottom_measured->setPalette( QPalette(USglobal->global_colors.cg_label, USglobal->global_colors.cg_label, USglobal->global_colors.cg_label));
+   lbl_bottom_measured->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1, QFont::Bold));
+
+   cnt_bottom_measured = new QwtCounter(this);
+   Q_CHECK_PTR(cnt_bottom_measured);
+   cnt_bottom_measured->setRange(7, 7.3, 0.0001);
+   cnt_bottom_measured->setValue(bottom_measured);
+   cnt_bottom_measured->setNumButtons(3);
+   cnt_bottom_measured->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
+   connect(cnt_bottom_measured, SIGNAL(valueChanged(double)), SLOT(set_bottom_measured(double)));
+
+   lbl_speed = new QLabel(tr(" Rotor Speed (rpm): "), this);
+   Q_CHECK_PTR(lbl_speed);
+   lbl_speed->setAlignment(AlignLeft|AlignVCenter);
+   lbl_speed->setPalette( QPalette(USglobal->global_colors.cg_label, USglobal->global_colors.cg_label, USglobal->global_colors.cg_label));
+   lbl_speed->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1, QFont::Bold));
+
+   cnt_speed = new QwtCounter(this);
+   Q_CHECK_PTR(cnt_speed);
+   cnt_speed->setRange(0, 60000, 10);
+   cnt_speed->setValue(speed);
+   cnt_speed->setNumButtons(3);
+   cnt_speed->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
+   connect(cnt_speed, SIGNAL(valueChanged(double)), SLOT(set_speed(double)));
+
+   lbl_rotor = new QLabel(tr(" Select a Rotor: "), this);
+   lbl_rotor->setPalette( QPalette(USglobal->global_colors.cg_label, USglobal->global_colors.cg_label, USglobal->global_colors.cg_label) );
+   lbl_rotor->setAlignment(AlignLeft|AlignVCenter);
+   lbl_rotor->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1, QFont::Bold));
+
+   lbl_centerpiece = new QLabel(tr(" Select a Centerpiece: "), this);
+   lbl_centerpiece->setPalette( QPalette(USglobal->global_colors.cg_label, USglobal->global_colors.cg_label, USglobal->global_colors.cg_label) );
+   lbl_centerpiece->setAlignment(AlignLeft|AlignVCenter);
+   lbl_centerpiece->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1, QFont::Bold));
+
+   lb_rotor = new QListBox(this);
+   for (i=0; i<rotor_list.size(); i++)
+   {
+      lb_rotor->insertItem(str.sprintf("%d " + rotor_list[i].type, rotor_list[i].serial_number));
+   }
+   lb_rotor->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
+   lb_rotor->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1));
+   lb_rotor->setCurrentItem(0);
+   connect(lb_rotor, SIGNAL(highlighted(int)), SLOT(update_rotor(int)));
+
+   lb_centerpiece = new QListBox(this);
+   for (i=0; i<cp_list.size(); i++)
+   {
+      str.sprintf(cp_list[i].material + tr(", %d channels, %3.2f cm "),  cp_list[i].channels * 2, cp_list[i].pathlength);
+      switch (cp_list[i].sector)
+      {
+         case 0:
+         {
+            str += "(standard)";
+            break;
+         }
+         case 1:
+         {
+            str += "(rectangular)";
+            break;
+         }
+         case 2:
+         {
+            str += "(circular)";
+            break;
+         }
+         case 3:
+         {
+            str += "(synthetic)";
+            break;
+         }
+         case 4:
+         {
+            str += "(band-forming)";
+            break;
+         }
+         case 5:
+         {
+            str += "(meniscus-matching)";
+            break;
+         }
+      }
+      lb_centerpiece->insertItem(str);
+   }
+   lb_centerpiece->setCurrentItem(0);
+   lb_centerpiece->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
+   lb_centerpiece->setVariableWidth(true);
+   int width = lb_centerpiece->contentsWidth();
+   lb_centerpiece->setMinimumWidth(width);
+   lb_centerpiece->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1));
+   connect(lb_centerpiece, SIGNAL(highlighted(int)), SLOT(update_centerpiece(int)));
+
+   lbl_bottom1 = new QLabel(tr(" Centerpiece Bottom Position: "), this);
+   lbl_bottom1->setPalette( QPalette(USglobal->global_colors.cg_label, USglobal->global_colors.cg_label, USglobal->global_colors.cg_label) );
+   lbl_bottom1->setAlignment(AlignLeft|AlignVCenter);
+   lbl_bottom1->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1, QFont::Bold));
+
+   lbl_bottom2 = new QLabel("", this);
+   lbl_bottom2->setFrameStyle(QFrame::WinPanel|Sunken);
+   lbl_bottom2->setPalette( QPalette(USglobal->global_colors.cg_edit, USglobal->global_colors.cg_edit, USglobal->global_colors.cg_edit) );
+   lbl_bottom2->setAlignment(AlignLeft|AlignVCenter);
+   lbl_bottom2->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1));
+
+   lbl_stretch1 = new QLabel(tr(" Rotor Stretch: "), this);
+   lbl_stretch1->setPalette( QPalette(USglobal->global_colors.cg_label, USglobal->global_colors.cg_label, USglobal->global_colors.cg_label) );
+   lbl_stretch1->setAlignment(AlignLeft|AlignVCenter);
+   lbl_stretch1->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1, QFont::Bold));
+
+   lbl_stretch2 = new QLabel("", this);
+   lbl_stretch2->setFrameStyle(QFrame::WinPanel|Sunken);
+   lbl_stretch2->setPalette( QPalette(USglobal->global_colors.cg_edit, USglobal->global_colors.cg_edit, USglobal->global_colors.cg_edit) );
+   lbl_stretch2->setAlignment(AlignLeft|AlignVCenter);
+   lbl_stretch2->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize - 1));
+
    pb_process = new QPushButton(tr(" Apply Radial Correction "), this);
    Q_CHECK_PTR(pb_process);
    pb_process->setAutoDefault(false);
    pb_process->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize));
    pb_process->setPalette( QPalette(USglobal->global_colors.cg_pushb, USglobal->global_colors.cg_pushb_disabled, USglobal->global_colors.cg_pushb_active));
    connect(pb_process, SIGNAL(clicked()), SLOT(process()));
+
+   pgb_progress = new QProgressBar(this, "iteration progress");
+   pgb_progress->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
+   pgb_progress->setMinimumHeight(26);
 
    pb_help = new QPushButton(tr("Help"), this);
    Q_CHECK_PTR(pb_help);
@@ -60,7 +258,7 @@ US_RadialCorrection::US_RadialCorrection(QWidget *p , const char *name) : QFrame
    pb_help->setPalette( QPalette(USglobal->global_colors.cg_pushb, USglobal->global_colors.cg_pushb_disabled, USglobal->global_colors.cg_pushb_active));
    connect(pb_help, SIGNAL(clicked()), SLOT(help()));
 
-   pb_reset = new QPushButton(tr("Reset"), this);
+   pb_reset = new QPushButton(tr("Reset File Selection"), this);
    Q_CHECK_PTR(pb_reset);
    pb_reset->setAutoDefault(false);
    pb_reset->setFont(QFont( USglobal->config_list.fontFamily, USglobal->config_list.fontSize));
@@ -74,17 +272,11 @@ US_RadialCorrection::US_RadialCorrection(QWidget *p , const char *name) : QFrame
    pb_close->setPalette( QPalette(USglobal->global_colors.cg_pushb, USglobal->global_colors.cg_pushb_disabled, USglobal->global_colors.cg_pushb_active));
    connect(pb_close, SIGNAL(clicked()), SLOT(close()));
 
-   pgb_progress = new QProgressBar(this, "iteration progress");
-   pgb_progress->setPalette( QPalette(USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal, USglobal->global_colors.cg_normal));
-   pgb_progress->setMinimumHeight(26);
-
-
    global_Xpos += 30;
    global_Ypos += 30;
-
    move(global_Xpos, global_Ypos);
-   
    setup_GUI();
+   update();
 }
 
 US_RadialCorrection::~US_RadialCorrection()
@@ -94,7 +286,7 @@ US_RadialCorrection::~US_RadialCorrection()
 void US_RadialCorrection::setup_GUI()
 {
    int j=0;
-   int rows = 5, columns = 2, spacing = 2;
+   int rows = 15, columns = 2, spacing = 2;
    
    QGridLayout * grid = new QGridLayout(this, rows, columns, spacing);
    grid->setMargin(spacing);
@@ -107,13 +299,40 @@ void US_RadialCorrection::setup_GUI()
    grid->addWidget(pb_load, j, 0);
    grid->addWidget(lbl_scans, j, 1);
    j++;
-   grid->addWidget(lbl_correction, j, 0);
+   grid->addWidget(rb_constant, j, 0);
    grid->addWidget(cnt_correction, j, 1);
+   j++;
+   grid->addWidget(rb_scaled, j, 0);
+   grid->addWidget(cb_trim, j, 1);
+   j++;
+   grid->addWidget(lbl_top_measured, j, 0);
+   grid->addWidget(cnt_top_measured, j, 1);
+   j++;
+   grid->addWidget(lbl_top_target, j, 0);
+   grid->addWidget(cnt_top_target, j, 1);
+   j++;
+   grid->addWidget(lbl_bottom_measured, j, 0);
+   grid->addWidget(cnt_bottom_measured, j, 1);
+   j++;
+   grid->addWidget(lbl_speed, j, 0);
+   grid->addWidget(cnt_speed, j, 1);
+   j++;
+   grid->addWidget(lbl_rotor, j, 0);
+   grid->addWidget(lbl_centerpiece, j, 1);
+   j++;
+   grid->addWidget(lb_rotor, j, 0);
+   grid->addWidget(lb_centerpiece, j, 1);
+   j++;
+   grid->addWidget(lbl_stretch1, j, 0);
+   grid->addWidget(lbl_stretch2, j, 1);
+   j++;
+   grid->addWidget(lbl_bottom1, j, 0);
+   grid->addWidget(lbl_bottom2, j, 1);
+   j++;
+   grid->addMultiCellWidget(pgb_progress, j, j, 0, 1);
    j++;
    grid->addWidget(pb_process, j, 0);
    grid->addWidget(pb_reset, j, 1);
-   j++;
-   grid->addMultiCellWidget(pgb_progress, j, j, 0, 1);
    j++;
    grid->addWidget(pb_help, j, 0);
    grid->addWidget(pb_close, j, 1);
@@ -217,12 +436,35 @@ void US_RadialCorrection::process()
          while (!ts.eof())
          {
             ts >> val;
-            val += correction;
-            x.push_back(val);
-            ts >> val;
-            y.push_back(val);
-            ts >> val;
-            z.push_back(val);
+            //cout << val << " ";
+            if (scaled)
+            {
+               val = top_target + (val-top_measured) * (bottom_target - top_target)/(bottom_measured - top_measured);
+               //cout << val << endl;
+            }
+            else
+            {
+               val += correction;
+            }
+            if (trim)
+            {
+               if (val > top_target && val < bottom_target)
+               {
+                  x.push_back(val);
+                  ts >> val;
+                  y.push_back(val);
+                  ts >> val;
+                  z.push_back(val);
+               }
+            }
+            else
+            {
+               x.push_back(val);
+               ts >> val;
+               y.push_back(val);
+               ts >> val;
+               z.push_back(val);
+            }
             ts.readLine();
          }
          f.close();
@@ -272,3 +514,60 @@ void US_RadialCorrection::help()
    online_help->show_help("manual/radial_correction.html");
 }
 
+void US_RadialCorrection::update()
+{
+   QString str_bottom, str1;
+   //float bottom = calc_bottom(rotor_list, cp_list, rotor, centerpiece, 0, speed);
+   stretch = stretch_with_rotor_list(rotor, (unsigned int) speed, &rotor_list);
+   str_bottom = "";
+   for (unsigned int i=0; i<cp_list[centerpiece].channels; i++)
+   {
+      bottom_target = cp_list[centerpiece].bottom_position[i]+stretch; // overwrite bottom_target until the last channel - which is used for the correction
+      str_bottom += str1.sprintf("%6.4f cm ", bottom_target);
+   }
+   lbl_bottom2->setText(str_bottom);
+   lbl_stretch2->setText(str1.sprintf("%6.4e cm ", stretch));
+}
+
+void US_RadialCorrection::set_speed(double val)
+{
+   speed = val;
+   update();
+}
+
+void US_RadialCorrection::set_top_measured(double val)
+{
+   top_measured = val;
+}
+
+void US_RadialCorrection::set_bottom_measured(double val)
+{
+   bottom_measured = val;
+}
+
+void US_RadialCorrection::set_top_target(double val)
+{
+   top_target = val;
+}
+
+void US_RadialCorrection::update_rotor(int val)
+{
+   rotor = val;
+   update();
+}
+
+void US_RadialCorrection::update_scaled(int val)
+{
+   scaled = val;
+}
+
+void US_RadialCorrection::update_trim()
+{
+   trim = cb_trim->isChecked();
+}
+
+void US_RadialCorrection::update_centerpiece(int val)
+{
+   centerpiece = val;
+   update();
+}
