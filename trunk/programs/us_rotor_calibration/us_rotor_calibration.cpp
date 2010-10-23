9c1917ebf3e008dced18dc8f8c0311dd69d1fe2b
demeler <demeler@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2010-10-23 18:23:01 +0000

    skeleton program for rotor calibration - need some test data to continue

---
+++ trunk/programs/us_rotor_calibration/us_rotor_calibration.cpp
@@ -0,0 +1,199 @@
+//! \file us_rotor_calibration.cpp
+
+#include "us_rotor_calibration.h"
+#include "us_settings.h"
+#include "us_license_t.h"
+#include "us_math2.h"
+#include "us_util.h"
+#include "us_db2.h"
+#include "us_license.h"
+#include "us_gui_settings.h"
+#include "us_run_details2.h"
+#include "us_passwd.h"
+
+//! \brief Main program for US_RotorCalibration. Loads translators and starts
+//         the class US_FitMeniscus.
+
+int main( int argc, char* argv[] )
+{
+   QApplication application( argc, argv );
+
+   #include "main1.inc"
+
+   // License is OK.  Start up.
+   
+   US_RotorCalibration w;
+   w.show();                   //!< \memberof QWidget
+   return application.exec();  //!< \memberof QApplication
+}
+
+US_RotorCalibration::US_RotorCalibration() : US_Widgets()
+{
+   check  = QIcon( US_Settings::usHomeDir() + "/etc/check.png" );
+   unsigned int row=0;
+
+   setWindowTitle( tr( "Edit Rotor Calibration" ) );
+   setPalette( US_GuiSettings::frameColor() );
+
+   QGridLayout* top = new QGridLayout( this );
+   top->setSpacing         ( 2 );
+   top->setContentsMargins ( 2, 2, 2, 2 );
+   
+   lbl_instructions = us_label( "Instructions:", -1 );
+   top->addWidget( lbl_instructions, row, 0 );
+
+   le_instructions = us_lineedit( "", 1 );
+   le_instructions->setReadOnly( true );
+   le_instructions->setText("Please load a calibration data set...");
+   top->addWidget( le_instructions, row, 1, 1, 3 );
+
+   row++;
+   
+   // Row 2
+   QPushButton* pb_load = us_pushbutton( tr( "Load Calibration Data" ) );
+   connect( pb_load, SIGNAL( clicked() ), SLOT( load() ) );
+   top->addWidget( pb_load, row, 0 );
+   
+   QPushButton* pb_reset = us_pushbutton( tr( "Reset" ) );
+   connect( pb_reset, SIGNAL( clicked() ), SLOT( reset() ) );
+   top->addWidget( pb_reset, row, 1 );
+   
+   row++;
+   
+   // Row 3
+   QGridLayout* lo_counterbalance = us_radiobutton( tr( "Show Counterbalance" ), rb_counterbalance );
+   rb_counterbalance->setChecked( false );
+   top->addLayout( lo_counterbalance, row, 0 );
+
+   QGridLayout* lo_cells = us_radiobutton( tr( "Show Cells" ), rb_cells );
+   rb_cells->setChecked( true );
+   top->addLayout( lo_cells, row, 1 );
+
+   row++;
+   
+   // Row 4
+
+   QLabel* lbl_spacer = us_banner( tr( "" ) );
+   top->addWidget( lbl_spacer, row, 0, 1, 2 );
+   
+   row++;
+   
+   // Row 5
+   QPushButton* pb_help = us_pushbutton( tr( "Help" ) );
+   connect( pb_help, SIGNAL( clicked() ), SLOT( help() ) );
+   top->addWidget( pb_help, row, 0 );
+
+   QPushButton* pb_close = us_pushbutton( tr( "Close" ) );
+   connect( pb_close, SIGNAL( clicked() ), SLOT( close() ) );
+   top->addWidget( pb_close, row, 1 );
+   
+   // Plot layout on right side of window
+   plot = new US_Plot( data_plot,
+         tr( "Absorbance Data" ),
+         tr( "Radius (in cm)" ), tr( "Absorbance" ) );
+   data_plot->setMinimumSize( 600, 400 );
+   data_plot->enableAxis( QwtPlot::xBottom, true );
+   data_plot->enableAxis( QwtPlot::yLeft  , true );
+   top->addLayout(plot, 1, 2, row, 2);
+
+   pick = new US_PlotPicker( data_plot );
+   // Set rubber band to display for Control+Left Mouse Button
+   pick->setRubberBand  ( QwtPicker::VLineRubberBand );
+   pick->setMousePattern( QwtEventPattern::MouseSelect1, Qt::LeftButton, Qt::ControlModifier );
+
+   for (unsigned int i=0; i<row-1; i++)
+   {
+      top->setRowStretch(i, 0);
+   }
+   top->setRowStretch(row-3, 0.1);
+}
+
+void US_RotorCalibration::reset()
+{
+   data.scanData .clear();
+   allData.clear();
+   le_instructions->setText("Please load a calibration data set...");
+}
+
+void US_RotorCalibration::load( void )
+{  
+// Ask for data directory
+   workingDir = QFileDialog::getExistingDirectory( this, tr("Raw Data Directory"), US_Settings::resultDir(), QFileDialog::DontResolveSymlinks );
+   if ( workingDir.isEmpty() ) return;
+   reset();
+   workingDir.replace( "\\", "/" );  // WIN32 issue
+
+   QStringList components =  workingDir.split( "/", QString::SkipEmptyParts );  
+   
+   runID = components.last();
+
+   QStringList nameFilters = QStringList( "*.auc" );
+
+   QDir d( workingDir );
+
+   files =  d.entryList( nameFilters, 
+         QDir::Files | QDir::Readable, QDir::Name );
+
+   if ( files.size() == 0 )
+   {
+      QMessageBox::warning( this,
+            tr( "No Files Found" ),
+            tr( "There were no files of the form *.auc\n"  
+                "found in the specified directory." ) );
+      return;
+   }
+
+   // Look for cell / channel / wavelength combinations
+   for ( int i = 0; i < files.size(); i++ )
+   {
+      QStringList part = files[ i ].split( "." );
+
+      QString t = part[ 2 ] + " / " + part[ 3 ] + " / " + part[ 4 ];
+      if ( ! triples.contains( t ) ) triples << t;
+   }
+
+   le_instructions->setText( runID + "; please review data...");
+
+   // Read all data
+   if ( workingDir.right( 1 ) != "/" ) workingDir += "/"; // Ensure trailing /
+
+   QString file;
+   foreach ( file, files )
+   {
+      QString filename = workingDir + file;
+      
+      int result = US_DataIO2::readRawData( filename, data );
+      if ( result != US_DataIO2::OK )
+      {
+         QMessageBox::warning( this,
+            tr( "UltraScan Error" ),
+            tr( "Could not read data file.\n" ) 
+            + US_DataIO2::errorString( result ) + "\n" + filename );
+         return;
+      }
+
+      allData << data;
+      data.scanData.clear();
+   }
+
+   if ( allData.isEmpty() )
+   {
+      QMessageBox::warning( this,
+         tr( "UltraScan Error" ),
+         tr( "Could not read any data file." ) );
+      return;
+   }
+
+   dataType = QString( QChar( data.type[ 0 ] ) ) 
+            + QString( QChar( data.type[ 1 ] ) );
+
+   if ( dataType != "RI" )
+   {
+      le_instructions->setText( "Attention - " + runID + " is not intensity data!");
+//      return;
+   }
+   
+   data = allData[ 0 ];
+   //plot_current( 0 );
+}
+
---
+++ trunk/programs/us_rotor_calibration/us_rotor_calibration.h
@@ -0,0 +1,68 @@
+#ifndef US_EDIT_H
+#define US_EDIT_H
+
+#include <uuid/uuid.h>
+
+#include <QtGui>
+#include <QApplication>
+#include <QDomDocument>
+
+#include <qwt_plot_marker.h>
+
+#include "us_extern.h"
+#include "us_widgets.h"
+#include "us_help.h"
+#include "us_plot.h"
+#include "us_dataIO2.h"
+
+class US_EXTERN US_RotorCalibration : public US_Widgets
+{
+	Q_OBJECT
+
+	public:
+		US_RotorCalibration();
+
+	private:
+
+      US_DataIO2::RawData            data;
+      QVector< US_DataIO2::RawData > allData;
+
+      US_Help            showHelp;
+
+      QIcon              check;
+
+      QString            workingDir;
+      QString            runID;
+      QString            editID;
+      QString            dataType;
+      QStringList        files;
+      QStringList        triples;
+                      
+      QwtPlot*           data_plot;
+      QwtPlotCurve*      raw_curve;
+      QwtPlotCurve*      fit_curve;
+      QwtPlotCurve*      v_line;
+      QwtPlotCurve*      minimum_curve;
+      QwtPlotGrid*       grid;
+      QwtPlotMarker*     marker;
+      US_PlotPicker*     pick;
+      US_Plot*           plot;
+      
+      QLabel*            lbl_instructions;
+      QLabel*            lbl_spacer;
+
+      QLineEdit*         le_instructions;
+                        
+      QRadioButton*      rb_counterbalance;
+      QRadioButton*      rb_cells;
+
+   public slots:
+      void help ( void )
+      {
+         showHelp.show_help( "manual/rotor_calibration.html" );
+      };
+      void reset ( void );
+      void load ( void );
+
+};
+#endif
