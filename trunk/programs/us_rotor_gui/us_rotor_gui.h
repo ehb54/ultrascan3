a314dc019da9bec1206b67ce88688377bde5c086
demeler <demeler@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2010-11-23 17:20:16 +0000

    separated out the abstract rotor code and made some minor changes in the GUI

--- trunk/programs/us_rotor_gui/us_rotor_gui.h
+++ trunk/programs/us_rotor_gui/us_rotor_gui.h
@@ -14,47 +14,7 @@
 #include "us_help.h"
 #include "us_editor.h"
 #include "us_investigator.h"
-
-//! \brief the abstractRotor structure describes a generic 4- or 8-hole rotor
-struct abstractRotor
-{
-   int ID;
-   QString GUID;
-   QString name;
-   QString material;
-   int numHoles;
-   int maxRPM;
-   double cellCenter;
-   QString manufacturer;
-};
-
-//! \brief the rotor structure describes a derived 4- or 8-hole rotor
-struct rotor
-{
-   int ID;
-   int abstractRotorID;
-   QString abstractRotorGUID;
-   int labID;                 //!< The ID of the laboratory which owns this rotor
-   QString GUID;
-   QString name;
-   QString serialNumber;
-};
-
-//! \brief the rotorCalbration structure describes a calibration experiment and associated data
-struct rotorCalibration
-{
-   int ID;
-   QString GUID;
-   QString rotorGUID;   //!< The GUID of the rotor this calibration is associated with
-   QString calibrationExperimentGUID; //!< The GUID of the experiment that contains the calibration data
-   double coeff1;       //!< The first order coefficient for the second order polynomial
-   double coeff2;       //!< The second order coefficient for the second order polynomial
-                        //!< the zeroth order coefficient is always zero (no stretch at 0 RPM)
-   QString report;      //!< a calibration report 
-   QDate lastUpdated;   //!< the date on which the rotor was calibrated
-   double omega2t;      //!< the cumulative force exerted on the rotor
-};
-
+#include "us_abstractrotor_gui.h"
 
 /*! \class US_RotorGui
    This class provides an interface to the Rotor tables of USLIMS and
@@ -211,64 +171,4 @@ class US_EXTERN US_RotorGui : public US_WidgetsDialog
       void newRotor           ( void );
 };
 
-class US_EXTERN US_AbstractRotorGui : public US_WidgetsDialog
-{
-
-   Q_OBJECT
-         
-   public:
-
-      //! \brief Generic constructor for the US_AbstractRotorGui class used to add a new rotor.
-      
-      US_AbstractRotorGui(abstractRotor *, rotor *);
-      /*! 
-          \param abstractRotor The abstractRotor structure from which the rotor will be derived
-          \param rotor The Rotor structure that will be added to the database
-      */
-      int rotorID;
-
-      abstractRotor     *currentAbstractRotor;
-      rotor             *currentRotor;
-      
-      QPushButton       *pb_help;
-      QPushButton       *pb_reset;
-      QPushButton       *pb_accept;
-      QPushButton       *pb_close;
-      
-      QRadioButton      *rb_db;
-      QRadioButton      *rb_disk;
-
-      QLineEdit         *le_name;
-      QLineEdit         *le_serialNumber;
-      
-      QComboBox         *cb_rotors;
-      
-      QTextEdit         *te_details;
-
-      QVector <abstractRotor> abstractRotorList;
-
-      US_Help           showHelp;
-      
-      //! A destructor.
-      ~US_AbstractRotorGui();
-
-   private slots:
-
-      void setupGui           ( void );
-      void select             ( void );
-      void reset              ( void );
-      void check_db           ( void );
-      void check_disk         ( void );
-      bool loadAbstractRotors ( void );
-      void changeRotor        ( int  );
-      int  getIndex           ( const QString & );
-      void connect_error      ( const QString & );
-      void help (void)
-      {
-         showHelp.show_help( "manual/abstractrotor.html" );
-      };
-      void setAbstractRotorInfo ( int id );
-      void updateName           ( const QString & );
-      void updateSerialNumber   ( const QString & );
-};
 #endif
--- trunk/programs/us_rotor_gui/us_rotor_gui.pro
+++ trunk/programs/us_rotor_gui/us_rotor_gui.pro
@@ -4,5 +4,5 @@ TARGET  = us_rotor_gui
 QT      += xml
 CONFIG  += debug
 
-HEADERS = us_rotor_gui.h
-SOURCES = us_rotor_gui.cpp    
+HEADERS = us_rotor_gui.h 		us_abstractrotor_gui.h
+SOURCES = us_rotor_gui.cpp    us_abstractrotor_gui.cpp
