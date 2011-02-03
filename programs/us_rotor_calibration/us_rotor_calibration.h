26ae3f5ef71ae7f5493b1c1dafd1d61544a67ed9
zollarsd <zollarsd@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2011-02-03 21:25:25 +0000

    Modified to work with revised us_rotor classes

--- programs/us_rotor_calibration/us_rotor_calibration.h
+++ programs/us_rotor_calibration/us_rotor_calibration.h
@@ -39,20 +39,16 @@ class US_EXTERN US_RotorCalibration : public US_Widgets
 	Q_OBJECT
 
 	public:
-		US_RotorCalibration();
-		~US_RotorCalibration();
+  		US_RotorCalibration();
+	  	~US_RotorCalibration();
 
 	private:
 
-      US_DataIO2::RawData            data;
-      QVector< US_DataIO2::RawData > allData;
-      QVector <Average> avg;
-      QVector <QVector <double> > reading;
-      QVector <double> stretch_factors, std_dev;
-      QVector <Limit> limit;
-
       double             left, right, top, bottom, coef[3];
-      double             *x, *y, *sd1, *sd2;
+      double*            x;
+      double*            y;
+      double*            sd1;
+      double*            sd2;
       int                maxcell, current_triple, current_cell;
       bool               top_of_cell, newlimit;
       QString            rotor, fileText, current_channel;
@@ -66,7 +62,6 @@ class US_EXTERN US_RotorCalibration : public US_Widgets
       QPushButton*       pb_calculate;
       QPushButton*       pb_save;
       QPushButton*       pb_load;
-      QPushButton*       pb_loadRotor;
       QPushButton*       pb_view;
             
       QString            workingDir;
@@ -89,38 +84,40 @@ class US_EXTERN US_RotorCalibration : public US_Widgets
       US_PlotPicker*     pick;
       US_Plot*           plot;
       
-//      QLabel*            lbl_instructions;
-//      QLabel*            lbl_spacer;
-
       QLineEdit*         le_instructions;
-      QLineEdit*         le_rotorInfo;
                         
       QRadioButton*      rb_channel;
       QRadioButton*      rb_top;
       QRadioButton*      rb_bottom;
 
       QCheckBox*         cb_assigned;
+      QComboBox*         cb_lab;
       
-   public slots:
-      void help (void)
-      {
-         showHelp.show_help( "manual/rotor_calibration.html" );
-      };
-      void reset(void);
-      void load(void);
-      void plotAll(void);
-      void currentRect(QwtDoubleRect);
-      void findTriple(void);
-      void next(void);
-      void calculate(void);
-      double findAverage(QwtDoubleRect, US_DataIO2::RawData, int);
-      void save(void);
-      void view(void);
-      void loadRotor(void);
-      void update_used();
-      void update_cell(double);
-      void update_channel(double);
-      void update_position();
-      void update_plot();
+      US_DataIO2::RawData            data;
+      QVector< US_DataIO2::RawData > allData;
+      QVector< Average >             avg;
+      QVector< QVector < double > >  reading;
+      QVector< double >              stretch_factors, std_dev;
+      QVector< Limit >               limit;
+
+   private slots:
+      void       reset(void);
+      void       load(void);
+      void       plotAll(void);
+      void       currentRect(QwtDoubleRect);
+      void       findTriple(void);
+      void       next(void);
+      void       calculate(void);
+      double     findAverage(QwtDoubleRect, US_DataIO2::RawData, int);
+      void       save(void);
+      void       view(void);
+      void       update_used();
+      void       update_cell(double);
+      void       update_channel(double);
+      void       update_position();
+      void       update_plot();
+      void       help (void)
+         { showHelp.show_help( "manual/rotor_calibration.html" ); };
+
 };
 #endif
