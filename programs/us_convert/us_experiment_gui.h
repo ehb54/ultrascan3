f727544d3bbd5a86f0a39005ed1350529979badc
zollarsd <zollarsd@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2011-03-29 20:59:48 +0000

    Code to ensure the label in us_experiment is there before accepting

--- programs/us_convert/us_experiment_gui.h
+++ programs/us_convert/us_experiment_gui.h
@@ -88,6 +88,7 @@ class US_EXTERN US_ExperimentGui : public US_WidgetsDialog
       void selectProject     ( void );
       void assignProject     ( US_Project& );
       void cancelProject     ( void );
+      void saveLabel         ( const QString& );
       QComboBox* us_expTypeComboBox         ( void );
       void setInstrumentList ( void );
       void setOperatorList   ( void );
