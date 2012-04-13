f25c15d22edf229655ca18b23e70e00bc10bfd0a
Gary E. Gorbet <gegorbet@gmail.com>
2012-04-13 22:52:31 +0000

    fix for Win32 us3-somo builds

--- develop/include/us_math.h
+++ develop/include/us_math.h
@@ -175,7 +175,7 @@ int nnls(double *a, int a_dim1, int m, int n, double *b, double *x,
    double stretch(int /*rotor id*/, unsigned int /*rotor speed*/);
    double stretch_with_rotor_list(int rotor, unsigned int rpm, vector < rotorInfo > *rotor_list);
 
-#ifdef WIN32
+#if !defined(QT4) && defined(WIN32)
   /* The following was derived from glibc source for IA32 architecture. */
 int __fpclassifyf (float x);
 
