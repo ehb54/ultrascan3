7f2a6fb91ba1313c39344dc7ce42b62589f2f8d9
Gary E. Gorbet <gegorbet@gmail.com>
2012-08-08 21:28:12 +0000

    Added ability to use threads to speed Lamm Equation calculations

---
+++ programs/us_fematch/us_thread_worker.h
@@ -0,0 +1,60 @@
+//! \file us_thread_worker.h
+#ifndef US_THREAD_WORKER_H
+#define US_THREAD_WORKER_H
+
+#include <QtCore>
+
+#include "us_extern.h"
+#include "us_dataIO2.h"
+#include "us_simparms.h"
+#include "us_model.h"
+#include "us_noise.h"
+#include "us_buffer.h"
+
+#ifndef DbgLv
+#define DbgLv(a) if(dbg_level>=a)qDebug()
+#endif
+
+//! \brief Worker thread to do Lamm equation calculations for us_fematch
+
+/*! \class ThreadWorker
+    This class is for each of the individual worker threads that do the
+    actual work of FeMatch analysis using a partitioned model.
+*/
+class ThreadWorker : public QObject
+{
+   Q_OBJECT
+
+   public:
+      ThreadWorker( US_Model&, US_SimulationParameters&,
+            US_DataIO2::RawData&, US_Buffer&, int );
+
+   public slots:
+      void calc_simulation();   // Where the real work is done
+      void forward_progress( int  );
+
+   signals:
+      void work_progress   ( int, int );
+      void work_complete   ( int );
+
+   private:
+      US_Model&                 model;        // Model for thread
+      US_SimulationParameters&  simparams;    // Simulation parameters
+      US_DataIO2::RawData&      simdat;       // Simulation data (pre-inited)
+      US_Buffer&                buffer;       // Buffer (density,compress)
+      int                       thrn;         // thread number (0,...)
+
+      int  dbg_level;           // debug flag
+};
+
+/*! \class QThreadEx
+    A special QThread that calls exec() to start an event loop
+*/
+class QThreadEx : public QThread
+{
+   protected:
+      void run() { exec(); }
+};
+
+#endif
+
--- programs/us_mpi_analysis/revision.sh
+++ programs/us_mpi_analysis/revision.sh
@@ -1,7 +1,20 @@
+#  revision.sh  - fill us_revision.h with lastest revision string
 REV1=`svn info|grep 'Changed Rev'`
+REV2=`(cd ../../utils;svn info|grep 'Changed Rev')`
+# Compare revisions of programs/us_mpi_analysis and utils
+NREV1=`echo $REV1|cut -f4 -d' '`
+NREV2=`echo $REV2|cut -f4 -d' '`
+NREV1=`expr $NREV1 + 0`
+NREV2=`expr $NREV2 + 0`
+if [ $NREV2 -gt $NREV1 ]; then
+  # If utils is latest, use its revision to store in the header
+  REV1=$REV2
+fi
+
+# Get the current header revision string and compare to svn-based string
 REV2=`cut -f2 -d\" us_revision.h`
-if [ "$REV1" != "$REV2" ]
-then
+if [ "$REV1" != "$REV2" ]; then
+  # If svn shows revision later than header, report and store that one
   echo "REVISION WAS -- \"$REV2\""
   echo "REVISION NOW -- \"$REV1\""
   echo "#define REVISION \"$REV1\"" > us_revision.h
