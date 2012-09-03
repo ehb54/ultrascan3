10a2d3f387706ce72bd443f5581ce546e1a108f3
zollarsd <zollarsd@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2012-09-03 21:50:34 +0000

    Database changes for 2DSA CG submit method

--- sql/us3.sql
+++ sql/us3.sql
@@ -827,7 +827,7 @@ CREATE TABLE IF NOT EXISTS HPCAnalysisRequest (
   editXMLFilename varchar(255) NOT NULL default '',
   submitTime datetime NOT NULL default '0000-00-00 00:00:00',
   clusterName varchar(80) default NULL,
-  method enum('2DSA','2DSA_MW','GA','GA_MW','GA_SC') NOT NULL default '2DSA',
+  method enum('2DSA','2DSA_CG','2DSA_MW','GA','GA_MW','GA_SC') NOT NULL default '2DSA',
   PRIMARY KEY (HPCAnalysisRequestID) )
 ENGINE=InnoDB;
 
@@ -963,6 +963,33 @@ CREATE TABLE IF NOT EXISTS 2DSA_Settings (
   PRIMARY KEY  (2DSA_SettingsID),
   INDEX ndx_2DSA_Settings_HPCAnalysisRequestID (HPCAnalysisRequestID ASC),
   CONSTRAINT fk_2DSA_Settings_HPCAnalysisRequestID
+    FOREIGN KEY (HPCAnalysisRequestID)
+    REFERENCES HPCAnalysisRequest (HPCAnalysisRequestID)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION )
+ENGINE=InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table structure for table 2DSA_CG_Settings
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS 2DSA_CG_Settings;
+
+CREATE TABLE IF NOT EXISTS 2DSA_CG_Settings (
+  2DSA_CG_SettingsID int(11) NOT NULL AUTO_INCREMENT,
+  HPCAnalysisRequestID int(11) NOT NULL,
+  CG_modelID int(11) NOT NULL,    -- maps to model with 'CustomGrid...' description
+  uniform_grid int(11) NOT NULL default '6',
+  mc_iterations int(11) NOT NULL default 1,
+  tinoise_option tinyint(1) NOT NULL default '0',
+  regularization int(11) NOT NULL default '0',
+  meniscus_range double NOT NULL default '0.01',
+  meniscus_points double NOT NULL default '3',
+  max_iterations int(11) NOT NULL default 1,
+  rinoise_option tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (2DSA_CG_SettingsID),
+  INDEX ndx_2DSA_CG_Settings_HPCAnalysisRequestID (HPCAnalysisRequestID ASC),
+  CONSTRAINT fk_2DSA_CG_Settings_HPCAnalysisRequestID
     FOREIGN KEY (HPCAnalysisRequestID)
     REFERENCES HPCAnalysisRequest (HPCAnalysisRequestID)
     ON DELETE CASCADE
