89097532b7f361b6118aff338b4c77f3cc4082c3
demeler <7659499+demeler@users.noreply.github.com>
2013-01-28 03:48:39 +0000

    Generic plotting program to check function shape

---
+++ programs/us_2dplot/us_2dplot.h
@@ -0,0 +1,58 @@
+#ifndef US_2DPLOT_H
+#define US_2DPLOT_H
+
+#include <QtGui>
+#include <QApplication>
+
+#include "us_extern.h"
+#include "us_widgets.h"
+#include "us_plot.h"
+
+class US_2dPlot : public US_Widgets
+{
+	Q_OBJECT
+
+	public:
+  		US_2dPlot();
+
+	private:
+
+      QVector <double>   x, y;
+
+      double             resolution;
+      double             s_min;
+      double             s_max;
+      double             par1;
+      double             par2;
+      double             par3;
+      double             par4;
+
+      QPushButton*       pb_calculate;
+      QPushButton*       pb_close;
+
+      QLabel*            lbl_par1;
+      QLabel*            lbl_par2;
+      QLabel*            lbl_par3;
+      QLabel*            lbl_par4;
+      QLabel*            lbl_resolution;
+            
+      QwtPlot*           data_plot;
+      QwtPlotCurve*      f_curve;
+      QwtCounter*        ct_par1;
+      QwtCounter*        ct_par2;
+      QwtCounter*        ct_par3;
+      QwtCounter*        ct_par4;
+      QwtCounter*        ct_resolution;
+      
+
+      US_Plot*           plot;
+      
+   private slots:
+      void       calculate         ( void );
+      void       update_par1       ( double );
+      void       update_par2       ( double );
+      void       update_par3       ( double );
+      void       update_par4       ( double );
+      void       update_resolution ( double );
+};
+#endif
---
+++ programs/us_2dplot/us_2dplot.pro
@@ -0,0 +1,7 @@
+include( ../../gui.pri )
+
+TARGET        = us_2dplot
+
+HEADERS       = us_2dplot.h
+
+SOURCES       = us_2dplot.cpp
