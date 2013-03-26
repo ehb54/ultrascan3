1cad8cd9c7d3e73fe0826c468b071a2e6859b406
Gary E. Gorbet <gegorbet@gmail.com>
2013-03-26 16:27:18 +0000

    Mods to default clusterAuthorizations

--- sql/us3.sql
+++ sql/us3.sql
@@ -33,7 +33,7 @@ CREATE  TABLE IF NOT EXISTS people (
   activated TINYINT(1) NOT NULL DEFAULT false ,
   signup TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
   lastLogin DATETIME NULL ,
-  clusterAuthorizations VARCHAR(255) NOT NULL default 'bcf:bcf-local:alamo:alamo-local:lonestar:ranger',
+  clusterAuthorizations VARCHAR(255) NOT NULL default 'lonestar:stampede:trestles:bcf:alamo'
   userlevel TINYINT NOT NULL DEFAULT 0 ,
   advancelevel TINYINT NOT NULL DEFAULT 0 ,
   PRIMARY KEY (personID) )
