66d760361ddabebcc206451a77dde788fa0d03a1
demeler <7659499+demeler@users.noreply.github.com>
2014-08-25 00:38:53 +0000

    updates

--- programs/us_buoyancy/us_buoyancy.h
+++ programs/us_buoyancy/us_buoyancy.h
@@ -10,32 +10,38 @@
 #include "us_help.h"
 #include "us_plot.h"
 #include "us_dataIO.h"
+#include "us_simparms.h"
 
 class US_Buoyancy : public US_Widgets
 {
 	Q_OBJECT
 
 	public:
-		US_Buoyancy();
+      US_Buoyancy();
 
 	private:
 
       US_DataIO::RawData            data;
       QVector< US_DataIO::RawData > allData;
-		QList< US_DataIO::SpeedData > sData;
+      QList< US_DataIO::SpeedData > sData;
 
       int                current_triple;
       int                current_scan;
       int                total_speeds;
       int                dbg_level;
 
-		bool					 expIsBuoyancy;
+      bool               expIsBuoyancy;
 
       double             meniscus;
-		QList< int >       sd_offs;    // speed data offsets, ea. triple
-		QList< int >       sd_knts;    // speed data counts, ea. triple
+      double             dens_0;
+      double             vbar;
+      double             MW;
+
+      QList< int >       sd_offs;    // speed data offsets, ea. triple
+      QList< int >       sd_knts;    // speed data counts, ea. triple
 
       US_Help            showHelp;
+      QVector <US_SimulationParameters> simparams;
 
       QString            workingDir;
       QString            runID;
@@ -46,7 +52,7 @@ class US_Buoyancy : public US_Widgets
       QStringList        triples;
       QStringList        trip_rpms;
 
-	  	QPushButton*       pb_details;
+      QPushButton*       pb_details;
 
       QwtPlot*           data_plot;
       QwtPlotCurve*      v_line;
@@ -57,9 +63,15 @@ class US_Buoyancy : public US_Widgets
       US_Plot*           plot;
 
       QLabel*            lbl_rpms;
+      QLabel*            lbl_dens_0;
+      QLabel*            lbl_vbar;
+      QLabel*            lbl_MW;
 
       QLineEdit*         le_info;
       QLineEdit*         le_investigator;
+      QLineEdit*         le_dens_0;
+      QLineEdit*         le_vbar;
+      QLineEdit*         le_MW;
 
       US_Disk_DB_Controls* disk_controls; //!< Radiobuttons for disk/db choice
 
@@ -84,6 +96,9 @@ private slots:
 	void new_rpmval        ( int  );
 	void set_meniscus      ( void );
 	void update_speedData  ( void );
+	void update_dens_0     ( void );
+	void update_vbar       ( void );
+	void update_MW         ( void );
 	void help              ( void )
       { showHelp.show_help( "manual/us_buoyancy.html" ); };
 };
