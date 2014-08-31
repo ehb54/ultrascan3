3404d16e65770ae7b0ace38a19132270bf66e5df
gegorbet <gegorbet@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2014-08-31 01:05:44 +0000

    re-enabled ClipData class to optionally set data range limits

--- trunk/programs/us_astfem_sim/us_clipdata.cpp
+++ trunk/programs/us_astfem_sim/us_clipdata.cpp
@@ -5,8 +5,6 @@ US_ClipData::US_ClipData( double& concentration, double& radius,
       double meniscus, double loading, QWidget* p, Qt::WindowFlags f ) 
   : US_WidgetsDialog( p, f ), conc( concentration ), rad( radius )
 {
-   setWindowTitle( tr( "Data Range Selection - "
-                       "UltraScan Finite Element Simulation" ) );
    setPalette    ( US_GuiSettings::frameColor() );
    setAttribute  ( Qt::WA_DeleteOnClose );
         
@@ -14,49 +12,73 @@ US_ClipData::US_ClipData( double& concentration, double& radius,
    main->setContentsMargins ( 2, 2, 2, 2 );
    main->setSpacing( 2 );
 
-   int row = 0;
+   setWindowTitle( tr( "Select Finite Element Simulation"
+                       " Data Range" ) );
 
-   QLabel* lb_info = us_banner( "Please select the Cropping Range\n"
-                                "for the Data to be saved" );
-   main->addWidget( lb_info, row++, 0, 1, 2 );
+   QLabel* lb_info    = us_banner( tr( "Please select the Cropping Range\n"
+                                       "for the Data to be saved" ) );
 
-   QLabel* lb_conc = us_label (tr( "Maximum Concentration\n"
-                                   "( 0 = entire range ):") );
-   main->addWidget( lb_conc, row, 0, 2, 1 );
+   QLabel* lb_conc    = us_label ( tr( "Maximum Concentration:" ) );
 
    QString s;
    s.sprintf( "%4.2f", loading );
 
-   QLabel* lb_loading = us_label( tr( "( Loading concentration: " ) + s + " )" );
-   main->addWidget( lb_loading, row, 1 );
+   QLabel* lb_loading = us_label( tr( "( Loading concentration: " )
+                        + QString().sprintf( "%4.2f" , loading ) + " )" );
 
    ct_conc = us_counter( 2, loading, conc, loading * 2 );
    ct_conc->setStep( 0.1 );
-   main->addWidget( ct_conc, row + 1, 1 );
-   row += 2;
 
-   QLabel* lb_rad = us_label( tr( "Maximum Radius:" ) );
-   main->addWidget( lb_rad, row, 0 );
+   QLabel* lb_rad     = us_label( tr( "Maximum Radius:" ) );
 
    ct_rad = us_counter( 2, meniscus, rad, rad ); 
    ct_rad->setStep( 0.01 );
-   main->addWidget( ct_rad, row++, 1 );
 
-   QBoxLayout* buttons = new QHBoxLayout();
-
-   QPushButton* pb_help = us_pushbutton( tr( "Help" ) );
-   connect( pb_help, SIGNAL( clicked() ), SLOT( help() ) );
-   buttons ->addWidget( pb_help );
-
+   QBoxLayout* buttons    = new QHBoxLayout();
+   QPushButton* pb_help   = us_pushbutton( tr( "Help" ) );
    QPushButton* pb_cancel = us_pushbutton( tr( "Cancel" ) );
-   connect( pb_cancel, SIGNAL( clicked() ), SLOT( reject() ) );
-   buttons ->addWidget( pb_cancel );
-
    QPushButton* pb_accept = us_pushbutton( tr( "OK" ) );
-   connect( pb_accept, SIGNAL( clicked() ), SLOT( ok() ) );
-   buttons ->addWidget( pb_accept );
 
-   main->addLayout( buttons, row++, 0, 1, 2 );
+   connect( pb_help,   SIGNAL( clicked() ), SLOT( help()   ) );
+   connect( pb_cancel, SIGNAL( clicked() ), SLOT( reject() ) );
+   connect( pb_accept, SIGNAL( clicked() ), SLOT( ok()     ) );
+
+   buttons->addWidget( pb_help );
+   buttons->addWidget( pb_cancel );
+   buttons->addWidget( pb_accept );
+
+   QTextEdit*   te_guide  = us_textedit();
+   us_setReadOnly( te_guide, true );
+   te_guide->setTextColor( Qt::blue );
+   te_guide->setText( "<div style='color:blue'>" +
+         tr( "To accept the data as computed, with no"
+             " limits on amplitude or radius range,"
+             " click on the <b>Cancel</b> button.<br/><br/>"
+             "To impose amplitude or radius limits on"
+             " exported data, set the maximum concentration"
+             " value and/or maximum radius and then click"
+             " on the <b>OK</b> button.<br/><br/>"
+             "( The current maximum OD is %1 )." ).arg( conc )
+           + "</div>" );
+
+   int row = 0;
+   main->addWidget( lb_info,    row++, 0, 1, 2 );
+   main->addWidget( lb_loading, row++, 0, 1, 2 );
+   main->addWidget( lb_conc,    row,   0, 1, 1 );
+   main->addWidget( ct_conc,    row++, 1, 1, 1 );
+   main->addWidget( lb_rad,     row,   0, 1, 1 );
+   main->addWidget( ct_rad,     row++, 1, 1, 1 );
+   main->addLayout( buttons,    row++, 0, 1, 2 );
+   main->addWidget( te_guide,   row++, 0, 5, 2 );
+
+   adjustSize();
+qDebug() << "(1) size" << size();
+   int rowht  = ct_conc->height();
+   int wwid   = width();
+   int whgt   = rowht * ( row + 7 );
+qDebug() << "(2) size" << QSize( wwid, whgt );
+   resize( wwid, whgt );
+qDebug() << "(3) size" << size();
 }
 
 void US_ClipData::ok( void )
