edef8f18ba9ad5d00669813fd66cc780967753bb
demeler <7659499+demeler@users.noreply.github.com>
2015-01-25 14:47:27 +0000

    corrected lower limit for frictional ratio - needs to allow values less than 1.0 for optimal display.

--- programs/us_pseudo3d_combine/us_pseudo3d_combine.cpp
+++ programs/us_pseudo3d_combine/us_pseudo3d_combine.cpp
@@ -1350,7 +1350,7 @@ qDebug() << "select-y: plot_y" << plot_y;
    lb_plt_kmax->setText( tr( "Plot Limit " ) + ylabs[ plot_y ]
                        + tr( " Maximum:" ) );
 qDebug() << "  ylab" << ylabs[plot_y];
-   ct_plt_kmin->setRange( ymins[ plot_y ], ymaxs[ plot_y ], yincs[ plot_y ] );
+   ct_plt_kmin->setRange( 0.1, 50, 0.1 );
    ct_plt_kmax->setRange( ymins[ plot_y ], ymaxs[ plot_y ], yincs[ plot_y ] );
    ct_plt_kmin->setValue( yvlos[ plot_y ] );
    ct_plt_kmax->setValue( yvhis[ plot_y ] );
