9e5b4d1a7536f43e8b432d3282060e46731a724d
minji <minji@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2015-12-16 19:04:11 +0000

    Added us_minimize to the gui library

---
+++ trunk/gui/us_minimize.h
@@ -0,0 +1,152 @@
+#ifndef US_MINIMIZE_H
+#define US_MINIMIZE_H
+//#include <QApplication>
+//#include <QtGui>
+
+#include "us_settings.h"
+#include "us_widgets.h"
+#include "us_plot.h"
+#include "us_util.h"
+#include "us_math2.h"
+#include "us_matrix.h"
+
+class US_Minimize : public US_Widgets
+{
+	Q_OBJECT
+	
+	public:
+		
+		US_Minimize(bool& temp_fitting_widget, bool temp_GUI, QWidget*&, const char*&);
+		~US_Minimize();
+      unsigned int parameters, iteration, function_evaluations, decompositions;
+      unsigned long int points, runs, datasets, firstScan, maxIterations;
+      unsigned int column1, column2, column3, span, totalSteps;
+      int nlsMethod, return_value;
+      unsigned int plotGroup;
+
+		bool *fitting_widget, plotResiduals, showGuiFit, GUI, constrained;
+      bool autoconverge;
+      bool suspend_flag, aborted, converged, completed, first_plot, init_simulation;
+      float *y_raw;
+      float lambdaStart, lambdaStep, runs_percent, variance, tolerance;
+      double **LL_transpose, *B, *y_guess, **jacobian, *guess,  *test_guess,
+           **information_matrix, *y_delta;
+      int buttonh;
+
+		US_Settings* settings;
+		US_Matrix*	matrix;
+	
+      QLabel	*lbl_header;
+      QLabel	*lbl_iteration;
+      QLabel	*lbl_variance;
+      QLabel	*lbl_stddev;
+      QLabel	*lbl_improvement;
+      QLabel	*lbl_tolerance;
+      QLabel	*lbl_evaluations;
+      QLabel	*lbl_decompositions;
+      QLabel	*lbl_currentLambda;
+      QLabel	*lbl_parameters;
+      QLabel	*lbl_datasets;
+      QLabel	*lbl_points;
+      QLabel	*lbl_constrained;
+		QLabel	*lbl_autoconverge;
+      QLabel	*lbl_showGuiFit;
+      QCheckBox	*cb_showGuiFit;
+      QLabel	*lbl_progress;
+      QLabel	*lbl_nlsMethod;
+      QLabel	*lbl_controls1;
+      QLabel	*lbl_controls2;
+		QLabel	*lbl_lambdaStart;
+		QLabel	*lbl_lambdaStep;
+		QLabel	*lbl_maxIterations;
+		QLabel	*lbl_global;
+      QLineEdit	*le_iteration;
+		QLineEdit	*le_variance;
+		QLineEdit	*le_stddev;
+		QLineEdit	*le_improvement;
+		QLineEdit	*le_evaluations;
+		QLineEdit	*le_decompositions;
+      QLineEdit	*le_currentLambda;
+		QLineEdit	*le_parameters;
+      QLineEdit	*le_datasets;
+		QLineEdit	*le_points;
+		QLineEdit	*le_lambdaStart;
+      QLineEdit	*le_maxIterations;
+      QLineEdit	*le_lambdaStep;
+      QLineEdit	*le_tolerance;
+	   QProgressBar *pgb_progress;
+      QRadioButton *bt_plotGroup;
+      QRadioButton *bt_plotSingle;
+      QRadioButton *bt_plotAll;
+      QRadioButton *bt_constrained;
+      QRadioButton *bt_unconstrained;
+      QRadioButton *bt_autoconverge;
+      QRadioButton *bt_manualconverge;
+      QPushButton *pb_fit;
+      QPushButton *pb_pause;
+      QPushButton *pb_resume;
+      QPushButton *pb_close;
+      QPushButton *pb_help;
+      QPushButton *pb_report;
+      QPushButton *pb_overlays;
+      QPushButton *pb_residuals;
+      QPushButton *pb_saveFit;
+      QPushButton *pb_print;
+      QPushButton *pb_plottwo;
+      QPushButton *pb_plotthree;
+      QPushButton *pb_plotfour;
+      QPushButton *pb_plotfive;
+      
+		QComboBox 	*cbb_nlsMethod;
+		QButtonGroup	*bg1;
+		QButtonGroup	*bg2;
+		QButtonGroup	*bg3;		
+		QwtCounter	*ct_unlabeled;
+		QCheckBox	*ck_monitor;
+		QRadioButton	*rb_plot1;
+		QRadioButton	*rb_plot2;
+		QRadioButton	*rb_plot3;
+		QRadioButton	*rb_linearN;
+		QRadioButton	*rb_linearY;
+		QRadioButton	*rb_autoN;
+		QRadioButton	*rb_autoY;
+		QComboBox	*cb_nlsalg;
+		US_Plot*		plotLayout;
+		QwtPlot	 	*data_plot;
+
+	protected slots:
+	float calc_testParameter(float **, float);
+	float linesearch(float**, float);
+	int calc_B();
+	void updateQN(float **, float**);
+	int Fit();
+
+//Virtual functions:
+
+	protected slots:
+	virtual float calc_residuals();
+	virtual int calc_model(double *);
+	virtual void cleanup();
+	virtual bool fit_init();
+	virtual void plot_residuals();
+	virtual void plot_overlays();
+	virtual int calc_jacobian();
+	virtual void endFit();
+
+	private slots:
+	void setup_GUI();
+	void save_Fit();
+
+	signals:
+   void hasConverged();
+   void currentStatus(const QString &);
+   void parametersUpdated();
+   void newParameters();
+   void fitSuspended();
+   void fitAborted();
+   void fitResumed();
+   void fitStarted();
+   void fittingWidgetClosed();
+};
+
+#endif
