70abe557eb4918df12e458716e53afe23966519a
demeler <demeler@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2016-04-09 23:07:21 +0000

    Cleanup of code for Modifications from Minji

---
+++ trunk/programs/us_extinction/us_extinctfitter.h
@@ -0,0 +1,60 @@
+#ifndef US_EXTINCTFITTER_H
+#define US_EXTINCTFITTER_H
+
+#include <QApplication>
+#include <QtGui>
+#include "us_editor.h"
+#include "us_minimize.h"
+
+//Structures needed for each file
+struct Reading
+{
+   float lambda, od;
+};
+
+struct WavelengthScan
+{
+   QVector<Reading> v_readings;
+   QString fileName, filePath;
+   QString description;
+	int pos, neg, runs;
+};
+
+
+class US_ExtinctFitter : public US_Minimize
+{
+	Q_OBJECT
+	
+	public:
+
+	US_ExtinctFitter(QVector <struct WavelengthScan> *, double*&, unsigned int&, unsigned int&, QString&, bool&);
+	
+	private: 
+		QVector <struct WavelengthScan> *wls_v;
+		QString projectName;
+		US_Editor *e;
+		QString htmlDir;
+		unsigned int order;
+	
+	private slots:
+		void cleanup();
+		int calc_jacobian();
+
+	public slots:
+		bool fit_init();
+		int calc_model(double *);
+		void view_report();
+		void write_report();
+		void plot_overlays();
+		void plot_residuals();
+      void updateRange(double scan);
+      void endFit();
+      void saveFit();
+      void startFit();
+
+   signals:
+
+      void dataSaved(const QString &, const int);   //needed to save a list of used scans
+};
+
+#endif
