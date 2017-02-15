a90e3747a377bc30e2ef1ff48b12c495b40a289e
gegorbet <gegorbet@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2017-02-15 14:41:55 +0000

    more us_experiment progress, mainly with PhotoMult and Upload panels

--- trunk/gui/us_extinction_gui.h
+++ trunk/gui/us_extinction_gui.h
@@ -11,100 +11,99 @@
 
 class US_GUI_EXTERN US_Extinction : public US_Widgets
 {
-	Q_OBJECT
+   Q_OBJECT
 
-	public:
-		US_Extinction();
+   public:
+      US_Extinction();
 
-		US_Extinction(QString temp_mode, const QString &text, QWidget *parent);
+      US_Extinction(QString temp_mode, const QString &text, QWidget *parent);
 
-		QVector <QString> filenames;
-		US_ExtinctFitter *fitter;
-		US_Disk_DB_Controls* disk_controls;
-		US_Analyte currentAnalyte;
-		QVector <double> lambda;
-		QVector <double> extinction;
-		float xmin, xmax;
-		unsigned int order, parameters;
-		double *fitparameters;
-	   bool fitted, fitting_widget;
-	   QString buffer_temp;
-	   
+      QVector <QString> filenames;
+      US_ExtinctFitter *fitter;
+      US_Disk_DB_Controls* disk_controls;
+      US_Analyte currentAnalyte;
+      QVector <double> lambda;
+      QVector <double> extinction;
+      float xmin, xmax;
+      unsigned int order, parameters;
+      double *fitparameters;
+      bool fitted, fitting_widget;
+      QString buffer_temp;
 
-	private:
-   QVector <WavelengthScan> v_wavelength;
-   QVector <QwtPlotCurve*>  v_curve;
-   
-   QWidget * parent;
+   private:
+      QVector <WavelengthScan> v_wavelength;
+      QVector <QwtPlotCurve*>  v_curve;
 
-   QString projectName, filename_one, filename_two;
-	unsigned int maxrange;
-	float odCutoff, lambdaLimitLeft, lambdaLimitRight,lambda_min, lambda_max,
-         pathlength, extinction_coefficient, factor, selected_wavelength;
-	QLabel* 			lbl_gaussians;
-	QLabel* 			lbl_peptide;
-	QLabel*			lbl_wvinfo;
-	QLabel*			lbl_associate;
-	QLabel* 			lbl_cutoff;
-	QLabel* 			lbl_lambda1;
-	QLabel* 			lbl_lambda2;
-	QLabel* 			lbl_pathlength;
-	QLabel* 			lbl_coefficient;
-	QListWidget*	lw_file_names;
-	QPushButton* 	pb_addWavelength;
-	QPushButton* 	pb_reset;
-	QPushButton* 	pb_update;
+      QWidget * parent;
 
-	QPushButton* 	pb_perform;
-	QPushButton* 	pb_perform_buffer;
-	QPushButton* 	pb_accept;
+      QString projectName, filename_one, filename_two;
+      unsigned int maxrange;
+      float odCutoff, lambdaLimitLeft, lambdaLimitRight,lambda_min, lambda_max,
+            pathlength, extinction_coefficient, factor, selected_wavelength;
+      QLabel*      lbl_gaussians;
+      QLabel*      lbl_peptide;
+      QLabel*      lbl_wvinfo;
+      QLabel*      lbl_associate;
+      QLabel*      lbl_cutoff;
+      QLabel*      lbl_lambda1;
+      QLabel*      lbl_lambda2;
+      QLabel*      lbl_pathlength;
+      QLabel*      lbl_coefficient;
+      QListWidget* lw_file_names;
+      QPushButton* pb_addWavelength;
+      QPushButton* pb_reset;
+      QPushButton* pb_update;
 
-	QPushButton* 	pb_calculate;
-	QPushButton* 	pb_save;
-	QPushButton* 	pb_view;
-	QPushButton* 	pb_help;
-	QPushButton* 	pb_close;
+      QPushButton* pb_perform;
+      QPushButton* pb_perform_buffer;
+      QPushButton* pb_accept;
 
-	QLineEdit*	 	le_associate;
-	QLineEdit*	 	le_associate_buffer;
+      QPushButton* pb_calculate;
+      QPushButton* pb_save;
+      QPushButton* pb_view;
+      QPushButton* pb_help;
+      QPushButton* pb_close;
 
-	QLineEdit* 		le_odCutoff;
-	QLineEdit* 		le_lambdaLimitLeft;
-	QLineEdit* 		le_lambdaLimitRight;
-	QLineEdit* 		le_pathlength;
-	QLineEdit*		le_coefficient;
-	QwtCounter* 	ct_gaussian;
-	QwtCounter* 	ct_coefficient;
-	QwtPlotCurve* 	changedCurve;
-	US_Plot* 		plotLayout;
-	QwtPlot* 		data_plot;
-	QWidget*			p;
+      QLineEdit*   le_associate;
+      QLineEdit*   le_associate_buffer;
 
-	private slots:
-	bool 	loadScan(const QString&);
-	bool 	isComment(const QString&);	
-	void	add_wavelength	(void);
-	void 	reading(QStringList);
-	void	reset_scanlist (void);
-	void 	update_data(void);
+      QLineEdit*   le_odCutoff;
+      QLineEdit*   le_lambdaLimitLeft;
+      QLineEdit*   le_lambdaLimitRight;
+      QLineEdit*   le_pathlength;
+      QLineEdit*   le_coefficient;
+      QwtCounter*   ct_gaussian;
+      QwtCounter*   ct_coefficient;
+      QwtPlotCurve* changedCurve;
+      US_Plot*      plotLayout;
+      QwtPlot*      data_plot;
+      QWidget*      p;
 
-	void 	perform_global(void);
-	void 	perform_global_buffer(void);
-	void 	accept(void);
+   private slots:
+      bool    loadScan ( const QString& );
+      bool    isComment( const QString& );
+      void    add_wavelength( void );
+      void    reading( QStringList );
+      void    reset_scanlist( void );
+      void    update_data( void );
 
-	void 	calculateE280(void);
-	void 	save(void);
-	void 	view_result(void);
-	void 	help(void);
-	void 	plot();
-	void  calc_extinction();
-	void	update_order(double);
-	void 	listToCurve();
-	bool 	deleteCurve();
-	void	accessAnalyteExtinc(US_Analyte);
+      void    perform_global( void );
+      void    perform_global_buffer( void );
+      void    accept( void );
 
- signals:
-	void get_results(QMap < double, double > &xyz);	
+      void    calculateE280( void );
+      void    save( void );
+      void    view_result( void );
+      void    help( void );
+      void    plot();
+      void    calc_extinction();
+      void    update_order( double );
+      void    listToCurve();
+      bool    deleteCurve();
+      void    accessAnalyteExtinc( US_Analyte );
+
+    signals:
+      void    get_results( QMap < double, double > &xyz );
 
 };
-#endif	
+#endif
