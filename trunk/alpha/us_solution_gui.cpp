db3b24714b0fba65a2a2ab22579b88bb4c2e336b
alexey <alexey@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2017-06-15 02:27:28 +0000

    US_Solution: working on Select panel..

--- trunk/alpha/us_solution_gui.cpp
+++ trunk/alpha/us_solution_gui.cpp
@@ -36,6 +36,7 @@ US_SolutionMgrSelect::US_SolutionMgrSelect( int *invID, int *select_db_disk,
 
    lw_solutions = us_listwidget();
    lw_solutions-> setSortingEnabled( true );
+   
 
    pb_accept->setEnabled( false );
    pb_delete->setEnabled( false );
@@ -47,7 +48,7 @@ US_SolutionMgrSelect::US_SolutionMgrSelect( int *invID, int *select_db_disk,
    //QLabel* lb_solutionDesc = us_label( tr( "Solution Name:" ) );
    //le_solutionDesc = us_lineedit( "", 1 );
 
-   QLabel* lb_amount = us_label( tr( "Analyte Molar Ratio:" ) );
+   lb_amount = us_label( tr( "Analyte Molar Ratio:" ) );
    QLabel* lb_bufferInfo = us_label( tr( "Buffer Name:" ) );
    QLabel* lb_commonVbar20 = us_label( tr( "Common VBar (20C):" ) );
    QLabel* lb_density = us_label( tr( "Buffer density:" ) );
@@ -84,7 +85,9 @@ US_SolutionMgrSelect::US_SolutionMgrSelect( int *invID, int *select_db_disk,
    
    lw_analytes = us_listwidget();
    lw_analytes-> setSortingEnabled( true );
-   lw_analytes->setSelectionMode( QAbstractItemView::NoSelection );
+   connect( lw_analytes, SIGNAL( itemClicked  ( QListWidgetItem* ) ),   // To see Molar ratio for each analyte...
+                         SLOT  ( selectAnalyte( QListWidgetItem* ) ) );
+   //lw_analytes->setSelectionMode( QAbstractItemView::NoSelection );
    
    int row = 0;
    main->addWidget( bn_select,       row++, 0, 1, 12 );
@@ -117,9 +120,6 @@ US_SolutionMgrSelect::US_SolutionMgrSelect( int *invID, int *select_db_disk,
    main->addWidget( lb_storageTemp,  row,   6, 1, 2);
    main->addWidget( le_storageTemp,  row,   8, 1, 4);
    
-   //for ( int ii = 0; ii < 12; ii++ )  main->setColumnStretch( ii, 99 );
-   //for ( int ii = 0; ii < 12; ii++ )  main->setRowStretch( ii, 99 );
-
    //connect( lw_analytes, SIGNAL( itemClicked  ( QListWidgetItem* ) ),
    //                      SLOT  ( selectAnalyte( QListWidgetItem* ) ) );
    //connect( lw_analytes, SIGNAL( itemDoubleClicked  ( QListWidgetItem* ) ),
@@ -127,29 +127,210 @@ US_SolutionMgrSelect::US_SolutionMgrSelect( int *invID, int *select_db_disk,
 
    connect( le_search,      SIGNAL( textChanged( const QString& ) ),
 	    this,           SLOT  ( search     ( const QString& ) ) );
-   //connect( lw_solutions, SIGNAL( itemClicked    ( QListWidgetItem* ) ),
-   //	                    SLOT  ( selectSolution ( QListWidgetItem* ) ) );
+   connect( lw_solutions, SIGNAL( itemClicked    ( QListWidgetItem* ) ),
+   	                    SLOT  ( selectSolution ( QListWidgetItem* ) ) );
    connect( pb_help,        SIGNAL( clicked() ),
 	    this,           SLOT  ( help()    ) );
-
-   // connect( pb_cancel,      SIGNAL( clicked() ),
-   //          this,           SLOT  ( reject()  ) );
-   // connect( pb_accept,      SIGNAL( clicked()       ),
-   //          this,           SLOT  ( accept() ) );
-
-   // //connect( lw_solutions,   SIGNAL( itemSelectionChanged() ),
-   // //         this,           SLOT  ( select_solution()        ) );
+   connect( pb_cancel,      SIGNAL( clicked() ),
+            this,           SLOT  ( reject()  ) );
+   connect( pb_accept,      SIGNAL( clicked()       ),
+	    this,           SLOT  ( accept_solution() ) );
    
    // connect( pb_spectrum,    SIGNAL( clicked()  ),
    //          this,           SLOT  ( spectrum() ) );
    // connect( pb_delete,      SIGNAL( clicked()       ),
    //          this,           SLOT  ( delete_buffer() ) );
       
-   //init_solution();
+   // Load the solution descriptions
+   load();
+
+   // Select the current one if we know what it is
+   if ( solution->solutionID > 0 )
+   {
+      QList< QListWidgetItem* > items 
+        = lw_solutions->findItems( solution->solutionDesc, Qt::MatchExactly );
+
+      // should be exactly 1, but let's make sure
+      if ( items.size() == 1 )
+      {
+         selectSolution( items[ 0 ] );
+         lw_solutions->setCurrentItem( items[ 0 ] );
+      }
+   }
 
 }
 
-/*
+
+// Function to load solutions into solutions list widget
+void US_SolutionMgrSelect::load( void )
+{
+   from_db       = ( (*db_or_disk) == 1 );
+   
+   /*  In Buffers/Analytes - skjipped here ...
+   QString bguid = buffer->GUID;
+   QString bufid = buffer->bufferID;
+   int idBuf     = bufid.toInt();
+
+   if ( ( from_db  &&  idBuf < 0 )  ||
+        ( !from_db  &&  bguid.isEmpty() ) )
+   {
+      lw_buffer_list->setCurrentRow( -1 );
+   }
+   */
+   
+   if ( from_db )
+      loadDB();
+
+   else
+      loadDisk();
+
+}
+
+// Function to load solutions from disk
+void US_SolutionMgrSelect::loadDisk( void )
+{
+   QString path;
+   if ( ! solution->diskPath( path ) ) return;
+
+   IDs.clear();
+   descriptions.clear();
+   GUIDs.clear();
+   filenames.clear();
+
+   QDir dir( path );
+   QStringList filter( "S*.xml" );
+   QStringList names = dir.entryList( filter, QDir::Files, QDir::Name );
+
+   QFile a_file;
+
+   for ( int i = 0; i < names.size(); i++ )
+   {
+      a_file.setFileName( path + "/" + names[ i ] );
+
+      if ( ! a_file.open( QIODevice::ReadOnly | QIODevice::Text) ) continue;
+
+      QXmlStreamReader xml( &a_file );
+
+      while ( ! xml.atEnd() )
+      {
+         xml.readNext();
+
+         if ( xml.isStartElement() )
+         {
+            if ( xml.name() == "solution" )
+            {
+               QXmlStreamAttributes a = xml.attributes();
+
+               IDs          << a.value( "id" ).toString();
+               GUIDs        << a.value( "guid" ).toString();
+               filenames    << path + "/" + names[ i ];
+
+            }
+
+            else if ( xml.name() == "description" )
+            {
+               xml.readNext();
+               descriptions << xml.text().toString();
+            }
+         }
+      }
+
+      a_file.close();
+   }
+
+   loadSolutions();
+}
+
+// Function to load solutions from db
+void US_SolutionMgrSelect::loadDB( void )
+{
+   US_Passwd pw;
+   QString masterPW = pw.getPasswd();
+   US_DB2 db( masterPW );
+
+   if ( db.lastErrno() != US_DB2::OK )
+   {
+      db_error( db.lastError() );
+      return;
+   }
+
+   if ( investigatorID < 1 ) investigatorID = US_Settings::us_inv_ID();
+
+   QStringList q( "all_solutionIDs" );
+   q << QString::number( investigatorID );
+   db.query( q );
+
+   if ( db.lastErrno() != US_DB2::OK ) return;
+
+   IDs.clear();
+   descriptions.clear();
+   GUIDs.clear();
+   filenames.clear();
+
+   while ( db.next() )
+   {
+      QString newID = db.value( 0 ).toString();
+      IDs          << newID;
+      descriptions << db.value( 1 ).toString();
+      GUIDs        << QString( "" );
+      filenames    << QString( "" );
+   }
+
+   loadSolutions();
+}
+
+// Function to load the solutions list widget from the solutions data structure
+void US_SolutionMgrSelect::loadSolutions( void )
+{
+   lw_solutions->clear();
+   info.clear();
+   solutionMap.clear();
+
+   for ( int i = 0; i < descriptions.size(); i++ )
+   {
+      SolutionInfo si;
+      si.solutionID  = IDs         [ i ].toInt();
+      si.description = descriptions[ i ];
+      si.GUID        = GUIDs       [ i ];
+      si.filename    = filenames   [ i ];
+      si.index       = i;
+      info << si;
+
+      // Create a map to account for automatic sorting of the list
+      QListWidgetItem* item = new QListWidgetItem( descriptions[ i ], lw_solutions );
+      solutionMap[ item ] = i;
+
+      lw_solutions->addItem( item );
+   }
+
+}
+
+// Function to display an error returned from the database
+void US_SolutionMgrSelect::db_error( const QString& error )
+{
+   QMessageBox::warning( this, tr( "Database Problem" ),
+         tr( "Database returned the following error: \n" ) + error );
+}
+
+
+
+// Reject the selected solutiom and return to caller with no change
+void US_SolutionMgrSelect::reject( void )
+{
+   emit selectionCanceled();
+}
+// Accept the currently selected solutiom
+void US_SolutionMgrSelect::accept_solution( void )
+{
+   emit solutionAccepted();
+}
+// Accept a selected analyte for return to the caller
+void US_SolutionMgrSelect::accept( void )
+{
+}
+
+
+// Function to handle when analyte listwidget item is selected
 void US_SolutionMgrSelect::selectSolution( QListWidgetItem* item )
 {
    // Account for the fact that the list has been sorted
@@ -161,7 +342,6 @@ void US_SolutionMgrSelect::selectSolution( QListWidgetItem* item )
 
    int status = US_DB2::OK;
 
-   //if ( disk_controls->db() )
    if ( from_db )
    {
       US_Passwd pw;
@@ -232,237 +412,25 @@ void US_SolutionMgrSelect::selectSolution( QListWidgetItem* item )
    changed = false;
 }
 
-// Function to display an error returned from the database
-void US_SolutionMgrSelect::db_error( const QString& error )
-{
-   QMessageBox::warning( this, tr( "Database Problem" ),
-         tr( "Database returned the following error: \n" ) + error );
-}
-
-void US_SolutionMgrSelect::init_solution( void )
-{
- DbgLv(1) << "agS: init_a";
-   from_db       = ( (*db_or_disk) == 1 );
- DbgLv(1) << "agS: init_a  from_db" << from_db << "dkordk" << *db_or_disk;
-   QString aguid = solution->solutionGUID;
-   //QString anaid = solution->solutionID;
-   int anaid = solution->solutionID;
-   int idAna     = anaid.toInt();
- DbgLv(1) << "agS-initb: db_or_disk" << *db_or_disk << "from_db" << from_db;
-DbgLv(1) << "agS-initb:   anaID anaGUID" << anaid << aguid << idAna;
-
-   if ( ( from_db  &&  idAna < 0 )  ||
-        ( !from_db  &&  aguid.isEmpty() ) )
-   {
-      lw_solutions->setCurrentRow( -1 );
-      idAna         = -1;
-      //anaid         = "";
-      aguid         = "";
-   }
-
-   query();
-DbgLv(1) << "agS-initb:   post-q:anaID anaGUID" << anaid << aguid;
-
-   if ( ! aguid.isEmpty() )
-   {  // There is a selected solution, select a list item
-      if ( from_db ) // DB access
-      {
-         // Search for solutionID
-         for ( int ii = 0; ii < info.size(); ii++ )
-         {
-            if ( anaid == info[ ii ].solutionID )
-            {
-DbgLv(1) << "agS-initb:  ii" << ii << "match anaID"<< solution->solutionID;
-               lw_solutions->setCurrentRow( ii );
-               QListWidgetItem* item = lw_solutions->item( ii );
-               select_solution( item );
-               break;
-            }
-         }
-      }
-
-      else            // Disk access
-      {
-         // Search for GUID
-         for ( int ii = 0; ii < info.size(); ii++ )
-         {
-            if ( aguid == info[ ii ].guid )
-            {
-               lw_solution_list->setCurrentRow( ii );
-               QListWidgetItem* item = lw_solutions>item( ii );
-               select_solution( item );
-               break;
-            }
-         }
-      }
-   }
-   else
-   {  // There is no selected solution, de-select a list item
-      lw_solution_list->setCurrentRow( -1 );
-
-      reset();
-   }
-}
-
-
-void US_SolutionMgrSelect::query( void )
-{
-   if ( from_db )
-   {
-      read_db();
-   }
-   else
-   {
-      read_soluttion();
-   }
-}
-
-// Read buffer information from local disk
-void US_SolutionMgrSelect::read_solution( void )
-{
-   QString path;
-   if ( ! buffer_path( path ) ) return;
-
-   filenames   .clear();
-   descriptions.clear();
-   GUIDs       .clear();
-   bufferIDs   .clear();
-   le_search->  clear();
-   le_search->setReadOnly( true );
-
- 
-
-   QDir f( path );
-   QStringList filter( "B*.xml" );
-   QStringList f_names = f.entryList( filter, QDir::Files, QDir::Name );
-
-   for ( int ii = 0; ii < f_names.size(); ii++ )
-   {
-      QFile b_file( path + "/" + f_names[ ii ] );
-
-      if ( ! b_file.open( QIODevice::ReadOnly | QIODevice::Text) ) continue;
-
-      QXmlStreamReader xml( &b_file );
-
-      while ( ! xml.atEnd() )
-      {
-         xml.readNext();
-
-         if ( xml.isStartElement() )
-         {
-            if ( xml.name() == "buffer" )
-            {
-               QXmlStreamAttributes a = xml.attributes();
-               descriptions << a.value( "description" ).toString();
-               GUIDs        << a.value( "guid"        ).toString();
-               filenames    << path + "/" + f_names[ ii ];
-               bufferIDs    << "";
-               break;
-            }
-         }
-      }
-   }
-
-   lw_buffer_list->clear();
-
-   if ( descriptions.size() == 0 )
-      lw_buffer_list->addItem( "No buffer files found." );
-   else
-   {
-      le_search->setReadOnly( false );
-      search();
-   }
-}
-
-// Read buffer information from the database
-void US_SolutionMgrSelect::read_db( void )
-{
-   US_Passwd pw;
-   US_DB2 db( pw.getPasswd() );
-
-   if ( db.lastErrno() != US_DB2::OK )
-   {
-      connect_error( db.lastError() );
-      return;
-   }
-
-   bufferIDs   .clear();
-   descriptions.clear();
-   GUIDs       .clear();
-   le_search->  clear();
-   le_search->setText( "" );
-   le_search->setReadOnly( true );
-
-   QStringList q;
-   q << "get_buffer_desc" << QString::number( *personID );
-   db.query( q );
-
-   while ( db.next() )
-   {
-      bufferIDs    << db.value( 0 ).toString();
-      descriptions << db.value( 1 ).toString();
-      GUIDs        << "";
-   }
-
-   lw_buffer_list->clear();
-
-   if ( descriptions.size() == 0 )
-   {
-      lw_buffer_list->addItem( "No buffer files found." );
-   }
-   else
-   {
-      le_search->setReadOnly( false );
-      search();
-   }
-
-}
-
-// Verify path to local buffer files and create directory if need be
-bool US_SolutionMgrSelect::solution_path( QString& path )
-{
-   QDir dir;
-   path = US_Settings::dataDir() + "/buffers";
-
-   if ( ! dir.exists( path ) )
-   {
-      if ( ! dir.mkpath( path ) )
-      {
-         QMessageBox::critical( this,
-            tr( "Bad Buffer Path" ),
-            tr( "Could not create default directory for buffers\n" ) + path );
-         return false;
-      }
-   }
-
-   return true;
-}
-
-// Report a database connection error
-void US_SolutionMgrSelect::connect_error( const QString& error )
-{
-   QMessageBox::warning( this, tr( "Connection Problem" ),
-                        tr( "Could not connect to database \n" ) + error );
-}
-*/
 // Function to refresh the display with values from the solution structure,
 //  and to enable/disable features
 void US_SolutionMgrSelect::reset( void )
 {
+
+   qDebug() << "We are at RESET: ";
    QList< US_Solution::AnalyteInfo >&   ai         = solution->analyteInfo;
    QString                              bufferDesc = solution->buffer.description;
 
    le_bufferInfo   -> setText( solution->buffer.description );
-   le_solutionDesc -> setText( solution->solutionDesc );
+   //le_solutionDesc -> setText( solution->solutionDesc );
    le_commonVbar20 -> setText( QString::number( solution->commonVbar20 ) );
    le_density      -> setText( QString::number( solution->buffer.density ) );
    le_viscosity    -> setText( QString::number( solution->buffer.viscosity ) );
    le_storageTemp  -> setText( QString::number( solution->storageTemp  ) );
    te_notes        -> setText( solution->notes        );
-   le_guid         -> setText( solution->solutionGUID );
-   //ct_amount       -> disconnect();
-   //ct_amount       -> setEnabled( false );
-   //ct_amount       -> setValue( 1 );
+
+   le_amount       -> setText( "1" );
+   //le_guid         -> setText( solution->solutionGUID );
 
    //pb_buffer       -> setEnabled( true );
 
@@ -471,6 +439,9 @@ void US_SolutionMgrSelect::reset( void )
 
    // Let's calculate if we're eligible to save this solution
    //pb_save         -> setEnabled( false );
+
+   qDebug() << "We are at RESET_1: ";
+
    if ( ! bufferDesc.isEmpty() ) //we can have a solution with buffer only
    {
      //pb_save      -> setEnabled( true );
@@ -528,7 +499,17 @@ void US_SolutionMgrSelect::reset( void )
    // le_investigator->setText( number +  US_Settings::us_inv_name() );
 }
 
-
+// Function to handle when solution listwidget item is selected
+void US_SolutionMgrSelect::selectAnalyte( QListWidgetItem* item )
+{
+   // Get the right index in the sorted list, and load the amount
+   int ndx = analyteMap[ item ];
+   le_amount ->setText( QString::number( solution->analyteInfo[ ndx ].amount ) );
+   
+   QPalette p = lb_amount->palette();
+   p.setColor( QPalette::WindowText, Qt::red );
+   lb_amount->setPalette( p );
+}
 
 
  //  will search solutions matching search string
