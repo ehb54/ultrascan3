ef8055152a21177c7f9f05a3ce399029476aaec7
gegorbet <gegorbet@0b344138-67e3-44e0-be2b-e1ba3e4701c6>
2018-02-09 01:52:39 +0000

    new timestate viewer standalone

---
+++ trunk/programs/us_tmst_viewer/us_tmst_viewer.h
@@ -0,0 +1,54 @@
+//! \file us_tmst_viewer.h
+#ifndef US_TMST_VIEWER_H
+#define US_TMST_VIEWER_H
+
+#include <QtGui>
+
+#include "us_extern.h"
+#include "us_xpn_data.h"
+#include "us_widgets.h"
+#include "us_help.h"
+#include "us_plot.h"
+#include "us_dataIO.h"
+
+class US_TmstViewer : public US_Widgets
+{
+  Q_OBJECT
+
+  public:
+
+     //! \brief  Generic constructor for the US_TmstViewer() program.
+     US_TmstViewer();
+
+  private:
+     QList< QStringList >   tmstdata;  // TMST entries
+
+     US_Help        showHelp;
+
+     QLineEdit*     le_dir;
+     QLineEdit*     le_runID;
+     QLineEdit*     le_status;
+
+     QCheckBox*     ck_src_tmp;
+     QCheckBox*     ck_src_imp;
+     QCheckBox*     ck_src_res;
+     QCheckBox*     ck_src_db;
+
+     QPushButton*   pb_loadtmst;
+     QPushButton*   pb_showtmst;
+
+     int            selx;
+     int            dbg_level;
+
+     bool           haveTmst;
+
+  private slots:
+     void   loadTimeState  ( void );
+     void   enableControls ( void );
+     void   showTimeState  ( void );
+     void   tmst_items     ( const QString );
+     void   help           ( void )
+     { showHelp.show_help( "tmst_viewer.html" ); };
+};
+#endif
+
---
+++ trunk/programs/us_tmst_viewer/us_tmst_viewer.pro
@@ -0,0 +1,11 @@
+include( ../../gui.pri )
+
+QT           += opengl sql xml
+
+TARGET        = us_tmst_viewer
+
+
+HEADERS       = us_tmst_viewer.h
+
+SOURCES       = us_tmst_viewer.cpp
+
