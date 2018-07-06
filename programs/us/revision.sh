56907bd5fcdd0205f64b12e6a5de5df04bf73978
demeler <7659499+demeler@users.noreply.github.com>
2018-07-06 12:07:32 +0000

    adjusting to svn.aucsolutions.com

--- programs/us/revision.sh
+++ programs/us/revision.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #  revision.sh  - update us_revision.h with latest svn REVISION value
-SURL="//wiki.aucsolutions.com/ultrascan3/trunk"
+SURL="//svn.aucsolutions.com/ultrascan3/trunk"
 REV1=`svn info svn:${SURL}|grep Revision`
 REV2=`cut -d\" -f2 us_revision.h`
 if [ "$REV1" != "$REV2" ]
