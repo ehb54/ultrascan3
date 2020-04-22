5b42de0b0b6c18f3d6d25a384fcb0364de601092
Alexey <alexsav.science@gmail.com>
2020-04-22 18:19:07 -0500

    Autoflow: ANALYSIS - real GUI now, and test on periodic update of the fields...

--- programs/us_autoflow_analysis/us_autoflow_analysis.cpp
+++ programs/us_autoflow_analysis/us_autoflow_analysis.cpp
@@ -47,14 +47,14 @@ US_Analysis_auto::US_Analysis_auto() : US_Widgets()
   in_gui_update = false;
   in_reload_end_process = false;
   
-  // // ---- Testing ----
-  // QMap < QString, QString > protocol_details;
-  // protocol_details[ "aprofileguid" ] = QString("d13ffad0-6f27-4fd8-8aa0-df8eef87a6ea");
-  // protocol_details[ "protocolName" ] = QString("alexey-abs-itf-test1");
-  // protocol_details[ "invID_passed" ] = QString("12");
+  // ---- Testing ----
+  QMap < QString, QString > protocol_details;
+  protocol_details[ "aprofileguid" ] = QString("d13ffad0-6f27-4fd8-8aa0-df8eef87a6ea");
+  protocol_details[ "protocolName" ] = QString("alexey-abs-itf-test1");
+  protocol_details[ "invID_passed" ] = QString("12");
   
   
-  // initPanel( protocol_details );
+  initPanel( protocol_details );
 
   // -----------------
 
@@ -80,10 +80,12 @@ void US_Analysis_auto::initPanel( QMap < QString, QString > & protocol_details )
   job5run     = false;
   job3auto    = false;
 
+  qDebug() << "Reading Aprofile data: ";
   read_aprofile_data_from_aprofile();
 
   //qDebug() << "job1run, job2run, job3run, job4run, job5run: " << job1run << ", " <<  job2run << ", " <<  job3run << ", " << job4run << ", " <<  job5run ;
-  
+
+  qDebug() << "Reading protocol's triple data: ";
   read_protocol_data_triples();
    
   //Generate GUI
@@ -186,48 +188,53 @@ void US_Analysis_auto::gui_update( )
   
   in_gui_update  = true;          // Flag in the midst of a reload
 
-  QStringList radiobuttons;
-  radiobuttons << "radio1" << "radio2" << "radio3";
-
-
-  int curr_index = rand() % radiobuttons.size(); // pick a random index
-  QString radio_name = radiobuttons[ curr_index ]; // 
+  /*************** DEBUG *************************************************************/ 
+  QStringList fields;
+  fields << "runID" << "owner" << "lastmsg" << "anatype" << "submit" << "cluster" << "lastupd";
+  int curr_index = rand() % fields.size(); // pick a random index
+  QString field_name = fields[ curr_index ]; // 
   
   //TEST access to certain groupboxes' children... Mocup
   for ( int i=0; i<Array_of_triples.size(); ++i )
     {
       QString triple_curr = Array_of_triples[i];
-
+      
       if( triple_curr.contains("Interference")) 
-	{
-	  QRadioButton * button_2dsa    = groupbox_2DSA    [ triple_curr ]->findChild<QRadioButton *>(radio_name, Qt::FindDirectChildrenOnly);
-	  QRadioButton * button_2dsa_fm = groupbox_2DSA_FM [ triple_curr ]->findChild<QRadioButton *>(radio_name, Qt::FindDirectChildrenOnly);
-	  QRadioButton * button_2dsa_it = groupbox_2DSA_IT [ triple_curr ]->findChild<QRadioButton *>(radio_name, Qt::FindDirectChildrenOnly);
-	  QRadioButton * button_2dsa_mc = groupbox_2DSA_MC [ triple_curr ]->findChild<QRadioButton *>(radio_name, Qt::FindDirectChildrenOnly);
+   	{
+   	  QLineEdit * lineedit_2dsa    = groupbox_2DSA    [ triple_curr ]->findChild<QLineEdit *>(field_name, Qt::FindDirectChildrenOnly);
+   	  QLineEdit * lineedit_2dsa_fm = groupbox_2DSA_FM [ triple_curr ]->findChild<QLineEdit *>(field_name, Qt::FindDirectChildrenOnly);
+   	  QLineEdit * lineedit_2dsa_it = groupbox_2DSA_IT [ triple_curr ]->findChild<QLineEdit *>(field_name, Qt::FindDirectChildrenOnly);
+   	  QLineEdit * lineedit_2dsa_mc = groupbox_2DSA_MC [ triple_curr ]->findChild<QLineEdit *>(field_name, Qt::FindDirectChildrenOnly);
+	  
+   	  lineedit_2dsa    ->setText( "Some Message" );
+   	  lineedit_2dsa_fm ->setText( "Some Message" );
+   	  lineedit_2dsa_it ->setText( "Some Message" );
+   	  lineedit_2dsa_mc ->setText( "Some Message" );
 
-	  button_2dsa    ->setChecked( true );
-	  button_2dsa_fm ->setChecked( true );
-	  button_2dsa_it ->setChecked( true );
-	  button_2dsa_mc ->setChecked( true );
-
-	  button_2dsa    ->setStyleSheet("QRadioButton { color: red;}");
-	  button_2dsa_fm ->setStyleSheet("QRadioButton { color: red;}");
-	  button_2dsa_it ->setStyleSheet("QRadioButton { color: red;}");
-	  button_2dsa_mc ->setStyleSheet("QRadioButton { color: red;}");
-
-	  for ( int i=0; i<radiobuttons.size(); ++i )
-	    {
-	      if ( i != curr_index )
-		{
-		  groupbox_2DSA    [ triple_curr ]->findChild<QRadioButton*>(radiobuttons[i], Qt::FindDirectChildrenOnly)->setStyleSheet("QRadioButton { color: black;}");
-		  groupbox_2DSA_FM [ triple_curr ]->findChild<QRadioButton*>(radiobuttons[i], Qt::FindDirectChildrenOnly)->setStyleSheet("QRadioButton { color: black;}");
-		  groupbox_2DSA_IT [ triple_curr ]->findChild<QRadioButton*>(radiobuttons[i], Qt::FindDirectChildrenOnly)->setStyleSheet("QRadioButton { color: black;}");
-		  groupbox_2DSA_MC [ triple_curr ]->findChild<QRadioButton*>(radiobuttons[i], Qt::FindDirectChildrenOnly)->setStyleSheet("QRadioButton { color: black;}");
-		}
-	    }
-	}
-    }
-
+   	  lineedit_2dsa    ->setStyleSheet("QLineEdit { color: red;}");
+   	  lineedit_2dsa_fm ->setStyleSheet("QLineEdit { color: red;}");
+   	  lineedit_2dsa_it ->setStyleSheet("QLineEdit { color: red;}");
+   	  lineedit_2dsa_mc ->setStyleSheet("QLineEdit { color: red;}");
+	  
+   	  for ( int i=0; i < fields.size(); ++i )
+   	    {
+   	      if ( i != curr_index )
+   		{
+		  groupbox_2DSA    [ triple_curr ]->findChild<QLineEdit*>(fields[i], Qt::FindDirectChildrenOnly)->setText( "" );
+   		  groupbox_2DSA_FM [ triple_curr ]->findChild<QLineEdit*>(fields[i], Qt::FindDirectChildrenOnly)->setText( "" );
+   		  groupbox_2DSA_IT [ triple_curr ]->findChild<QLineEdit*>(fields[i], Qt::FindDirectChildrenOnly)->setText( "" );
+   		  groupbox_2DSA_MC [ triple_curr ]->findChild<QLineEdit*>(fields[i], Qt::FindDirectChildrenOnly)->setText( "" );
+		  
+   		  groupbox_2DSA    [ triple_curr ]->findChild<QLineEdit*>(fields[i], Qt::FindDirectChildrenOnly)->setStyleSheet("QLineEdit { color: black;}");
+   		  groupbox_2DSA_FM [ triple_curr ]->findChild<QLineEdit*>(fields[i], Qt::FindDirectChildrenOnly)->setStyleSheet("QLineEdit { color: black;}");
+   		  groupbox_2DSA_IT [ triple_curr ]->findChild<QLineEdit*>(fields[i], Qt::FindDirectChildrenOnly)->setStyleSheet("QLineEdit { color: black;}");
+   		  groupbox_2DSA_MC [ triple_curr ]->findChild<QLineEdit*>(fields[i], Qt::FindDirectChildrenOnly)->setStyleSheet("QLineEdit { color: black;}");
+		  
+   		}
+   	    }
+   	}
+     }
+  
   in_gui_update  = false; 
 }
 
@@ -305,27 +312,84 @@ QGroupBox * US_Analysis_auto::createGroup( QString & triple_name )
   p.setColor(QPalette::Dark, Qt::white);
   groupBox->setPalette(p);
 
-  groupBox-> setStyleSheet( "QGroupBox { font: bold;  background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #E0E0E0, stop: 1 #FFFFFF); border: 2px solid gray; border-radius: 10px; margin-top: 20px; margin-bottom: 10px; padding-top: 5px; } QGroupBox::title { subcontrol-origin: margin; subcontrol-position: top left; left: 10px; margin: 0 5px; background-color: black; color: white; padding: 0 3px;}  QGroupBox::indicator { width: 13px; height: 13px; border: 1px solid grey; background-color: rgba(204, 204, 204, 255);} QGroupBox::indicator:hover {background-color: rgba(235, 235, 235, 255);}");
+  groupBox-> setStyleSheet( "QGroupBox { font: bold;  background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #E0E0E0, stop: 1 #FFFFFF); border: 2px solid gray; border-radius: 10px; margin-top: 20px; margin-bottom: 10px; padding-top: 5px; } QGroupBox::title { subcontrol-origin: margin; subcontrol-position: top left; left: 10px; margin: 0 5px; background-color: black; color: white; padding: 0 3px;}  QGroupBox::indicator { width: 13px; height: 13px; border: 1px solid grey; background-color: rgba(204, 204, 204, 255);} QGroupBox::indicator:hover {background-color: rgba(235, 235, 235, 255);} QLabel {background-color: rgb(105,105,105);}");
 
   
   groupBox->setFlat(true);
 
-  //Moc-up GUI
-  QRadioButton *radio1 = new QRadioButton(tr("&Radio button 1"));
-  radio1->setObjectName("radio1");
-  QRadioButton *radio2 = new QRadioButton(tr("R&adio button 2"));
-  radio2->setObjectName("radio2");
-  QRadioButton *radio3 = new QRadioButton(tr("Ra&dio button 3"));
-  radio3->setObjectName("radio3");
+  //GUI
+  QGridLayout* genL   = new QGridLayout();
+  genL->setSpacing        ( 2 );
+  genL->setContentsMargins( 20, 10, 20, 15 );
   
-  radio1->setChecked(true);
+  int row = 0;
+
+  //RunID
+  QLabel*     lb_runID   = us_label( tr( "RunID:" ) );
+  QLineEdit*  le_runID   = us_lineedit( "", 0, true );
+  le_runID -> setObjectName("runID");
+
+  //Owner
+  QLabel*     lb_owner   = us_label( tr( "Owner:" ) );
+  QLineEdit*  le_owner   = us_lineedit( "", 0, true );
+  le_owner -> setObjectName("owner");
+
+  //Last Msg
+  QLabel*     lb_lastmsg = us_label( tr( "Last Message:" ) );
+  QLineEdit*  le_lastmsg = us_lineedit( "", 0, true );
+  le_lastmsg -> setObjectName("lastmsg");
+
+  //Status
+  QLabel*     lb_status  = us_label( tr( "Status:" ) );
+  QLineEdit*  le_status  = us_lineedit( "", 0, true );
+  le_status -> setObjectName("status");
+
+  //Analysis Type
+  QLabel*     lb_anatype = us_label( tr( "Analysis Type:" ) );
+  QLineEdit*  le_anatype = us_lineedit( "", 0, true );
+  le_anatype -> setObjectName("anatype");
+
+  //Submitted On
+  QLabel*     lb_submit  = us_label( tr( "Submitted on:" ) );
+  QLineEdit*  le_submit  = us_lineedit( "", 0, true );
+  le_submit -> setObjectName("submit");
+
+  //Running On
+  QLabel*     lb_cluster  = us_label( tr( "Running on:" ) );
+  QLineEdit*  le_cluster  = us_lineedit( "", 0, true  );
+  le_cluster -> setObjectName("cluster");
+  le_cluster->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Preferred);
   
-  QVBoxLayout *vbox = new QVBoxLayout;
-  vbox->addWidget(radio1);
-  vbox->addWidget(radio2);
-  vbox->addWidget(radio3);
-  vbox->addStretch(1);
-  groupBox->setLayout(vbox);
+  //Last Status Update
+  QLabel*     lb_lastupd  = us_label( tr( "Last Status Update:" ) );
+  QLineEdit*  le_lastupd  = us_lineedit( "", 0, true );
+  le_lastupd -> setObjectName("lastupd");
+  
+  
+  genL->addWidget( lb_runID,   row,    0, 1, 2 );
+  genL->addWidget( le_runID,   row++,  2, 1, 8 );
+
+  genL->addWidget( lb_owner,   row,    0, 1, 2 );
+  genL->addWidget( le_owner,   row++,  2, 1, 8 );  
+
+  genL->addWidget( lb_lastmsg, row,    0, 1, 2 );
+  genL->addWidget( le_lastmsg, row++,  2, 1, 8 );
+
+  genL->addWidget( lb_status,  row,    0, 1, 2 );
+  genL->addWidget( le_status,  row,    2, 1, 3 );
+  genL->addWidget( lb_anatype, row,    5, 1, 2 );
+  genL->addWidget( le_anatype, row++,  7, 1, 3 );
+
+  genL->addWidget( lb_submit,  row,    0, 1, 2 );
+  genL->addWidget( le_submit,  row,    2, 1, 3 );
+  genL->addWidget( lb_cluster, row,    5, 2, 2 );
+  genL->addWidget( le_cluster, row++,  7, 2, 3 );
+
+  genL->addWidget( lb_lastupd, row,    0, 1, 2 );
+  genL->addWidget( le_lastupd, row,    2, 1, 3 );
+  
+  groupBox->setLayout(genL);
+  
   
   return groupBox;
 }
