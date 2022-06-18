532f425dbbf72b73cb57671a64cd1c15c16c540f
ehb54 <ehb54@users.noreply.github.com>
2022-06-18 10:05:12 -0500

    *alpha* save plot fix when both errorbars and SD checked

--- us_somo/develop/src/us_hydrodyn_saxs_plots.cpp
+++ us_somo/develop/src/us_hydrodyn_saxs_plots.cpp
@@ -1628,12 +1628,29 @@ void US_Hydrodyn_Saxs::pp()
       use_plot_info[ "US-SOMO SAXS PRR Residuals" ] = saxs_residuals_window->plot;
    }
 
+   // save and restore flags since SD errorbars duplicate Err. Maybe should remove sd errorbars flag or mirror
+   bool eb_set = cb_eb                  ->isChecked();
+   bool sd_set = cb_resid_show_errorbars->isChecked();
+   
+   if ( eb_set || sd_set ) {
+      cb_eb                  ->setChecked( true );
+      cb_resid_show_errorbars->setChecked( false );
+      set_eb();
+      set_resid_show_errorbars();
+   }
+
    if ( !US_Plot_Util::printtofile( fn, use_plot_info, errors, messages ) )
    {
       editor_msg( "red", errors );
    } else {
       editor_msg( "blue", messages );
    }
+
+   cb_eb                  ->setChecked( eb_set );
+   cb_resid_show_errorbars->setChecked( sd_set );
+   set_eb();
+   set_resid_show_errorbars();
+
 }
 
 void US_Hydrodyn_Saxs::set_eb() 
