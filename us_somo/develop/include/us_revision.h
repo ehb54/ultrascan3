2cf7e0081f3564c8ad761dfa13bfb26f0d42e01d
ehb54 <ehb54@users.noreply.github.com>
2022-06-29 10:02:41 -0500

    *alpha* setting text edit (vdW GRPY -> GRPY)

--- us_somo/develop/include/us_revision.h
+++ us_somo/develop/include/us_revision.h
@@ -1,4 +1,4 @@
-#define REVISION "Revision: 6890-somo-dev"
+#define REVISION "Revision: 6899-somo-dev"
 // #define REVISION_DATE "2020-03-20 08:00:00 -0500 (Fri, 20 March 2020)"
-#define REVISION_DATE "2022-06-28"
+#define REVISION_DATE "2022-06-29"
 
