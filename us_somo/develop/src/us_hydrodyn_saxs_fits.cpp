79ff9b5d15df0535c698b61bc40895138c13e838
ehb54 <ehb54@users.noreply.github.com>
2022-07-29 17:35:31 +0100

    *alpha* also optional scaling for NNLS I(q)

--- us_somo/develop/src/us_hydrodyn_saxs_fits.cpp
+++ us_somo/develop/src/us_hydrodyn_saxs_fits.cpp
@@ -190,7 +190,8 @@ void US_Hydrodyn_Saxs::calc_iqq_nnls_fit( QString /* title */, QString csv_filen
    
    editor->append(QString("Residual Euclidian norm of NNLS fit %1\n").arg(nnls_rmsd));
    
-   vector < double > rescaled_x = rescale(use_x);
+   vector < double > rescaled_x = our_saxs_options->disable_nnls_scaling ? use_x : rescale(use_x);
+
    // list models & concs
    
    QString use_color = "black";
