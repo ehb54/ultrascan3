db6490a46f67ea01cb17a520f3ab5614a0ac6cef
ehb54 <ehb54@users.noreply.github.com>
2022-08-02 12:00:40 +0100

    *alpha* grid fix for multi csv load

--- us_somo/develop/include/us_revision.h
+++ us_somo/develop/include/us_revision.h
@@ -1,4 +1,4 @@
-#define REVISION "Revision: 6971-somo-dev"
+#define REVISION "Revision: 6972-somo-dev"
 // #define REVISION_DATE "2020-03-20 08:00:00 -0500 (Fri, 20 March 2020)"
-#define REVISION_DATE "2022-08-01"
+#define REVISION_DATE "2022-08-02"
 
