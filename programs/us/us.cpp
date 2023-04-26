9fc7dfb4ded067d790e446cfedd76a6686c964e9
alexsav815 <alexsav.science@gmail.com>
2023-04-26 10:53:54 -0500

    US, main launcher -- if DB connection defined BUT no DB connection (no internet), inform user of inability to synchronize & proceed...

--- programs/us/us.cpp
+++ programs/us/us.cpp
@@ -109,31 +109,33 @@ int main( int argc, char* argv[] )
       
       if ( db.lastErrno() != US_DB2::OK )
         {
-          //qDebug() << "USCFG: UpdInv: ERROR connect";
-          QMessageBox msgBox;
-          msgBox.setWindowTitle ("ERROR");
-          msgBox.setText("Error making the DB connection!");
+	  QMessageBox msgBox;
+          msgBox.setWindowTitle ("ERROR: User Level Synchronization");
+          msgBox.setText("Error making the DB connection! User-level cannot be synchronized.");
           msgBox.setIcon  ( QMessageBox::Critical );
           msgBox.exec();
       
-          exit( -1 );
+          //exit( -1 );
         }
+      else
+	{
       
-      QStringList q( "get_user_info" );
-      db.query( q );
-      db.next();
-      
-      int ID        = db.value( 0 ).toInt();
-      QString fname = db.value( 1 ).toString();
-      QString lname = db.value( 2 ).toString();
-      int     level = db.value( 5 ).toInt();
-      
-      qDebug() << "USCFG: UpdInv: ID,name,lev" << ID << fname << lname << level;
-      //if(ID<1) return;
-      
-      US_Settings::set_us_inv_name ( lname + ", " + fname );
-      US_Settings::set_us_inv_ID   ( ID );
-      US_Settings::set_us_inv_level( level );
+	  QStringList q( "get_user_info" );
+	  db.query( q );
+	  db.next();
+	  
+	  int ID        = db.value( 0 ).toInt();
+	  QString fname = db.value( 1 ).toString();
+	  QString lname = db.value( 2 ).toString();
+	  int     level = db.value( 5 ).toInt();
+	  
+	  qDebug() << "USCFG: UpdInv: ID,name,lev" << ID << fname << lname << level;
+	  //if(ID<1) return;
+	  
+	  US_Settings::set_us_inv_name ( lname + ", " + fname );
+	  US_Settings::set_us_inv_ID   ( ID );
+	  US_Settings::set_us_inv_level( level );
+	}
     }
   
   //END OF user-level sync//////////////////////////////////////
