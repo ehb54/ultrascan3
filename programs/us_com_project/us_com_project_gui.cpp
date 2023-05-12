d9bdf26e7e04e7476fc85701f018bc594b30c064
alexsav815 <alexsav.science@gmail.com>
2023-05-11 19:40:13 -0500

    GMP  -- cosmetic fix for a message on Optima(s) being disconnected...

--- programs/us_com_project/us_com_project_gui.cpp
+++ programs/us_com_project/us_com_project_gui.cpp
@@ -1317,7 +1317,7 @@ void US_InitDialogueGui::checkCertificates( void )
 	    {
 	      if ( daysToExpiration > 0 )
 		{
-		  msg_sys_text_info += QString(tr("\n%1 Please check the following for %2:")).arg( QChar(0x2022), alias );
+		  msg_sys_text_info += QString(tr("\n%1 Please check the following for %2:\n")).arg( QChar(0x2022), alias );
 		  msg_sys_text_info += QString( tr("1. %1 is turned on\n"
 						   "2. the data acquisition server on %1 is running\n"
 						   "3. your license key is stored in $HOME/ultrascan/etc/optima and is valid and not expired.\n")).arg( alias );
