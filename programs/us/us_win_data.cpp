efcba61792873dd0428007292743f4fdde4f1fcb
alexsav815 <alexsav.science@gmail.com>
2023-09-20 11:39:38 -0500

    US | GMP Menu  -- moved GMP Reporter and Viewer to GMP menu...

--- programs/us/us_win_data.cpp
+++ programs/us/us_win_data.cpp
@@ -21,7 +21,7 @@ namespace US_WinData
 
     P_GLOBFITEQ,                                // Global Fit Menu
 
-    P_ABDE_FIT,     P_GETDATA,      P_GMPRPT,       P_CONVERT,      P_EXPORT,       P_CEXPERI,  // Utilities Menu
+    P_ABDE_FIT,     P_GETDATA,      P_CONVERT,      P_EXPORT,       P_CEXPERI,  // Utilities Menu
     P_FDSMAN,       P_FITMEN,       P_COLORGRAD,
     P_RPTGEN,       P_ROTORCAL,     P_LICENSE,
     P_VHWCOMB,      P_DDCOMB,       P_INTCOMB,      P_GLOMODL,
@@ -35,7 +35,7 @@ namespace US_WinData
     P_SASSOC,       P_MODEL1,       P_MODEL2,
     P_SOMO,         P_SOMOCONFIG,
 
-    P_GMPACQ,       P_PROTOCOL,     P_ESIGN,    // GMP Menu
+    P_GMPACQ,       P_PROTOCOL,     P_GMPRPT, P_ESIGN,    // GMP Menu
     P_AUDIT,
 
     P_RMSD,         P_INVESTIGATOR, P_BUFFER,   // Database Menu
@@ -174,11 +174,7 @@ namespace US_WinData
       QObject::tr( "Acquisition Experimental Data Program" )
     },
 
-    { P_GMPRPT,    1, 0, "us_reporter_gmp",
-      QObject::tr( "Loading GMP Report Generator and Viewer" ),
-      QObject::tr( "GMP Report Generator and Viewer Program" )
-    },
-            
+    
     { P_CONVERT,  1, 0, "us_convert",
       QObject::tr( "Loading Experimental Data Import Routine" ),
       QObject::tr( "Experimental Data Import Program" )
@@ -343,6 +339,11 @@ namespace US_WinData
     { P_PROTOCOL, 0, 0, "us_protocoldev",
       QObject::tr( "Loading Protocol Development Program" ),
       QObject::tr( "Protocol Development System" )
+    },
+
+    { P_GMPRPT,    1, 0, "us_reporter_gmp",
+      QObject::tr( "Loading GMP Report Generator and Viewer" ),
+      QObject::tr( "GMP Report Generator and Viewer Program" )
     },
 
     { P_ESIGN, 0, 0, "us_esigner_gmp",
