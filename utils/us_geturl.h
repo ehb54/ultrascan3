24df57a6925f63e52a69352242d92c31546cdf8c
aaron-auc <aaron@aucsolutions.com>
2024-05-22 14:40:49 -0400

    Generated comments

--- utils/us_geturl.h
+++ utils/us_geturl.h
@@ -1,3 +1,8 @@
+/**
+ * @file us_geturl.h
+ * @brief Defines the US_GetUrl class for performing HTTP GET requests.
+ */
+
 #ifndef US_GETURL_H
 #define US_GETURL_H
 
@@ -5,24 +10,52 @@
 #include <QtWidgets/QApplication>
 #include "us_extern.h"
 
-// derived from last answer https://stackoverflow.com/questions/46943134/how-do-i-write-a-qt-http-get-request
-
+/**
+ * @class US_GetUrl
+ * @brief A class to perform HTTP GET requests.
+ *
+ * This class uses QNetworkAccessManager to perform HTTP GET requests and
+ * retrieve data from a specified URL.
+ */
 class US_UTIL_EXTERN US_GetUrl : public QObject {
-   Q_OBJECT
- public:
-   explicit US_GetUrl();
-   void get( const QString & url );
-   QNetworkAccessManager *manager;
-   QByteArray downloadedData() const;
+    Q_OBJECT
+public:
+    /**
+     * @brief Constructs a US_GetUrl object.
+     */
+    explicit US_GetUrl();
 
- signals:
-   void downloaded();
+    /**
+     * @brief Initiates an HTTP GET request for the given URL.
+     *
+     * @param url The URL to send the GET request to.
+     */
+    void get(const QString & url);
 
- private:
-   QByteArray m_DownloadedData;
-   
- private slots:
-   void ReplyFinished(QNetworkReply *reply);
+    /**
+     * @brief Returns the downloaded data.
+     *
+     * @return The downloaded data as a QByteArray.
+     */
+    QByteArray downloadedData() const;
+
+    signals:
+            /**
+             * @brief Signal emitted when the download is complete.
+             */
+            void downloaded();
+
+private:
+    QNetworkAccessManager *manager; ///< Manager for handling network operations.
+    QByteArray m_DownloadedData;    ///< Byte array to store downloaded data.
+
+private slots:
+            /**
+             * @brief Slot called when the network reply is finished.
+             *
+             * @param reply The QNetworkReply containing the response.
+             */
+            void ReplyFinished(QNetworkReply *reply);
 };
 
 #endif // US_GETURL_H
