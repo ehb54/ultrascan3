ba76834577bf1f571e45df19467a1d4375a8b15d
ehb54 <brookes@uthscsa.edu>
2024-06-06 09:50:23 -0500

    default grpy exclude buried, revision

--- us_somo/develop/include/us_revision.h
+++ us_somo/develop/include/us_revision.h
@@ -1,4 +1,5 @@
-#define REVISION "Revision: 2024-02-08-somo-dev"
+// #define REVISION "Revision: 2024-06-06-somo-dev"
+#define REVISION "Revision: 7211"
 // #define REVISION_DATE "2020-03-20 08:00:00 -0500 (Fri, 20 March 2020)"
-#define REVISION_DATE "2024-02-08"
+#define REVISION_DATE "2024-06-06"
 
