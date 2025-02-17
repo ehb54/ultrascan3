844c1299d713164825435b9e0aeeb8e1542a09b9
doluk <69309597+doluk@users.noreply.github.com>
2025-02-17 16:07:40 +0100

    Revert "Attempt to gracefully shut down mpi processes"
    This reverts commit 6c81c1cc42879f07272e556cf8b931653d2998ce.

--- programs/us_mpi_analysis/us_mpi_analysis.cpp
+++ programs/us_mpi_analysis/us_mpi_analysis.cpp
@@ -1205,7 +1205,7 @@ void US_MPI_Analysis::start( void )
                   << "MB,  total run seconds " << cputime;
       }
    }
-   //MPI_Barrier( MPI_COMM_WORLD );
+   MPI_Barrier( MPI_COMM_WORLD );
    MPI_Finalize();
    exit( exit_status );
 }
