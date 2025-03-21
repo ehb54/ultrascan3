fd700352a45226b0da7a733e268a08635fc6070f
doluk <69309597+doluk@users.noreply.github.com>
2025-03-21 13:13:49 +0100

    Allow less delay between acceleration end and first scan
    The previous calculation relied on an overestimated acceleration rate in the speed step resulting in an offset of -3 for the scan timing based on experimental observations. The updated calculation for the timestate makes this correction obsolete. In order to keep not suddenly display errors for older data, the offset of +1 is applied due to making tf_aend a double instead of an integer avoiding potential rounding issues.
    
    Signed-off-by: doluk <69309597+doluk@users.noreply.github.com>

--- programs/us_experiment/us_experiment_gui_optima.cpp
+++ programs/us_experiment/us_experiment_gui_optima.cpp
@@ -1993,7 +1993,12 @@ DbgLv(1) << "CGui:IOD:   cSS nspeed" << speedsteps.size();
       int tf_scan      = speedsteps[ 0 ].time_first;
       int accel1       = (int)qRound( rate );
       int rspeed       = speedsteps[ 0 ].rotorspeed;
-      int tf_aend      = ( rspeed + accel1 - 1 ) / accel1;
+      double  tf_aend   = static_cast<double>(tf_scan);
+      // prevent any division by zero
+      if (accel1 != 0)
+      {
+         tf_aend = static_cast<double>(rspeed) / static_cast<double>(accel1);
+      }
 
       QString wmsg = tr( "The SpeedStep computed/used is likely bad:<br/>"
                          "The acceleration implied is %1 rpm/sec.<br/>"
@@ -2002,7 +2007,7 @@ DbgLv(1) << "CGui:IOD:   cSS nspeed" << speedsteps.size();
                          "<b>You should rerun the experiment without<br/>"
                          "any interim constant speed, and then<br/>"
                          "you should reimport the data.</b>" )
-                     .arg( accel1 ).arg( tf_aend ).arg( tf_scan );
+                     .arg( accel1 ).arg( QString::number(tf_aend) ).arg( QString::number(tf_scan) );
 
       QMessageBox msgBox( this );
       msgBox.setWindowTitle( tr( "Bad TimeState Implied!" ) );
