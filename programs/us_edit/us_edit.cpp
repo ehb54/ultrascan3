472c38b2ebbd72614ebd517dc0a352c96c1ae4e8
alexsav815 <alexsav.science@gmail.com>
2025-07-07 12:55:29 -0500

    [SUPress unit test] GMP: 4. EDIT, manual edit  -- pass meniscus & data range values even when edited in the respective fields ...

--- programs/us_edit/us_edit.cpp
+++ programs/us_edit/us_edit.cpp
@@ -756,14 +756,14 @@ pb_plateau->setVisible(false);
    // details[ "runID" ]        = QString("1981");
    // details[ "OptimaName" ]   = QString("Optima 1");
 
-   details[ "invID_passed" ] = QString("165");
-   details[ "filename" ]     = QString("GMPMeniscusFitTest_6MAY24-run2023");
-   details[ "protocolName" ] = QString("GMPMeniscusFitTest_6MAY24v2");
-   details[ "statusID" ]     = QString("334");
-   details[ "autoflowID" ]   = QString("1140");
-   details[ "runID" ]        = QString("2023");
-   details[ "OptimaName" ]   = QString("Optima 1");
-   details[ "expType" ]      = QString("VELOCITY");  
+   // details[ "invID_passed" ] = QString("165");
+   // details[ "filename" ]     = QString("GMPMeniscusFitTest_6MAY24-run2023");
+   // details[ "protocolName" ] = QString("GMPMeniscusFitTest_6MAY24v2");
+   // details[ "statusID" ]     = QString("334");
+   // details[ "autoflowID" ]   = QString("1140");
+   // details[ "runID" ]        = QString("2023");
+   // details[ "OptimaName" ]   = QString("Optima 1");
+   // details[ "expType" ]      = QString("VELOCITY");  
 
    // details[ "invID_passed" ] = QString("165");
    // details[ "filename" ]     = QString("ABDE-Test-052124-run1693");
@@ -784,7 +784,7 @@ pb_plateau->setVisible(false);
    // details[ "expType" ]      = QString("ABDE");
    // details[ "dataSource" ]   = QString("dataDiskAUC");
    
-   load_auto( details );
+   //load_auto( details );
   
 }
 
