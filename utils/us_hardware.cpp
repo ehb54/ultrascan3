c03e53c33a046303a0973d609fb2077954717ba0
samo38 <saeed.mortezazadeh25@gmail.com>
2025-07-22 12:24:34 -0600

    replace qSor with str::sort for vectors of int, double, and QString

--- utils/us_hardware.cpp
+++ utils/us_hardware.cpp
@@ -208,7 +208,7 @@ bool US_AbstractCenterpiece::read_centerpieces( US_DB2* db,
          cpids   << db->value( 0 ).toString().toInt();
       }
 
-      qSort( cpids );
+      std::sort( cpids.begin(), cpids.end() );
 
       for ( int ii = 0; ii < cpids.size(); ii++ )
       {
