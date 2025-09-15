0337b97ebfe25072971b60c29b4a2d390667da17
alexsav815 <alexsav.science@gmail.com>
2025-09-15 14:13:02 -0500

    [test1]GMP | 5. ANALYSIS -- fix for false 'no -FM data' message triggering for the newly developed custom grid analysis...

--- programs/us_fit_meniscus/us_fit_meniscus_main.cpp
+++ programs/us_fit_meniscus/us_fit_meniscus_main.cpp
@@ -13,11 +13,11 @@ int main( int argc, char* argv[] )
 
    // License is OK.  Start up.
    
-   US_FitMeniscus w;
+   //US_FitMeniscus w;
 
    // AutoFlow DEBUG
 
-   /*
+   
    QMap < QString, QString > triple_info_map;
    // triple_info_map[ "triple_name" ]     = QString("2 / A / 259");
    // triple_info_map[ "invID" ]           = QString("3");
@@ -40,12 +40,12 @@ int main( int argc, char* argv[] )
    // triple_info_map[ "filename" ]        = QString("YeQ_Calpain3-lowTCEP_052721-run1464");
 
 
-   triple_info_map[ "triple_name" ]     = QString("3 / B / 235");
-   triple_info_map[ "invID" ]           = QString("143");
-   triple_info_map[ "filename" ]        = QString("DicksionC_SDS-1_032122-run1281");
+   triple_info_map[ "triple_name" ]     = QString("2 / A / 259");
+   triple_info_map[ "invID" ]           = QString("2");
+   triple_info_map[ "filename" ]        = QString("demo1_veloc-dataDiskRun-1607");
    
    US_FitMeniscus w( triple_info_map );
-   */
+   
    
    w.show();                   //!< \memberof QWidget
    return application.exec();  //!< \memberof QApplication
