0fa81d8ac6c3fc80cbfb121038e9a6a0cde634bf
alexsav815 <alexsav.science@gmail.com>
2025-09-26 14:35:46 -0500

    [REstore form -- unit test for multi-channel case] GMP | 4. EDIT -- treat scan exclusion differently for ABDE vs. VELOCITY types (ABDE - usually only last scan remains for each triple, ensured manually; VEL. - based on AProfile's settings beg|nth|end)...

--- programs/us_edit/us_edit_main.cpp
+++ programs/us_edit/us_edit_main.cpp
@@ -13,8 +13,8 @@ int main( int argc, char* argv[] )
 
    // License is OK.  Start up.
    
-   US_Edit w("AUTO");
-   //US_Edit w;
+   //US_Edit w("AUTO");
+   US_Edit w;
    w.show();                   //!< \memberof QWidget
    return application.exec();  //!< \memberof QApplication
 }
