f70cdd2c5f08ff6ae3e6708bdeeccf1e157b7061
doluk <69309597+doluk@users.noreply.github.com>
2025-10-15 13:33:02 +0200

    Comment out redundant widget movement and debug logging in `US_FeMatch::main` for cleaner output.

--- programs/us_fematch/us_fematch_main.cpp
+++ programs/us_fematch/us_fematch_main.cpp
@@ -15,8 +15,8 @@ int main( int argc, char* argv[] )
    qDebug() << "US_FeMatch::main()" << w.pos() << w.mapToGlobal(w.pos()) << w.frameGeometry() << w.size();
    w.show();                   //!< \memberof QWidget
    qDebug() << "US_FeMatch::main()" << w.pos() << w.mapToGlobal(w.pos()) << w.frameGeometry() << w.size();
-   w.move(300,300);
-   qDebug() << "US_FeMatch::main()" << w.pos() << w.mapToGlobal(w.pos()) << w.frameGeometry() << w.size();
+   //w.move(300,300);
+   //qDebug() << "US_FeMatch::main()" << w.pos() << w.mapToGlobal(w.pos()) << w.frameGeometry() << w.size();
 
    return application.exec();  //!< \memberof QApplication
 }
