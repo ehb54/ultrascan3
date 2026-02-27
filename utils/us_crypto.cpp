817a32ad7fe5e5c35ec0479673a2f4c20a7a8e3c
Lukas Dobler <69309597+doluk@users.noreply.github.com>
2026-02-27 15:37:17 +0100

    Fix tag data retrieval in EVP_CIPHER_CTX_ctrl

--- utils/us_crypto.cpp
+++ utils/us_crypto.cpp
@@ -293,7 +293,7 @@ QString US_Crypto::decrypt( const QString& ciphertext, const QString& pw,
          ctx.get(),
          EVP_CTRL_GCM_SET_TAG,
          tag.size(),
-         const_cast<char*>(tag.constData())
+         tag.data()
       );
    }
 
