062c3d2366cc1939df31abf0a90ee9aa81093ffe
Lukas Dobler <69309597+doluk@users.noreply.github.com>
2026-03-13 22:14:23 +0100

    Replace 'using namespace std' with std:: in us_timer.h
    Prevent windows specific type definition conflicts caused by using namespace std;

--- utils/us_timer.h
+++ utils/us_timer.h
@@ -9,8 +9,6 @@
 #include "us_extern.h"
 #include <QElapsedTimer>
 
-using namespace std;
-
 /**
  * @class US_Timer
  * @brief A class to manage multiple timers.
@@ -70,10 +68,10 @@ public:
     QString list_times();
 
 private:
-    map<QString, QElapsedTimer> timers;    ///< Map of timer names to QElapsedTimer objects.
-    map<QString, unsigned int> counts;     ///< Map of timer names to counts.
-    map<QString, unsigned long> times;     ///< Map of timer names to recorded times.
-    map<QString, unsigned long> times2;    ///< Map of timer names to secondary recorded times.
+    std::map<QString, QElapsedTimer> timers;    ///< Map of timer names to QElapsedTimer objects.
+    std::map<QString, unsigned int> counts;     ///< Map of timer names to counts.
+    std::map<QString, unsigned long> times;     ///< Map of timer names to recorded times.
+    std::map<QString, unsigned long> times2;    ///< Map of timer names to secondary recorded times.
 };
 
 #endif // US_TIMER_H
