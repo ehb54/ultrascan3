4bbc316763ec89ecf5dbf5a61dbdfd15aa7f007c
alexsav815 <alexsav.science@gmail.com>
2026-08-10 02:31:04 -0500

    [3b]GMP-VELOCITY-MWL: 5.ANALYSIS -- after MPI analysis ends, proceed with MWL pre-fit simulations; .pro & respective CMake file edited accordingly  ...

--- programs/us_autoflow_analysis/us_autoflow_analysis.cpp
+++ programs/us_autoflow_analysis/us_autoflow_analysis.cpp
@@ -1298,7 +1298,7 @@ void US_Analysis_auto::gui_update( )
 	      //now call sim. contructor
 	      sdiag_mwlsim = new US_MwlSpeciesSim();
 	      sdiag_mwlsim -> select_models_auto( QString::number( invID ), m_c_r_id );
-	      sdiag_mwlsim->show(); //DEBUG
+	      //sdiag_mwlsim->show(); //DEBUG
 	    }
 	    
 	  return;
