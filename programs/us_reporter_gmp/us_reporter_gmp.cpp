b2df77484199df515ac9ac99c157beb7b4d56d4f
alexsav815 <alexsav.science@gmail.com>
2026-08-23 21:49:24 -0500

    [2]GMP-ABDE: 5. ANALYSIS -- work on reporting intagrations for dna and protein ...

--- programs/us_reporter_gmp/us_reporter_gmp.cpp
+++ programs/us_reporter_gmp/us_reporter_gmp.cpp
@@ -8150,7 +8150,7 @@ QString US_ReporterGMP::distrib_info_abde( QString& abde_channame  )
 
        mstr += "\n" + indent( 2 ) + tr( "<h3>Integration Results: Fraction of Total Concentration:</h3>\n" );
        QString mstr_dna = QString(tr( "<h4>DNA signal</h4>\n") );
-       mstr_dna += "<table>\n";
+       //mstr_dna += "<table>\n";
        mstr += QString(tr( "<h4>Protein signal</h4>\n") );
        mstr += indent( 2 ) + "<table>\n";
        QString header_trftp = table_row( tr( "Type:" ),
@@ -8209,9 +8209,10 @@ QString US_ReporterGMP::distrib_info_abde( QString& abde_channame  )
 				  QString::number( frac_tot_tol_r ),
 				  tot_frac_passed_dna );
 	 }
-       mstr     += indent( 2 ) + "</table>\n";
-       mstr_dna += "</table>\n";
        mstr     += mstr_dna;
+       mstr     += indent( 2 ) + "</table>\n";
+       //mstr_dna += "</table>\n";
+       //mstr     += mstr_dna;
        //End of integration results
      }
  
