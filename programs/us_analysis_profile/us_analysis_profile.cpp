c4fd147ae564f10f4f9363aa2926b0b5b763db87
alexsav815 <alexsav.science@gmail.com>
2026-09-10 18:15:01 -0500

    [5a]GMP: 1. EXP.: 8. APorfile->ABDE -- fix geomentry...ushush

--- programs/us_analysis_profile/us_analysis_profile.cpp
+++ programs/us_analysis_profile/us_analysis_profile.cpp
@@ -1811,15 +1811,25 @@ DbgLv(1) << "Ge:SL: nchn" << nchn << "lcrat size" << le_lcrats.count();
    middle_h->addWidget( controlsRestrictorWidget_left, 0, 0 );
    middle_h->addWidget( controlsRestrictorWidget_right, 0, 7, -1, 2, Qt::AlignRight);
    
-   //middle_h->setRowStretch( 0, 1);
-   //Hide all gr_mwvbox instances:
-   for ( int i=0; i < gr_mwvbox.size(); ++i )
-     gr_mwvbox[ i ]->setVisible( false );
+   // //middle_h->setRowStretch( 0, 1);
+   // //Hide all gr_mwvbox instances:
+   // for ( int i=0; i < gr_mwvbox.size(); ++i )
+   //   gr_mwvbox[ i ]->setVisible( false );
 
    panel->addLayout( middle_h );
 
    if ( mainw->abde_mode_aprofile )
-     controlsRestrictorWidget_right->hide();
+     {
+       controlsRestrictorWidget_right->hide();
+       middle_h->removeWidget(controlsRestrictorWidget_left);
+       middle_h->addWidget(controlsRestrictorWidget_left, 0, 0, 1, 9);
+     }
+   else
+     {
+       middle_h->removeWidget(controlsRestrictorWidget_left);
+       middle_h->addWidget(controlsRestrictorWidget_left, 0, 0); // Reset to default 1x1 cell
+       controlsRestrictorWidget_right->show();
+     }
    
    panel->addStretch();
    
